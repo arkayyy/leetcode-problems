4264ebe36f41fe3e72c36627526ac3ae37d7586b
Rajarshi Karan <aparnapatro2001@gmail.com>
2021-09-11 21:10:15 +0530

    push

---
+++ Matrices/p1-noOfIslands.cpp
@@ -0,0 +1,60 @@
+// Given an m x n 2D binary grid grid which represents a map of '1's (land) and '0's (water), return the number of islands.
+
+// An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
+
+//  Example 1:
+
+// Input: grid = [
+//   ["1","1","1","1","0"],
+//   ["1","1","0","1","0"],
+//   ["1","1","0","0","0"],
+//   ["0","0","0","0","0"]
+// ]
+// Output: 1
+
+// Example 2:
+
+// Input: grid = [
+//   ["1","1","0","0","0"],
+//   ["1","1","0","0","0"],
+//   ["0","0","1","0","0"],
+//   ["0","0","0","1","1"]
+// ]
+// Output: 3
+
+// Constraints:
+
+//     m == grid.length
+//     n == grid[i].length
+//     1 <= m, n <= 300
+//     grid[i][j] is '0' or '1'.
+
+void floodFill(vector<vector<bool>> &visited, vector<vector<char>> &grid, int r, int c)
+{
+    if (r < 0 || c < 0 || r >= grid.size() || c >= grid[0].size() || visited[r][c] || grid[r][c] == '0')
+        return;
+    visited[r][c] = true;
+
+    floodFill(visited, grid, r + 1, c);
+    floodFill(visited, grid, r - 1, c);
+    floodFill(visited, grid, r, c + 1);
+    floodFill(visited, grid, r, c - 1);
+}
+
+int numIslands(vector<vector<char>> &grid)
+{
+    int cnt = 0;
+    vector<vector<bool>> visited(grid.size(), vector<bool>(grid[0].size(), false));
+    for (int i = 0; i < grid.size(); i++)
+    {
+        for (int j = 0; j < grid[0].size(); j++)
+        {
+            if (grid[i][j] == '1' && !visited[i][j])
+            {
+                cnt++;
+                floodFill(visited, grid, i, j);
+            }
+        }
+    }
+    return cnt;
+}

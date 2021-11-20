082fd24afe4f83d9f80b4714fd5e94c14276b978
Rajarshi Karan <72148786+arkayyy@users.noreply.github.com>
2021-11-21 02:35:55 +0530

    Time: 32 ms (78.66%), Space: 21.6 MB (24.10%) - LeetHub

---
+++ group-anagrams/group-anagrams.cpp
@@ -0,0 +1,60 @@
+class Solution {
+public:
+    //way to define a custom hash function for any type of data structure as the key of the unordered map
+    struct vector_hash
+    {
+        template <class T1>
+        size_t operator () (vector<T1> const &v) const
+        {
+            return hash<T1>()(v.size());    
+        }
+    };
+    
+    
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<vector<string>> ans;
+        
+        //BRUTE FORCE soln. - TLE error:-
+        
+//         unordered_map<vector<int>,vector<string>,vector_hash> mp;
+        
+//         for(auto str: strs)
+//         {
+//             vector<int> freqmap(26,0);
+//             for(auto c:str)
+//             {
+//                 freqmap[c-'a']++;
+//             }
+//             mp[freqmap].push_back(str);
+//         }
+        
+//         for(auto freqm: mp)
+//         {vector<string> temp;
+//             for(auto s: freqm.second)
+//                 temp.push_back(s);
+//          ans.push_back(temp);
+//         }
+        
+        
+        //A bit more OPTIMAL APPROACH:-
+        unordered_map<string,vector<string>> mp;
+        
+        for(auto str: strs)
+        {
+            string sortedString = str;
+            sort(sortedString.begin(),sortedString.end());
+            mp[sortedString].emplace_back(str);
+        }
+        
+        for(auto ele: mp)
+        {vector<string> t;
+            for(auto s: ele.second)
+            {
+                t.emplace_back(s);
+            }
+         ans.push_back(t);
+        }
+        
+        return ans;
+    }
+};

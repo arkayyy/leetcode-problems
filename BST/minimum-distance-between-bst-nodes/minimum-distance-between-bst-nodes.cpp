30e549a5541ea5804ec0500f64dbd4a502ae83a8
Rajarshi Karan <72148786+arkayyy@users.noreply.github.com>
2021-11-21 22:15:36 +0530

    push

--- BST/minimum-distance-between-bst-nodes/minimum-distance-between-bst-nodes.cpp
+++ BST/minimum-distance-between-bst-nodes/minimum-distance-between-bst-nodes.cpp
@@ -11,14 +11,14 @@
  */
 class Solution {
 public:
-    int findMax(TreeNode* root)
+    int findMax(TreeNode* root) //finds maximum value appearing in any subtree of root
     {
         if(!root)
             return INT_MIN;
         
         return max(root->val , max(findMax(root->left),findMax(root->right)));
     }
-    int findMin(TreeNode* root)
+    int findMin(TreeNode* root) //finds minimum value appearing in any subtree of root
     {
         if(!root)
             return INT_MAX;
@@ -31,13 +31,14 @@ public:
         
         int mini = INT_MAX;
         
-        int maxl = findMax(root->left);
-        int minr = findMin(root->right);
+        int maxl = findMax(root->left); //finding the maximum element in the left subtree
+        int minr = findMin(root->right);///finding the minimum element in the right subtree
+        //above steps are because the max element of the left subtree in a BST, will be just smaller than the root's value. and the min element of the right subtree will be just greater than the root. So the min. difference of the root's value can only be with these two.
         
         if(maxl!=INT_MIN)
             mini = min(mini, abs(root->val - maxl));
         if(minr!=INT_MAX)
-            mini = min(mini,abs(root->val - minr));
+            mini = min(mini,abs(root->val - minr)); 
         
         return min(mini,min(minDiffInBST(root->left),minDiffInBST(root->right)));
     }

e4cc53533d2f9ffc5ef02257f271749ad58d7c6e
Rajarshi Karan <72148786+arkayyy@users.noreply.github.com>
2021-11-23 00:53:41 +0530

    changes

--- Binary Trees/deepest-leaves-sum/deepest-leaves-sum.cpp
+++ Binary Trees/deepest-leaves-sum/deepest-leaves-sum.cpp
@@ -11,26 +11,51 @@
  */
 class Solution {
 public:
-    int deepestLeavesSum(TreeNode* root) {
-        queue<TreeNode*> q;
+    
+    void utilSum(TreeNode* root, int& sum, int level, int& maxlevel)
+    {
+        if(!root) return;
         
-        q.push(root);
-        int sumLevel = 0;
-        while(!q.empty())
-        {
-            int len = q.size();
-            sumLevel = 0;
-            for(int i = 0;i<len;i++)
-            {
-                TreeNode* t = q.front();
-                sumLevel+=t->val;
-                q.pop();
-                if(t->left)
-                    q.push(t->left);
-                if(t->right)
-                    q.push(t->right);
-            }
-        }
-        return sumLevel;
+        if(level==maxlevel)
+            {sum+=root->val;}
+        else if(level>maxlevel) //whenever a greater level is found in the tree, sum is reset to 0 and maxlevel is updated
+        {sum = root->val; maxlevel = level;}
+        
+        utilSum(root->left, sum, level+1, maxlevel);
+        utilSum(root->right,sum,level+1, maxlevel);
+    }
+    
+    int deepestLeavesSum(TreeNode* root) {
+
+        //BRUTE FORCE - BFS/Level Order Traversal Approach:-
+//         queue<TreeNode*> q;
+
+//         q.push(root);
+//         int sumLevel = 0;
+//         while(!q.empty())
+//         {
+//             int len = q.size();
+//             sumLevel = 0;
+//             for(int i = 0;i<len;i++)
+//             {
+//                 TreeNode* t = q.front();
+//                 sumLevel+=t->val;
+//                 q.pop();
+//                 if(t->left)
+//                     q.push(t->left);
+//                 if(t->right)
+//                     q.push(t->right);
+//             }
+//         }
+//         return sumLevel;
+        
+        
+        //MOST Optimal Approach - One Pass DFS:-
+        int sum = 0, maxlevel = INT_MIN;
+        utilSum(root,sum,0,maxlevel);
+        
+        return sum;
+        
+        
     }
 };

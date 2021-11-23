b5450c62cb261ad1e0c42ca1e8350420e26bf7e1
Rajarshi Karan <72148786+arkayyy@users.noreply.github.com>
2021-11-23 22:43:57 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (80.34%) - LeetHub

--- reverse-bits/reverse-bits.cpp
+++ reverse-bits/reverse-bits.cpp
@@ -1,16 +1,14 @@
 class Solution {
 public:
     uint32_t reverseBits(uint32_t n) {
-         unsigned rev = 0;
+        unsigned int num = 0;
         
-        for(int i=0;i<31;i++)
+        for(int i =0;i<32;i++)
         {
-            rev += n & 1;  //adding 0 if last bit of n is 0 or else 1
-            n = n>>1; //eliminating one 1 from last of n
-            rev = rev<<1; //shifting 1 from starting of rev
+            if((n&(1<<(31-i)))) // doing (31-i) to check every bit that is ith position from the end of the given number and if it is set
+                num = (1<<i)|num;  //setting that bit set ith position from the starting of the answer(initially taken as 0)
         }
         
-        rev += n & 1; //adding the last 0 or 1
-        return rev;    //reversed number
+        return num;
     }
 };

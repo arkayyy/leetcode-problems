df86aeae56f9f5552cc597334b96f52d187b4c10
Rajarshi Karan <72148786+arkayyy@users.noreply.github.com>
2021-11-28 01:22:22 +0530

    push

--- product-of-array-except-self/product-of-array-except-self.cpp
+++ product-of-array-except-self/product-of-array-except-self.cpp
@@ -9,6 +9,11 @@ public:
         vector<int> prefix(n,1);
         vector<int> suffix(n,1);
         
+        //The IDEA is:
+        //if the array is 1,2,3,4,5.
+        //the product of the whole array without 3 will be equal to product of all numbers before 3 with the product of all numbers after 3 in the array.
+        //the before and after elements product results are stored in prefix and suffix array respetively.
+        
         for(int i = 0;i<n;i++)
         {
             prefix[i] = ((i-1>=0)?prefix[i-1]:1)*nums[i];

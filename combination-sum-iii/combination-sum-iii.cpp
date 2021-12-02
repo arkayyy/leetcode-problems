ee3d8d925ed20c5347d2256f9f583d26fc09b8ea
Rajarshi Karan <72148786+arkayyy@users.noreply.github.com>
2021-12-02 15:32:34 +0530

    Time: 4 ms (28.90%), Space: 6.8 MB (19.09%) - LeetHub

---
+++ combination-sum-iii/combination-sum-iii.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    void util(int n, int k,int prev,vector<vector<int>>&ans, vector<int> curr)
+    {
+        if(n<0 || k<0) return;
+        
+        if(n==0 && k==0) {
+            ans.push_back(curr); 
+            return;
+        }
+        
+        for(int i = prev+1;i<=9;i++)
+        {
+            curr.push_back(i);
+            util(n-i, k-1, i, ans, curr);
+            curr.pop_back();
+        }
+     
+    }
+    vector<vector<int>> combinationSum3(int k, int n) {
+        vector<vector<int>> ans;
+        
+        util(n,k,0,ans,{});
+        
+        return ans;
+    }
+};

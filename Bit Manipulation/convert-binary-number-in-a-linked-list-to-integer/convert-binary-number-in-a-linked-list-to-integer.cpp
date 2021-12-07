902891de448f57ca3bb3cbc596ce2c4b4bd4ce10
Rajarshi Karan <72148786+arkayyy@users.noreply.github.com>
2021-12-07 21:40:33 +0530

    push

--- Bit Manipulation/convert-binary-number-in-a-linked-list-to-integer/convert-binary-number-in-a-linked-list-to-integer.cpp
+++ Bit Manipulation/convert-binary-number-in-a-linked-list-to-integer/convert-binary-number-in-a-linked-list-to-integer.cpp
@@ -8,37 +8,39 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-class Solution {
+class Solution
+{
 public:
-    int getDecimalValue(ListNode* head) {
+    int getDecimalValue(ListNode *head)
+    {
         int len = 0;
-        ListNode* curr = head;
-        
-        // while(curr){
-        //     len++;
-        //     curr = curr->next;
-        // }
-        
+        ListNode *curr = head;
         int ans = 0;
-        
-        // curr = head;
-        // for(int i = len-1; i>=0 ;i--)
-        // {
-        //     if(curr->val == 1)
-        //         ans = ((1<<(i))|ans);
-        //     curr = curr->next;
-        // }
 
-                //1 pass soln.:-
-        
-        while(curr)
+        // 2 pass solution:-
+
+        //         while(curr){
+        //             len++;
+        //             curr = curr->next;
+        //         }
+
+        //         curr = head;
+        //         for(int i = len-1; i>=0 ;i--)
+        //         {
+        //             if(curr->val == 1)
+        //                 ans = ((1<<(i))|ans);
+        //             curr = curr->next;
+        //         }
+
+        // 1 pass soln.:-
+
+        while (curr)
         {
-            ans = ans<<1; //shifting all bits of ans by 1 to the left
-            ans = (ans|(curr->val));//if a set bit occurs, the 0th bit of ans will be set
+            ans = ans << 1;            // shifting all bits of ans by 1 to the left
+            ans = (ans | (curr->val)); // if a set bit occurs, the 0th bit of ans will be set
             curr = curr->next;
         }
-        
-        
+
         return ans;
     }
 };

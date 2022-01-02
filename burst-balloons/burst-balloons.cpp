fe382421c2bda86990ca7f5c0a42c5ebb643d8da
Rajarshi Karan <72148786+arkayyy@users.noreply.github.com>
2022-01-02 10:01:13 +0530

    Time: 320 ms (87.93%), Space: 8.9 MB (95.85%) - LeetHub

---
+++ burst-balloons/burst-balloons.cpp
@@ -0,0 +1,27 @@
+class Solution {
+    private:
+        int maxScore = INT_MIN;
+public:
+    int maxCoins(vector<int> &iNums) {
+        int nums[iNums.size() + 2];
+        int n = 1;
+        for (int x : iNums) if (x > 0) nums[n++] = x;
+        nums[0] = nums[n++] = 1;
+
+
+        int dp[510][510] = {};
+        for (int k = 2; k < n; ++k) 
+        {
+            for (int left = 0; left < n - k; ++left)
+            {
+                int right = left + k;
+                for (int i = left + 1; i < right; ++i)
+                    dp[left][right] = max(dp[left][right],
+                         nums[left] * nums[i] * nums[right] + dp[left][i] + dp[i][right]);
+            }
+               
+        }
+
+        return dp[0][n - 1];
+    }
+};

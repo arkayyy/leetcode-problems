16df4fcf02b2f61b7476fc831cd4453f45d24a17
Rajarshi Karan <72148786+arkayyy@users.noreply.github.com>
2022-01-03 00:57:36 +0530

    Time: 66 ms (9.34%), Space: 23.1 MB (90.11%) - LeetHub

--- pairs-of-songs-with-total-durations-divisible-by-60/pairs-of-songs-with-total-durations-divisible-by-60.cpp
+++ pairs-of-songs-with-total-durations-divisible-by-60/pairs-of-songs-with-total-durations-divisible-by-60.cpp
@@ -4,7 +4,7 @@ public:
         vector<int> c(60);
         int res = 0;
         for (int t : time) {
-            res += c[(600 - t) % 60];
+            res += c[(540 - t) % 60];
             c[t % 60] += 1;
         }
         return res;

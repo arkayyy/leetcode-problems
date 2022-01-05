769e0fb78fe007198c62796e0b9c380a3c09330c
Rajarshi Karan <72148786+arkayyy@users.noreply.github.com>
2022-01-05 22:01:11 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (69.21%) - LeetHub

---
+++ 263-ugly-number/263-ugly-number.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isUgly(int n) {
+        if(n==0)
+            return false;
+        if(n==1)
+            return true;
+        if(n%2==0)
+            return isUgly(n/2);
+        else if(n%3==0)
+            return isUgly(n/3);
+        else if(n%5==0)
+            return isUgly(n/5);
+        else
+            return false;
+    }
+    
+    
+};

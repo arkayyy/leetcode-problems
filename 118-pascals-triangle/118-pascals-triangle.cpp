7a23653a3591ef62698d874b745994845bf207b1
Rajarshi Karan <72148786+arkayyy@users.noreply.github.com>
2022-01-06 09:35:19 +0530

    Time: 3 ms (11.27%), Space: 6.6 MB (32.86%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.cpp
+++ 118-pascals-triangle/118-pascals-triangle.cpp
@@ -13,8 +13,7 @@ public:
         while(numRows--)
         {
             vector<int> curr;
-            
-            
+
             curr.push_back(1);
             for(int i = 0; i<prev.size()-1;i++)
                 curr.push_back(prev[i]+prev[i+1]);

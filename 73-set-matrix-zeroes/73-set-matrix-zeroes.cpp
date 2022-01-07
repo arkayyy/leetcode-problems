9a30dd04d7c2c0e3fb01a43eadd3aad8c88d95b0
Rajarshi Karan <72148786+arkayyy@users.noreply.github.com>
2022-01-07 11:00:22 +0530

    Time: 24 ms (14.24%), Space: 13.3 MB (52.46%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -1,15 +1,18 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
+        //ALGO: We will use the first row and first column elements as the indicator for rest of the matrix. They will act like the heads of each row and column. 
+        //If any of them is zero that indicates that whole row or column will become 0.
+        
         bool firstRowZero=false, firstColZero=false;
         for(int j = 0; j<matrix[0].size(); j++)
         {
-            if(matrix[0][j]==0){ firstRowZero = true; break;}
+            if(matrix[0][j]==0){ firstRowZero = true; break;} //setting indicator if any 0 is present in the first(0 index) row itself
         }
         
         for(int  i= 0; i<matrix.size(); i++)
         {
-            if(matrix[i][0]==0) {firstColZero = true; break;}
+            if(matrix[i][0]==0) {firstColZero = true; break;} //setting indicator if any 0 is present in the first (0th) column itself
         }
         
         for(int i = 1; i<matrix.size(); i++)
@@ -30,16 +33,16 @@ public:
             {
                 if(matrix[i][0]==0 || matrix[0][j]==0)
                 {
-                    matrix[i][j]=0;
+                    matrix[i][j]=0; //making an element 0 if the head of its row or head of its column is zero.
                 }
             }
         }
         
         if(firstRowZero)
             for(int j = 0; j<matrix[0].size(); j++)
-                matrix[0][j] = 0;
+                matrix[0][j] = 0; //if the first row itself has 0, then we make the whole first row zero separately
         if(firstColZero)
             for(int i = 0; i<matrix.size(); i++)
-                matrix[i][0] = 0;
+                matrix[i][0] = 0; //if the first column itself has 0, then we make the whole first row zero separately
     }
 };

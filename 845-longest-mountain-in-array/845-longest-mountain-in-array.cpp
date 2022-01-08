2ead83e754a719d7a09dd26522c3c5583966f956
Rajarshi Karan <72148786+arkayyy@users.noreply.github.com>
2022-01-08 09:44:42 +0530

    Time: 25 ms (33.50%), Space: 18.5 MB (51.67%) - LeetHub

--- 845-longest-mountain-in-array/845-longest-mountain-in-array.cpp
+++ 845-longest-mountain-in-array/845-longest-mountain-in-array.cpp
@@ -5,22 +5,28 @@ public:
         int maxLen = INT_MIN;
         while(i<arr.size()-1)
         {
-            if(arr[i]<arr[i+1])
+            if(arr[i]<arr[i+1]) //condition for the beginning of uprising side of a mountain...
             {
                 int startMountain = i;
                 bool validMountain = false;
-                while(i<arr.size()-1 && arr[i]<arr[i+1])
+                
+                while(i<arr.size()-1 && arr[i]<arr[i+1]) //for the uprising side of the mountain
                     i++;
-                while(i<arr.size()-1 && arr[i]>arr[i+1])
-                    {i++; validMountain = true;}
+                
+                while(i<arr.size()-1 && arr[i]>arr[i+1]) //for the downgoing side of the mountain
+                    {i++; validMountain = true;} //we are making the validMountain indicator true, since a mountain can be valid only if it as both an uprising and a downgoing side.
+                
                 int endMountain = i;
+                
                 int len = endMountain - startMountain +1;
+                
                 if(validMountain)
-                    maxLen = max(maxLen, len);
+                    maxLen = max(maxLen, len); //updating maximum length of mountain stretch found till now
+                
                 continue;
             }
             i++;
         }
-        return (maxLen==INT_MIN)?0:maxLen;
+        return (maxLen==INT_MIN)?0:maxLen; //if maxLen not updated, it means no valid mountain stretch was found
     }
 };

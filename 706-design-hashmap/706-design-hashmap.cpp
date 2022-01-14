e289c2117f4d46d05b3f947cc8af07e1f032a9b7
Rajarshi Karan <72148786+arkayyy@users.noreply.github.com>
2022-01-15 02:18:23 +0530

    Time: 237 ms (24.62%), Space: 57.7 MB (58.05%) - LeetHub

--- 706-design-hashmap/706-design-hashmap.cpp
+++ 706-design-hashmap/706-design-hashmap.cpp
@@ -1,21 +1,29 @@
-struct Node {
-public:
-    int key, val;
-    Node* next;
-    Node(int k, int v, Node* n) {
-        key = k;
-        val = v;
-        next = n;
-    }
+struct Node{
+    public:
+        int key, val;
+        Node* next;
+        Node(int k, int v, Node* n)
+        {
+            key = k, val = v, next = n;
+        }
 };
+
 class MyHashMap {
+    
+    //EXPLANATION: https://leetcode.com/problems/design-hashmap/discuss/1097755/JS-Python-Java-C%2B%2B-or-(Updated)-Hash-and-Array-Solutions-w-Explanation
+    
 public:
+
+    //Reason for choice of size and mult: For the size, I wanted something that was larger than the number of possible operations (10^4), but as small as possible without risking too many collisions, and preferably prime. 
+    //The mult is just a random large multiplier, also preferably a prime.
     const static int size = 19997;
     const static int mult = 12582917;
     Node* data[size];
+    
+    
     int hash(int key) {
-        return (int)((long)key * mult % size);
-    }
+            return (int)((long)key * mult % size);
+        }
     void put(int key, int val) {
         remove(key);
         int h = hash(key);
@@ -41,3 +49,11 @@ public:
             }
     }
 };
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap* obj = new MyHashMap();
+ * obj->put(key,value);
+ * int param_2 = obj->get(key);
+ * obj->remove(key);
+ */

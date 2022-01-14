9c56e71b0249f7203811fc197ece7ea5645b4d91
Rajarshi Karan <72148786+arkayyy@users.noreply.github.com>
2022-01-15 04:33:59 +0530

    Time: 253 ms (10.52%), Space: 14.4 MB (49.35%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -1,41 +1,22 @@
 class Solution {
 public:
-//     int util(int idx, vector<int>& coins, int amount, vector<vector<int>> &dp)
-//     {
-//         if(amount<0 || idx<0) return INT_MAX-1;
-        
-//         if(amount==0) return 0;
-        
-//         if(dp[amount][idx]!=INT_MAX) return dp[amount][idx];
-        
-//         return dp[amount][idx] = min( 1+util( idx, coins, amount-coins[idx],dp) , util(idx-1,coins,amount,dp));     
-//     }
     int utilDP(vector<int>& coins, int amount)
     {
-        vector<vector<int>> dp(amount+1, vector<int>(coins.size()+1, INT_MAX-1));
-        
-        for(int j = 0; j<=coins.size(); j++)
-            dp[0][j] = 0;
-        
-        for(int i = 1; i<=amount; i++)
+        vector<int> dp(amount+1, INT_MAX-1);
+        dp[0] = 0;
+        for(int i = 1; i<=amount; i++) // looping through every possible amount 
         {
-            for(int j = 1; j<=coins.size(); j++)
+            for(int j = 0; j<coins.size(); j++) // checking each and every coin for every value of amount that needs to be summed up to
             {
-                if(coins[j-1] > i) // means the coin amount is greater than the amount that needs to be made up, so we can't include it
-                    dp[i][j] = dp[i][j-1];
-                else
-                    dp[i][j] = min(dp[i][j-1] , 1 + dp[i-coins[j-1]][j]);
-                    
+                if(coins[j]<=i) // only if the coin is of value less than the amount that needs to be summed up to
+                    dp[i] = min(dp[i], 1+dp[i-coins[j]]); 
             }
         }
-        return dp[amount][coins.size()];
+        
+        return dp[amount];
     }
     int coinChange(vector<int>& coins, int amount) {
-        vector<vector<int>> dp(amount+1,vector<int>(coins.size()+1, INT_MAX));
-        
-       // int ans =  util(coins.size()-1,coins,amount, dp);
         int ans = utilDP(coins,amount);
-        
-        return (ans==INT_MAX-1?-1:ans);
+        return (ans==INT_MAX-1? -1 : ans);
     }
 };

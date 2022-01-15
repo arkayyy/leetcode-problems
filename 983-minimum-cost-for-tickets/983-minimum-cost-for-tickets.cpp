08ff0493682ccff9b275ee9fd69edd2b091c09d4
Rajarshi Karan <72148786+arkayyy@users.noreply.github.com>
2022-01-15 05:47:58 +0530

    Time: 7 ms (31.27%), Space: 9.4 MB (86.19%) - LeetHub

--- 983-minimum-cost-for-tickets/983-minimum-cost-for-tickets.cpp
+++ 983-minimum-cost-for-tickets/983-minimum-cost-for-tickets.cpp
@@ -7,18 +7,18 @@ public:
         if(days[idx]<=dayCoveredTo)
             return util(idx+1, dayCoveredTo, days, costs, dp);
         
-        if(dp[idx]!=-1) return dp[idx];
+        if(dp[days[idx]]!=-1) return dp[days[idx]];
         
         int mini = INT_MAX;
         mini = min(mini, costs[0]+util(idx+1, days[idx]+0,days,costs,dp)); // for 1-day pass
         mini = min(mini, costs[1]+util(idx+1, days[idx]+6,days,costs,dp)); // for 7-day pass
         mini = min(mini, costs[2]+util(idx+1, days[idx]+29,days,costs,dp)); //for 30-days pass
         
-        return dp[idx] = mini; //memoizing solution
+        return dp[days[idx]] = mini; //memoizing solution
     }
 
     int mincostTickets(vector<int>& days, vector<int>& costs) {
-        vector<int> dp(days.size(), -1);
+        vector<int> dp(380, -1);
         return util(0,0,days,costs,dp);
         
     }

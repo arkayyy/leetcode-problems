41f655b3b8220f7aacd348702381c5aa5e80eb65
Rajarshi Karan <72148786+arkayyy@users.noreply.github.com>
2022-01-15 07:16:09 +0530

    Time: 5 ms (10.44%), Space: 6.6 MB (51.77%) - LeetHub

--- 58-length-of-last-word/58-length-of-last-word.cpp
+++ 58-length-of-last-word/58-length-of-last-word.cpp
@@ -3,7 +3,7 @@ public:
     int lengthOfLastWord(string s) {
         int len = 0;
         int i = s.size()-1;
-        while(s[i]==' ')
+        while(s[i]==' ') //for all spaces at last
             i--;
         while(i>=0 && s[i]!=' ')
             {len++; i--;}

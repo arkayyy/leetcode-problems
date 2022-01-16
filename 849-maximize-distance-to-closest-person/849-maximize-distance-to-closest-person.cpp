1b30df7cc03e73ae4d093001f57b784400d8eb9d
Rajarshi Karan <72148786+arkayyy@users.noreply.github.com>
2022-01-16 14:37:55 +0530

    Time: 16 ms (48.56%), Space: 17 MB (93.10%) - LeetHub

--- 849-maximize-distance-to-closest-person/849-maximize-distance-to-closest-person.cpp
+++ 849-maximize-distance-to-closest-person/849-maximize-distance-to-closest-person.cpp
@@ -1,6 +1,14 @@
 class Solution {
 public:
     int maxDistToClosest(vector<int>& seats) {
+        //INTUITION: Same as exam room problem, we have three types of gaps (consecutive empty seats) here. 
+        //1. At the starting,  before the firsmost occupied seat
+        //2. In the middle of two occupied seats
+        //3. At the end, after the lastmost occupied seat
+        //Thus we need to calculate in which case the distance produced would be maximum
+        //Also remember that in a series of empty chairs between two occupants, in order to maximise distance from both of them we sit at the middle.
+        
+        //ALGO:We find the starting gap, middle gap (maximum of all the middle gaps) and ending gap. We check which one produces maximum distance.
         int prev=0;
         int maxGapFirst = INT_MIN, maxGapBetween = INT_MIN, maxGapEnd = INT_MIN;
         int a=-1,b=-1;
@@ -14,7 +22,6 @@ public:
         }
         int start = prev;
         maxGapFirst = prev - 0;
-        //cout<<maxGapFirst<<endl;
         
         for(int i=start+1; i<seats.size();i++)
         {
@@ -26,24 +33,25 @@ public:
                 prev = i;
             }
         }
-        //cout<<maxGapBetween<<endl;
+        //calculating the distance produced if the person is seated right in the middle of the two occupants
+        int distForMaxGapBetween;
         if(maxGapBetween&1)
-            maxGapBetween = max(abs(((a+b)>>1)-a), abs(((a+b)>>1)-b));
+            distForMaxGapBetween = max(abs(((a+b)>>1)-a), abs(((a+b)>>1)-b));
         else
-            maxGapBetween= max(abs(((a+b)>>1)-a)-1, abs(((a+b)>>1)-b)-1);
+            distForMaxGapBetween= max(abs(((a+b)>>1)-a)-1, abs(((a+b)>>1)-b)-1);
         
         maxGapEnd = seats.size()-1 - prev;
-        //cout<<maxGapEnd<<endl;
         
-        int maxGap = max({maxGapFirst, maxGapBetween, maxGapEnd});
+        
+        int maxGap = max({maxGapFirst, distForMaxGapBetween, maxGapEnd});
         
         
         if(maxGap == maxGapFirst) return start-0;
         
         if(maxGap == maxGapEnd) return seats.size()-1-prev;
-        cout<<a<<"->"<<b<<endl;
-        //if(maxGap == maxGapBetween) 
-        return maxGapBetween;
+    
+    
+        return distForMaxGapBetween;
         
     }
 };

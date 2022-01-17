4243a491a184fa4ceffc2b7c9e95301ddfec91bb
Rajarshi Karan <72148786+arkayyy@users.noreply.github.com>
2022-01-18 02:52:48 +0530

    Time: 20 ms (51.58%), Space: 18.4 MB (5.01%) - LeetHub

---
+++ 57-insert-interval/57-insert-interval.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public://INTUITION: Insert the new interval and then merge all the intervals together
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        vector<vector<int>> ans(intervals.begin(),intervals.end());
+        ans.push_back(newInterval); //inserting new interval
+        sort(ans.begin(),ans.end());
+        
+        vector<vector<int>> finans;
+        
+        //merging all intervals:-
+        int left = ans[0][0], right = ans[0][1];
+        for(int i = 1; i<ans.size(); i++)
+        {
+            
+            
+            if(ans[i][0]<=right && ans[i][1]>right)
+                right = ans[i][1];
+            
+            else if(ans[i][0]>=left && ans[i][1]<=right) continue;
+                
+            else if(ans[i][0]>right) {finans.push_back({left,right}); left = ans[i][0]; right = ans[i][1];}
+        }
+        finans.push_back({left,right});
+        
+        return finans;
+        
+    }
+};

d1650ad51d21c015079fc1c454df9da0870e014d
Rajarshi Karan <72148786+arkayyy@users.noreply.github.com>
2022-01-18 02:54:38 +0530

    Time: 57 ms (5.10%), Space: 18.5 MB (5.01%) - LeetHub

--- 57-insert-interval/57-insert-interval.cpp
+++ 57-insert-interval/57-insert-interval.cpp
@@ -7,13 +7,11 @@ public://INTUITION: Insert the new interval and then merge all the intervals tog
         
         vector<vector<int>> finans;
         
-        //merging all intervals:-
+        //merging all intervals:- (FOR EXPLANATION see Merge Intervals 56 on Leetcode, my YT channel and GitHub Repo - leetcode-problems)
         int left = ans[0][0], right = ans[0][1];
         for(int i = 1; i<ans.size(); i++)
-        {
-            
-            
-            if(ans[i][0]<=right && ans[i][1]>right)
+        {   
+            if(ans[i][0]<=right && ans[i][1]>right) 
                 right = ans[i][1];
             
             else if(ans[i][0]>=left && ans[i][1]<=right) continue;

524b886020fd35391b81a3bd7d86f05d960a0994
Rajarshi Karan <72148786+arkayyy@users.noreply.github.com>
2022-01-17 23:58:00 +0530

    Time: 9 ms (14.19%), Space: 7.7 MB (56.68%) - LeetHub

---
+++ 46-permutations/46-permutations.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    //INTUITION: simple backtracking, once adding an element to the current permutation and marking it visited so next time it won't be added in the same permutation.
+    void permutate( vector<int> & curr, vector<vector<int>>& ans,  vector<bool>&visited, vector<int> &nums)
+    {
+        if(curr.size()>nums.size()) return;
+        if(curr.size()==nums.size()) {
+            ans.push_back(curr);
+            return;
+        }
+        for(int i = 0; i<nums.size(); i++) //we go through all elements of the array and we include any element that has not already been visited/added to the combination before.
+        {
+            if(!visited[i])
+            {
+                visited[i] = true;
+                curr.push_back(nums[i]);
+                permutate(curr,ans,visited,nums);
+                //for backtracking:-
+                visited[i] = false;
+                curr.pop_back();
+            }
+        }
+     }
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> ans;
+        vector<bool> visited(nums.size(),false);
+        vector<int> curr;
+        permutate(curr,ans,visited,nums);
+        return ans;
+    }
+};

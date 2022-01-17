1d9d34a0e7ef44101bcbb3cc9bb91eecc8c5d304
Rajarshi Karan <72148786+arkayyy@users.noreply.github.com>
2022-01-18 00:44:25 +0530

    Time: 0 ms (100.00%), Space: 7.8 MB (56.68%) - LeetHub

--- 46-permutations/46-permutations.cpp
+++ 46-permutations/46-permutations.cpp
@@ -1,6 +1,11 @@
 class Solution {
 public:
     //INTUITION: simple backtracking, once adding an element to the current permutation and marking it visited so next time it won't be added in the same permutation.
+    
+    //NOTE:  If we have repeating elements in array, this won't work since different indexes can contain same elements here, so ultimately there maybe two same permutations present in the final answer. 
+    //In that case we use simple backtracking with a map...
+    //...once adding an element to the current permutation and decrease its frequency in the map so next time it won't be added in the same permutation if its frequency <=0.
+
     void permutate( vector<int> & curr, vector<vector<int>>& ans,  vector<bool>&visited, vector<int> &nums)
     {
         if(curr.size()>nums.size()) return;

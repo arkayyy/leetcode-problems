ade4d2a4e5825b1b673bc13e207751511c94a75f
Rajarshi Karan <72148786+arkayyy@users.noreply.github.com>
2022-01-19 18:20:50 +0530

    Time: 12 ms (96.92%), Space: 19.6 MB (88.07%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -1,6 +1,12 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
+        //Moore's voting algorithm:-
+        
+        //INTUITION: since the major element is present more than half length of array, its net count will always be more than all the other elements count combined...
+        
+        //AALGO: We start with the first element and count as 1, as soon as a dissimilar element is found we decrease the count...
+        //...as our count reaches 0, we reset the count to 1 and set the new element as candidate
         if(nums.size()==1) return nums[0];
         int cand = nums[0], cnt = 1;
         for(int i = 1; i<nums.size(); i++)

0f2e7ad5ca9e7f579fc1ef9ebc341b5b6ba2a709
Rajarshi Karan <72148786+arkayyy@users.noreply.github.com>
2022-01-20 04:24:48 +0530

    Time: 184 ms (96.17%), Space: 46.7 MB (71.98%) - LeetHub

--- 493-reverse-pairs/493-reverse-pairs.cpp
+++ 493-reverse-pairs/493-reverse-pairs.cpp
@@ -1,47 +1,43 @@
 class Solution {
 public:
-    //INTUITION: If we employ merge sort here, we can do something similar to count inversions...
-    
-    //ALGO: In count inversions, we check if arr1[p1]>arr2[p2] then it's considered an inversion
-    //Here we simply check if arr1[p1]>2*arr2[p2] and consider that as reverse pair..so all elements in arr1 appearing after p1 also form reverse pairs with p2th element of arr2..
-    int merge(int i, int mid, int j, vector<int>&temp, vector<int>& arr)
+    int merge(int i, int mid, int j, vector<int>& temp, vector<int>& nums)
     {
-        int x = i;
-        int y = mid+1;
-        int z = i; //will be used for storing elems in temp array (in fully sorted fashion)
+        int p1 = i, p2 = mid+1;
+        int p3 = i; 
         int revc = 0;
-        //counting reverse pairs
-        for(int p = i;p<=mid;p++){
-            while(y<=j && (long)arr[p]>2*(long)arr[y]) y++;
-            revc+=y-(mid+1);
-        }
-       
-        //sorting using original merge method -----
-        x = i, y = mid+1, z = i;
-        while(x<=mid && y<=j)
+        //counting reverse pairs:-
+        while(p1<=mid && p2<=j)
         {
-            if(arr[x]>arr[y]) temp[z++] = arr[y++]; 
-            else temp[z++] = arr[x++];
+            if((long)nums[p1]>(long)2*nums[p2]) {revc+=(mid+1-p1); p2++;}
+            else p1++;
         }
-        while(x<=mid) temp[z++] = arr[x++];
-        while(y<=j) temp[z++] = arr[y++];
         
-        for(int p = i; p<=j;p++) arr[p] = temp[p];
-        //----merging ends
+        //merging the array
+        p1 = i, p2 = mid+1;
+        while(p1<=mid && p2<=j)
+        {
+            if(nums[p1]>nums[p2]) temp[p3++] = nums[p2++];
+            else temp[p3++] = nums[p1++];
+        }
+        while(p1<=mid)
+            temp[p3++] = nums[p1++];
+        while(p2<=j) 
+            temp[p3++] = nums[p2++];
+        
+        for(int x = i; x<=j; x++) nums[x] = temp[x];
+        
         return revc;
     }
-    
-    int mergeSort(int i, int j, vector<int>&temp, vector<int>& nums)
+    int mergeSort(int i, int j, vector<int>& temp, vector<int>& nums)
     {
         if(i>=j) return 0;
-        int mid = i+((j-i)>>1);
         int revc = 0;
-        revc+=mergeSort(i,mid,temp,nums);
-        revc+=mergeSort(mid+1,j,temp,nums);
+        int mid = i+((j-i)>>1);
+        revc+=mergeSort(i,mid,temp, nums);
+        revc+=mergeSort(mid+1,j, temp,nums);
         revc+=merge(i,mid,j,temp,nums);
         return revc;
     }
-    
     int reversePairs(vector<int>& nums) {
         vector<int> temp(nums.size());
         return mergeSort(0,nums.size()-1,temp,nums);

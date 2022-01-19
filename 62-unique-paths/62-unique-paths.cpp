398ab4cb0dc83cfe05c9037f56160c71ea7d5635
Rajarshi Karan <72148786+arkayyy@users.noreply.github.com>
2022-01-20 02:35:07 +0530

    Time: 8 ms (5.01%), Space: 6.5 MB (39.58%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,5 +1,10 @@
 class Solution {
 public:
+    //INTUITION: We can go in two directions, so at every point in the matrix we see that in both the directions how many total ways are present to reach the ending point...
+    
+    //ALGO: we do kind of dfs and reach the ending point, so at the cells just before the ending point would store the no. of paths to ending...
+    //then we backtrack to the earlier point and add that many ways with more ways possible from current node. 
+    //In DP we memoize the total no. of ways possible from the current node in both directions(down and right).
     int util(int i, int j, int& ans, vector<vector<bool>>& visited, vector<vector<int>> &dp)
     {
         if(i==visited.size()-1 && j==visited[0].size()-1) {ans++; return 1;}
@@ -11,7 +16,6 @@ public:
         {
             visited[i+1][j] = true;
             int t=util(i+1,j,ans, visited,dp);
-           // if(dp[i+1][j]==-1) dp[i+1][j] = t;
             res+=t;
             visited[i+1][j] = false;
         }
@@ -20,11 +24,10 @@ public:
         {
             visited[i][j+1] = true;
             int t=util(i,j+1,ans,visited,dp);
-            //if(dp[i][j+1]==-1) dp[i][j+1] = t;
             res+=t;
             visited[i][j+1] = false;
         }
-        return dp[i][j] = res;
+        return dp[i][j] = res; //memoization - 
     }
     int uniquePaths(int m, int n) {
         vector<vector<bool>> visited(m,vector<bool>(n,false));

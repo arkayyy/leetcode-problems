3d3ffd9424da1603ff2ff86e7d51cbea8fe80eac
Rajarshi Karan <72148786+arkayyy@users.noreply.github.com>
2022-01-20 07:30:36 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (88.04%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -2,11 +2,11 @@ class Solution {
 public:
     int uniquePaths(int m, int n) { 
         int total = m+n-2;
-        int d = m-1;
+        int r = n-1;
         double ans = 1;
-        for(int i = 1; i<=d; i++)
+        for(int i = 1; i<=r; i++)
         {
-            ans = ans*(total-d+i)/i;
+            ans = ans*(total-r+i)/i;
         }
         return (int)ans;
     }

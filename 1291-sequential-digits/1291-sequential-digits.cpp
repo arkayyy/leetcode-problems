74ab46dd7ae40997fbdcd2a507d90e966c248d23
Rajarshi Karan <72148786+arkayyy@users.noreply.github.com>
2022-01-23 13:25:10 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (36.79%) - LeetHub

--- 1291-sequential-digits/1291-sequential-digits.cpp
+++ 1291-sequential-digits/1291-sequential-digits.cpp
@@ -1,33 +1,25 @@
 class Solution {
 public:
     vector<int> sequentialDigits(int low, int high) {
-        vector<int> curr({12,23,34,45,56,67,78,89});
-        int idx = 1;
-        int tenss = 100;
+        queue<int> q;
+        for(int i = 1; i<=9; i++)
+            q.push(i);
+        
         vector<int> ans;
-        for(auto n: curr)
-            if(n>=low && n<=high)
-                ans.push_back(n);
-        bool flag =  false;
-        while(idx<curr.size())
+        
+        while(!q.empty())
         {
-            int j = idx;
-            int tens = tenss;
-            int m = 1;
-            while(j<curr.size()) 
-            {   
-                curr[j] = (curr[j]+(tens*(m++)));
-                if(curr[j]>=low && curr[j]<=high)
-                    ans.push_back(curr[j]);
-                if(curr[j]>high)
-                {flag = true; break;}
-                j++;
-            }
-            if(flag) break;
-            idx++;
-            tenss*=10;
+            int t = q.front();
+            q.pop();
+            if(t>high) break;
             
+            if(t>=low) ans.push_back(t);
+            
+            int lastDigit = t%10;
+            if(lastDigit<9)
+                q.push((t*10)+(lastDigit+1));
         }
+        
         return ans;
     }
 };

b935121e33e67c3ad7072a11c490a4c4e9bbb4a5
Rajarshi Karan <72148786+arkayyy@users.noreply.github.com>
2022-01-23 14:13:33 +0530

    Time: 7 ms (7.14%), Space: 6.2 MB (76.43%) - LeetHub

--- 1291-sequential-digits/1291-sequential-digits.cpp
+++ 1291-sequential-digits/1291-sequential-digits.cpp
@@ -13,11 +13,11 @@ public:
             q.pop();
             if(t>high) break;
             
-            if(t>=low) ans.push_back(t);
+            if(t>=low) ans.push_back(t); 
             
-            int lastDigit = t%10;
-            if(lastDigit<9)
-                q.push((t*10)+(lastDigit+1));
+            int lastDigit = t%10; //extracting last digit
+            if(lastDigit<9) //if last digit is =9, then on doing 9+1 we get 10 which can't be accomodated in sinle digit space
+                q.push((t*10)+(lastDigit+1)); //storing element for next sequence
         }
         
         return ans;

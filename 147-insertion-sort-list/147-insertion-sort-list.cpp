ec5418bcc49610b5834f94af72d5fd5ba942442b
Rajarshi Karan <72148786+arkayyy@users.noreply.github.com>
2022-01-26 14:22:11 +0530

    Time: 50 ms (42.22%), Space: 9.6 MB (76.60%) - LeetHub

--- 147-insertion-sort-list/147-insertion-sort-list.cpp
+++ 147-insertion-sort-list/147-insertion-sort-list.cpp
@@ -11,21 +11,24 @@
 class Solution {
 public:
     ListNode* insertionSortList(ListNode* head) {
-        ListNode* preHead = new ListNode(-1);
-        ListNode* prev = preHead;
+        ListNode* preHead = new ListNode(0);
         ListNode* curr = head;
         ListNode* next = NULL;
+        ListNode* prev = preHead;
         while(curr)
         {
             next = curr->next;
             while(prev->next && prev->next->val<curr->val)
-                prev = prev->next;
-            curr->next = prev->next;///
+                prev = prev->next; //will stop when prev->next >= val, so we know that the curr node is to be inserted in between prev and prev->next
+        
+            //inserting curr in between prev and prev->next
+            curr->next = prev->next;
             prev->next = curr;
+            //reinitialising prev for the next element's traversal iteration
             prev = preHead;
-            curr = next;
+            curr = next; //curr moved to the next that we had stored before, because we have to follow the orginal sequence of the unsorted array.
         }
+        
         return preHead->next;
     }
-    
 };

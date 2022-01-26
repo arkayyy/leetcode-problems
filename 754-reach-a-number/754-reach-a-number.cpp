260fc5b1b57dcce8b4e25e4fd82768aec5cd719c
Rajarshi Karan <72148786+arkayyy@users.noreply.github.com>
2022-01-26 10:26:23 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (77.36%) - LeetHub

--- 754-reach-a-number/754-reach-a-number.cpp
+++ 754-reach-a-number/754-reach-a-number.cpp
@@ -2,35 +2,33 @@ class Solution {
 public:
     int reachNumber(int target) {
         target = abs(target);
-        long long lo = 1, hi = target; // moves: [1,target]
+        long long lo = 1, hi = target;
         long long pos;
-        long long minSteps = INT_MAX;
+        long long minSteps;
         while(lo<=hi)
         {
-            long long mid = lo+((hi-lo)>>1);
-            if( ((mid*(mid+1))>>1) >= target)
+            long long mid = (lo+hi)/2;
+            long long dist = (mid*(mid+1))/2;
+            
+            if(dist>=target)
             {
-                pos = ((mid*(mid+1))>>1); // the pos
-                minSteps = mid; //mid -> no. of moves
+                pos= dist;
+                minSteps = mid;
                 hi = mid-1;
             }
             else
                 lo = mid+1;
         }
         
-        long long diff = pos-target; 
+        long long diff = pos - target;
         
-        if(diff&1)
+        if(diff&1)//diff%2!=0 //diff = 0
         {
             if(minSteps&1)
-            {
                 minSteps+=2;
-            }
             else
                 minSteps+=1;
         }
-        
         return minSteps;
-        
     }
 };

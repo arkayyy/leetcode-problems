64d4dc86870fc53eb5e43428cec94f3ea85dfea4
Rajarshi Karan <72148786+arkayyy@users.noreply.github.com>
2022-01-28 12:22:55 +0530

    Time: 753 ms (27.96%), Space: 18.7 MB (30.09%) - LeetHub

--- 127-word-ladder/127-word-ladder.cpp
+++ 127-word-ladder/127-word-ladder.cpp
@@ -8,15 +8,16 @@ public:
         
         queue<pair<string,int>> q;  //contains word and the level associated with it
         q.push({beginWord,1});
-        unordered_set<string> visited;
         
-        int level = 1;
-        // int minLevel = INT_MAX;
+        unordered_set<string> visited;//contains visited words in a level ONLY
+        
+        int level = 1; //initially globally we are present at level 1
         
         while(!q.empty())
         {
-            string last = q.front().first;
-            int currLevel = q.front().second;
+            string last = q.front().first; //last word
+            
+            int currLevel = q.front().second; 
             q.pop();
             if(currLevel>level)
             {
@@ -25,19 +26,20 @@ public:
                // if(currLevel>minLevel) break;
                 level = currLevel;
             }
+            //We keep swapping each character in last word to any one from 'a' to 'z' one by one
             for(int i = 0;i<last.size(); i++)
             {
                 string news = last;
                 for(char c = 'a'; c<='z'; c++)
                 {
                     news[i] = c;
-                    if(wordSet.find(news)!=wordSet.end())
+                    if(wordSet.find(news)!=wordSet.end()) //if new reformed word is yet to be found
                     {
                         visited.insert(news);
-                        if(news==endWord)
-                        {return currLevel+1;}
+                        if(news==endWord) //the first time we reach the target word will always be by shortest/min path itself...BFS RULE!!!
+                            return currLevel+1; 
                         else
-                            q.push({news,currLevel+1});
+                            q.push({news,currLevel+1}); //otherwise storing new word for next level
                     }
                 }
             }

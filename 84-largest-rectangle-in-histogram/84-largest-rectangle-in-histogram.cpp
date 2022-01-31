f34de20c5015a1acfd42c9e0d8689ceb6c9ed8ea
Rajarshi Karan <72148786+arkayyy@users.noreply.github.com>
2022-01-31 20:18:21 +0530

    Time: 231 ms (30.01%), Space: 61.6 MB (80.50%) - LeetHub

--- 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
@@ -1,17 +1,22 @@
 class Solution {
 public:
     int largestRectangleArea(vector<int>& heights) {
+        
+        //----------------------------------APPROACH 2-----------------------------------//
+        
+        //MOST OPTIMAL TC:O(N)+O(N) SC:O(N) Single Pass Approach:-
         stack<int> st;
         int maxArea = 0;
         
         for(int i = 0; i<=heights.size(); i++)
         {
-            while(!st.empty() && (i==heights.size() || heights[i]<=heights[st.top()])) // will loop until the elements in stack are in increasing order from top to bottom while including heights[i]
+            //INTUITITON: for the current height we assess all the bigger heights on the left and find area formed by current height with all such bigger heights on its left
+            while(!st.empty() && (i==heights.size() || heights[i]<=heights[st.top()])) 
             {
                 int height = heights[st.top()];
                 st.pop();
                 int width;
-                if(st.empty()) width = i;
+                if(st.empty()) width = i; 
                 else width = i-st.top()-1;
                 
                 maxArea = max(maxArea, height*width);

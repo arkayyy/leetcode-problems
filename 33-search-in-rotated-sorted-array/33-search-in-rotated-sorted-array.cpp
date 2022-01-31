415286fe4d21f7d788acadb9e034c723c30a3bc0
Rajarshi Karan <72148786+arkayyy@users.noreply.github.com>
2022-01-31 12:05:20 +0530

    Time: 14 ms (6.24%), Space: 11.2 MB (28.81%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -8,14 +8,14 @@ public:
             if(nums[mid]==target) return mid;
             if(nums[lo]<=nums[mid])
             {
-                if(target>=nums[lo] && target<=nums[mid])
+                if(target>=nums[lo] && target<nums[mid])
                     hi = mid-1;
                 else
                     lo = mid+1;
             }
             else
             {
-                if(target>=nums[mid] && target<=nums[hi])
+                if(target>nums[mid] && target<=nums[hi])
                     lo = mid+1;
                 else
                     hi = mid-1;

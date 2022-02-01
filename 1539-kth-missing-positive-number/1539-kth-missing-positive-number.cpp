2e9507509fba7ce629bbb2abff895eaf54d3a625
Rajarshi Karan <72148786+arkayyy@users.noreply.github.com>
2022-02-01 08:06:08 +0530

    Time: 0 ms (100.00%), Space: 9.6 MB (67.66%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
@@ -7,8 +7,9 @@ public:
         while(lo<=hi)
         {
             int mid = lo+((hi-lo)>>1); 
-            if(arr[mid]-mid-1 < k) lo = mid+1; //arr[mid]-(mid+1) signifies the total number of missing number that appears before arr[mid]
-            else hi = mid-1;
+            if(arr[mid]-(mid+1) < k) lo = mid+1; //arr[mid]-(mid+1) signifies the total number of missing number that appears before arr[mid]..because (mid+1) was the expected value at the index mid. 
+            //But if the number of missing elements before mid index is less than k, we need to shift to searching in the right section of our range of bin search
+            else hi = mid-1; //if kth missing lies in the first half..we shift there
         }
         return lo+k;
     }

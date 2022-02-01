b316a962689ee0dc93eddfb8a7311f652881f446
Rajarshi Karan <72148786+arkayyy@users.noreply.github.com>
2022-02-01 22:10:02 +0530

    Time: 12 ms (62.66%), Space: 7.8 MB (47.44%) - LeetHub

--- 264-ugly-number-ii/264-ugly-number-ii.cpp
+++ 264-ugly-number-ii/264-ugly-number-ii.cpp
@@ -30,11 +30,13 @@ public:
         int p2 = 0, p3 = 0, p5 = 0;
         for(int i = 1; i<n;i++)
         {
+            //instead of using a priority queue, at every step we store the minimum number that can be produced by multiplying the results as below
             dp[i] = min(dp[p2]*2, min(dp[p3]*3, dp[p5]*5));
+            //whichever pointer produces the minimum element we forward that (can be multiple pointers)
             if(dp[i]==dp[p2]*2) p2++;
             if(dp[i]==dp[p3]*3) p3++;
             if(dp[i]==dp[p5]*5) p5++;
         }
-        return dp[n-1];
+        return dp[n-1]; 
     }
 };

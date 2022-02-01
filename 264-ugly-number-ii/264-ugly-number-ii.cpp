ddff8c09fb90169e958c070cebf4e0d868726f07
Rajarshi Karan <72148786+arkayyy@users.noreply.github.com>
2022-02-01 22:02:48 +0530

    Time: 18 ms (43.94%), Space: 7.6 MB (77.29%) - LeetHub

--- 264-ugly-number-ii/264-ugly-number-ii.cpp
+++ 264-ugly-number-ii/264-ugly-number-ii.cpp
@@ -1,24 +1,40 @@
 class Solution {
 public:
     int nthUglyNumber(int n) {
-        vector<long long> primes{2,3,5};
-        vector<long long> arr(n,-1);
-        priority_queue<long long, vector<long long>, greater<long long>> pq;
-        pq.push(1);
-        long long i = 0;
         
-        //we use a priority queue in order to retreive all the elements in ascending fashion
-        while(!pq.empty() && arr[n-1]==-1)
+        //---------------APPROACH 1 - TC:O(NK) SC:O(N)-----------------//
+//         vector<long long> primes{2,3,5};
+//         vector<long long> arr(n,-1);
+//         priority_queue<long long, vector<long long>, greater<long long>> pq;
+//         pq.push(1);
+//         long long i = 0;
+        
+//         //we use a priority queue in order to retreive all the elements in ascending fashion
+//         while(!pq.empty() && arr[n-1]==-1)
+//         {
+//             long long top = pq.top();
+//             pq.pop();
+//             arr[i++] = top;
+//             for(auto p:primes) //multiplying current element will all available primes
+//                 pq.push(top*p);
+//             while(!pq.empty() && pq.top()==top) //removing duplicate elements
+//                 pq.pop();
+//         }
+        
+//         return arr[n-1];
+        
+        
+        //-----------------APPROACH 2 - DP -  TC: O(N) SC:O(N)--------------//
+        vector<int> dp(n);
+        dp[0] = 1;
+        int p2 = 0, p3 = 0, p5 = 0;
+        for(int i = 1; i<n;i++)
         {
-            long long top = pq.top();
-            pq.pop();
-            arr[i++] = top;
-            for(auto p:primes) //multiplying current element will all available primes
-                pq.push(top*p);
-            while(!pq.empty() && pq.top()==top) //removing duplicate elements
-                pq.pop();
+            dp[i] = min(dp[p2]*2, min(dp[p3]*3, dp[p5]*5));
+            if(dp[i]==dp[p2]*2) p2++;
+            if(dp[i]==dp[p3]*3) p3++;
+            if(dp[i]==dp[p5]*5) p5++;
         }
-        
-        return arr[n-1];
+        return dp[n-1];
     }
 };

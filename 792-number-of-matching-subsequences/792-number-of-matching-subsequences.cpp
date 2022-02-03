ef9e8b599038cd21a8041feebae35c56b376584d
Rajarshi Karan <72148786+arkayyy@users.noreply.github.com>
2022-02-04 01:34:38 +0530

    Time: 350 ms (44.10%), Space: 50.2 MB (44.75%) - LeetHub

--- 792-number-of-matching-subsequences/792-number-of-matching-subsequences.cpp
+++ 792-number-of-matching-subsequences/792-number-of-matching-subsequences.cpp
@@ -10,14 +10,14 @@ public:
         int cnt = 0;
         for(auto w: words)
         {
-            bool flag = false;
+            bool flag = false; //willl turn true if all characters of this word are not available in s for subsequence
             fill(visited.begin(), visited.end(),false);
-            int lastIndex = -1;
+            int lastIndex = -1; //used to store the index in s used for previous character of the subsequence at every step
             for(auto ch: w)
             {
-                bool f = false;
+                bool f = false; //will turn true if the an index for the current character of w is available for being considered (in s)
                 for(auto i: mp[ch])
-                    if(!visited[i] && i>lastIndex)
+                    if(!visited[i] && i>lastIndex) //if the index is not visited and the index is greater than the index used in s by previous character of the subsequence
                     {
                         visited[i] = true;
                         f = true;

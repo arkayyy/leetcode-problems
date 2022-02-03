0c0f3c364aeddcc35cba32030920136d9a9e99c5
Rajarshi Karan <72148786+arkayyy@users.noreply.github.com>
2022-02-03 22:29:19 +0530

    Time: 0 ms (100.00%), Space: 6.9 MB (35.39%) - LeetHub

--- 316-remove-duplicate-letters/316-remove-duplicate-letters.cpp
+++ 316-remove-duplicate-letters/316-remove-duplicate-letters.cpp
@@ -9,7 +9,7 @@ public:
             freq[ch]--;
             if(visited[ch]) continue;
             // ch < res.back() -> because we need the lexicographically least subsequence containing all unique letters in the string, so we keep popping until we get lexo least subsequence
-            // freq[res.back()] > 0 -> because if freq[res.back()]==0 we know that no more of this character will occur next in the string, so we can't pop this
+            // freq[res.back()] > 0 -> because if freq[res.back()]==0 we know that no more of this character will occur next in the string, so we can't pop this, it's already part of lexo least subseq
             while(ch < res.back() && freq[res.back()]) {visited[res.back()] = false; res.pop_back();}
             res+=ch;
             visited[ch] = true;

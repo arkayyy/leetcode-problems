43eeeac74a26c3593efbd197b6d84316a7a5cd6b
Rajarshi Karan <72148786+arkayyy@users.noreply.github.com>
2022-02-03 22:28:43 +0530

    Time: 0 ms (100.00%), Space: 6.9 MB (35.39%) - LeetHub

---
+++ 316-remove-duplicate-letters/316-remove-duplicate-letters.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string removeDuplicateLetters(string s) {
+        vector<int> freq(256,0); vector<bool> visited(256,false);
+        for(auto ch: s) freq[ch]++;
+        string res = "0";
+        for(auto ch: s)
+        {
+            freq[ch]--;
+            if(visited[ch]) continue;
+            // ch < res.back() -> because we need the lexicographically least subsequence containing all unique letters in the string, so we keep popping until we get lexo least subsequence
+            // freq[res.back()] > 0 -> because if freq[res.back()]==0 we know that no more of this character will occur next in the string, so we can't pop this
+            while(ch < res.back() && freq[res.back()]) {visited[res.back()] = false; res.pop_back();}
+            res+=ch;
+            visited[ch] = true;
+        }
+        return res.substr(1);
+    }
+};

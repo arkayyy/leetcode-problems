bc4388ba2a6eb668677817ac23a2f206a70a25ca
Rajarshi Karan <72148786+arkayyy@users.noreply.github.com>
2022-02-04 10:52:58 +0530

    Time: 272 ms (12.48%), Space: 83.8 MB (86.74%) - LeetHub

--- 525-contiguous-array/525-contiguous-array.cpp
+++ 525-contiguous-array/525-contiguous-array.cpp
@@ -2,15 +2,20 @@ class Solution {
 public:
     int findMaxLength(vector<int>& nums) {
         int sum = 0;
-        int ans = 0;
         unordered_map<int,int> mp;
-        for(int i = 0; i<nums.size(); i++)
+        
+        int ans = 0;
+        mp[0] = 0;
+        for(int i = 1; i<=nums.size(); i++)
         {
-            sum+=((nums[i]==1)?1:-1);
-            if(sum==0) ans = i+1; //if sum is 0 means the first element of a subarray is being taken
-            else if(mp.find(sum)==mp.end()) mp[sum] = i; //this sum has not been encountered before so we associate it with current index
-            else ans = max(ans, i-mp[sum]); //if this sum has been already encountered before means that all the elements between current index and that index sum is 0(i.e. same no. of 0(-1)'s and 1's).
-        }
+            sum+=(nums[i-1]==1)?1:-1;
+            if(mp.find(sum)!=mp.end())
+            {
+                ans = max(ans, i-mp[sum]);
+            }
+            else
+            mp[sum] = i;
+        }          
         return ans;
     }
 };

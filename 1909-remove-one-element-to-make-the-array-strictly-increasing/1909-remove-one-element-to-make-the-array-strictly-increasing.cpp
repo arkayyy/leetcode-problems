9387ffc268346c43e9567d7b7be7f13d00d24775
Rajarshi Karan <72148786+arkayyy@users.noreply.github.com>
2022-02-04 14:34:24 +0530

    Time: 17 ms (20.88%), Space: 9.9 MB (85.15%) - LeetHub

--- 1909-remove-one-element-to-make-the-array-strictly-increasing/1909-remove-one-element-to-make-the-array-strictly-increasing.cpp
+++ 1909-remove-one-element-to-make-the-array-strictly-increasing/1909-remove-one-element-to-make-the-array-strictly-increasing.cpp
@@ -7,10 +7,10 @@ public:
         {
             if(nums[i]<=prev)
             {
-                if(usedRemoval) return false;
+                if(usedRemoval) return false; //if already used up but still a descending sequence is found
                 usedRemoval = true;
                 
-                if(i==1 || nums[i]>nums[i-2]) prev = nums[i];
+                if(i==1 || nums[i]>nums[i-2]) prev = nums[i]; //if seq is something like ..100,21,101.., 21 removed from middle and prev shifted to 101
             }
             else
                 prev = nums[i];

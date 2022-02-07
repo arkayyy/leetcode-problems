1f8ba1ad34a24b5c551dfc69891ef6e15380d09a
Rajarshi Karan <72148786+arkayyy@users.noreply.github.com>
2022-02-07 17:35:56 +0530

    Time: 49 ms (18.65%), Space: 15.3 MB (17.02%) - LeetHub

--- 207-course-schedule/207-course-schedule.cpp
+++ 207-course-schedule/207-course-schedule.cpp
@@ -1,37 +1,66 @@
 class Solution {
 public:
+    
+    bool isCyclic(int v, vector<int>& visited, unordered_map<int,vector<int>>& adj)
+    {
+        if(visited[v]==1) return true; //cycle exists because a node whose adj is being traversed using DFS currently is visited again in a loop
+        if(visited[v]==0) //if not already visited and adjacents not traversed using DFS
+        {
+            visited[v]= 1; //starting DFS traversals of all adjacents of node v
+            for(auto a: adj[v])
+                if(isCyclic(a,visited,adj))
+                    return true;
+        }
+        visited[v] = 2; //after DFS traversal of adj nodes of v
+        return false;
+    }
+
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<int> ans;
+        
+        //----------------------APPROACH - 1: Topo sort using BFS (Kahn's Algorithm) -------------------//
+        //Using topological sort with BFS (Kahn's Algo):- TC:O(N) SC: O(2N)
         
         unordered_map<int,vector<int>> adj;
-        vector<int> ans;  
-        
-        //---------------APPROACH 1 - Using BFS (Kahn's Algo) TC:O(N) SC:O(N)-------------------------------------------//
-        vector<int> inDegree(numCourses);
-        for(auto p: prerequisites)
+//         queue<int> q;
+//         vector<int> inDegree(numCourses);
+        for(auto e: prerequisites)
         {
-            adj[p[1]].push_back(p[0]);
-            inDegree[p[0]]++;
+            //inDegree[e[0]]++;
+            adj[e[1]].push_back(e[0]);
         }
         
-        queue<int> q; //for bfs
+//         for(int i = 0; i<numCourses; i++)
+//             if(inDegree[i]==0)
+//             {q.push(i);}
+        
+//         while(!q.empty())
+//         {
+//             int t = q.front();
+//             q.pop();
+//             ans.push_back(t);
+//             for(auto a: adj[t])
+//                 if((--inDegree[a])==0)
+//                     q.push(a);
+//         }
+        
+//         if(ans.size()!=numCourses) return false;
+        
+//         return true;
+        
+        
+        //-------------------------APPROACH - 2: Topo sort using DFS (Coloring algo) -------------------------------------//
+        //Intuition: We color the node according to its status:
+        // 0 - not visited
+        // 1 - visited and its adj nodes are currently in process being traversed one by one 
+        // 2 - visited and all adjacents also traversed
+        
+        vector<int> visited(numCourses);
         
         for(int i = 0; i<numCourses; i++)
-            if(inDegree[i] == 0)
-                q.push(i);
+            if(!visited[i] && isCyclic(i,visited,adj))
+                return false;
         
-        while(!q.empty())
-        {
-            int v = q.front();
-            q.pop();
-            ans.push_back(v);
-            for(auto a: adj[v])
-                if(--inDegree[a]==0)
-                    q.push(a);
-        }
-        
-        if(ans.size()==numCourses) return true;
-        
-        return false;
-        
+        return true;
     }
 };

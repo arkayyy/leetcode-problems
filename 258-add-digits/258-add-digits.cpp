1e9ec08b013dbdd3b1371fc279fd957e96270d2a
Rajarshi Karan <72148786+arkayyy@users.noreply.github.com>
2022-02-08 09:38:33 +0530

    Time: 4 ms (43.34%), Space: 5.9 MB (75.31%) - LeetHub

--- 258-add-digits/258-add-digits.cpp
+++ 258-add-digits/258-add-digits.cpp
@@ -3,7 +3,7 @@ public:
     int addDigits(int num) {
         if(num<10) return num;
         
-        
-        return 1+(num-1)%9;
+        //To find the root digit of a number using congruance formula : 1+(num-1)%(base-1)
+        return 1+(num-1)%(10-1);
     }
 };

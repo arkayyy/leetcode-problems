79e4c9c1afe2fe59db45dbed0437ef22e7a6614f
Rajarshi Karan <72148786+arkayyy@users.noreply.github.com>
2022-02-10 20:33:54 +0530

    Time: 374 ms (28.59%), Space: 74.8 MB (11.65%) - LeetHub

---
+++ 1109-corporate-flight-bookings/1109-corporate-flight-bookings.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    //INTUITION: If we keep the value of seats at both the start and end index of an interval. 
+    //While printing the final answer, we can print all the values in between these 2 indexes as the same value.
+    vector<int> corpFlightBookings(vector<vector<int>>& bookings, int n) {
+        vector<int> ans(n+1,0);
+        for(auto b: bookings)
+        {
+            //NOTE:We are using 0 based indexing in our result vector..that's why changing value of index b[0]-1 and not b[0]..it will be useful in doing prefix sum tho..
+            
+            ans[b[0]-1] += b[2]; //adding the value of seats before the starting interval so that when prefix sum is done(i.e.ans[i]+=ans[i-1]), the number of seats gets included in the starting index
+            ans[b[1]] -= b[2]; //subtracting the value because when we come after doing prefix sums till this index, at this index it will become 0 after getting neutralised
+        }
+        for(int i=1; i<=n; i++)
+           ans[i]+=ans[i-1];//doing prefix sum
+        return {ans.begin(),ans.end()-1};
+    }
+};

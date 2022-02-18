9bd4a0daf0b6663e4955141dbe487af1147d859d
Rajarshi Karan <72148786+arkayyy@users.noreply.github.com>
2022-02-19 04:12:23 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (56.07%) - LeetHub

--- 1190-reverse-substrings-between-each-pair-of-parentheses/1190-reverse-substrings-between-each-pair-of-parentheses.cpp
+++ 1190-reverse-substrings-between-each-pair-of-parentheses/1190-reverse-substrings-between-each-pair-of-parentheses.cpp
@@ -2,6 +2,22 @@ class Solution {
 public:
     string reverseParentheses(string s) {
         
+        //----------------APPROACH - 1 : BRUTE FORCE O(N^2) ----------------------------------------------------//
+        // vector<int> opened;
+        // string ans;
+        // for (int i = 0; i < s.length(); ++i) {
+        //     if (s[i] == '(')
+        //         opened.push_back(ans.length());
+        //     else if (s[i] == ')') {
+        //         int j = opened.back();
+        //         opened.pop_back();
+        //         reverse(ans.begin() + j, ans.end());
+        //     } else {
+        //         ans += s[i];
+        //     }
+        // }
+        // return ans;
+        
         
         //----------------------------------MOST OPTIMAL O(N) APPROACH - WORMHOLE (by lee)---------------------------//
         vector<int> pair(s.size());

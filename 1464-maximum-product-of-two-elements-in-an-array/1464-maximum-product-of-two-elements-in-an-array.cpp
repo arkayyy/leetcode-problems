6f59dccae04405aabc8cc884201ab90d8a0b9bf7
Rajarshi Karan <72148786+arkayyy@users.noreply.github.com>
2022-02-21 02:53:15 +0530

    Time: 5 ms (68.88%), Space: 9.8 MB (94.90%) - LeetHub

--- 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.cpp
+++ 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.cpp
@@ -2,14 +2,18 @@ class Solution {
 public:
     int maxProduct(vector<int>& nums) {
         int max1 = INT_MIN;
-        int maxIndex = -1;
         int max2 = INT_MIN;
         for(int i = 0; i<nums.size(); i++)
-            if(nums[i]>max1)
-            {max1=nums[i]; maxIndex = i;}
-        for(int i = 0; i<nums.size(); i++)
-            if(nums[i]>max2 && nums[i]<=max1 && i!=maxIndex)
+            if(nums[i]>=max1) //found a number greater than both max1 and max2,so updating both
+            {
+                max2 = max1;
+                max1=nums[i]; 
+            }
+            else if(nums[i]>max2) //found a number just greater than max2 so updating only that
+            {
                 max2 = nums[i];
+            }
+        
         return (max2-1)*(max1-1);
     }
 };

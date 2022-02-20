b74f20964a0b039e2d6968715554c3cdbac17868
Rajarshi Karan <72148786+arkayyy@users.noreply.github.com>
2022-02-20 16:12:05 +0530

    Time: 25 ms (68.86%), Space: 11.3 MB (89.47%) - LeetHub

--- 1288-remove-covered-intervals/1288-remove-covered-intervals.cpp
+++ 1288-remove-covered-intervals/1288-remove-covered-intervals.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     int removeCoveredIntervals(vector<vector<int>>& intervals) {
-       
+       //INTUITION: Merge intervals concept
         sort(intervals.begin(), intervals.end());
         int cnt = 1;
          int a = intervals[0][0], b = intervals[0][1];
         for(int i = 1; i<intervals.size(); i++)
         {
-            if(intervals[i][0]>a && intervals[i][1]>b)
+            if(intervals[i][0]>a && intervals[i][1]>b) //counting number of intervals to be considered finally
                {cnt++; }
-            if(intervals[i][1]>b)
+            if(intervals[i][1]>b) //if intervals[i][1]<=b, we can continue with the same interval
             {
                 a = intervals[i][0];
                 b = intervals[i][1];

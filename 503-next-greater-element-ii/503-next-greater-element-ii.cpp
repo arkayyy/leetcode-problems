7692cdc72146f3fb9b5ac5d733fac7a2b4708c78
Rajarshi Karan <72148786+arkayyy@users.noreply.github.com>
2022-02-23 12:06:22 +0530

    Time: 58 ms (37.92%), Space: 24.1 MB (40.46%) - LeetHub

--- 503-next-greater-element-ii/503-next-greater-element-ii.cpp
+++ 503-next-greater-element-ii/503-next-greater-element-ii.cpp
@@ -6,7 +6,7 @@ public:
         vector<int> ans(nums.size(),-1);
         for(int i = 0; i<2*nums.size(); i++)
         {
-            while(!st.empty() && nums[i%nums.size()]>st.top().first)
+            while(!st.empty() && nums[i%nums.size()]>st.top().first) //GENERAL RULE: for all the elements in the stack that are less than the current index element in the array, the next greater element of all those elements will be nums[i]
             {
                 ans[st.top().second % nums.size()] = nums[i%nums.size()];
                 st.pop();

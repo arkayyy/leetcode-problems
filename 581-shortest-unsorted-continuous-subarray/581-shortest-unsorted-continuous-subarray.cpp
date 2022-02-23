2c2c3da42c5e47ab19a0eda325b1892e6474c2e1
Rajarshi Karan <72148786+arkayyy@users.noreply.github.com>
2022-02-23 10:42:03 +0530

    Time: 44 ms (53.51%), Space: 29.1 MB (6.40%) - LeetHub

--- 581-shortest-unsorted-continuous-subarray/581-shortest-unsorted-continuous-subarray.cpp
+++ 581-shortest-unsorted-continuous-subarray/581-shortest-unsorted-continuous-subarray.cpp
@@ -1,6 +1,11 @@
 class Solution {
 public:
     int findUnsortedSubarray(vector<int>& nums) {
+        //Brute force O(N logN) -  we can sort the array and find the first dissimilar element and the last dissimilar element between the sorted and unsorted array and O/P their difference in index 
+        
+        
+        //======================APPROACH 2 - Using a MONOTONIC STACK: - O(N) SC:O(N) ====================//
+        //A monotonic queue/deque can also be used...
         int n = nums.size();
         stack<pair<int,int>> st;
         stack<pair<int,int>> st1;
@@ -8,6 +13,8 @@ public:
         
         int firstViolation = nums.size(), lastViolation = -1;
         
+        
+        //finding the first violation from the beginning
         for(int i=0; i<nums.size(); i++)
         {
             if(!st.empty() && nums[i]<st.top().first)
@@ -21,24 +28,21 @@ public:
                 
             }
             st.push({nums[i],i});
-            
-            if(!st1.empty() && nums[n-i-1]>st1.top().first)
+        }
+        
+        //finding the first violation from the end(or the last violation)
+        for(int i =nums.size()-1; i>=0; i--)
+        {
+            if(!st1.empty() && nums[i]>st1.top().first)
             {
-                while(!st1.empty() && nums[n-i-1]>st1.top().first)
+                while(!st1.empty() && nums[i]>st1.top().first)
                 {
                     lastViolation = max(lastViolation,st1.top().second); 
                     st1.pop();
                 }
             }
-            st1.push({nums[n-i-1],n-i-1});
-        
-        }
-        
-        
-//         for(int i =nums.size()-1; i>=0; i--)
-//         {
-            
-//         }
+            st1.push({nums[i],i});
+         }
         
         int ans =  (lastViolation - firstViolation +1);
         

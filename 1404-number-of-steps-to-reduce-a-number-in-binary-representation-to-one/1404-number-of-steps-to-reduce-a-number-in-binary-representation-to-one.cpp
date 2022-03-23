5a7e54d5ee09fcf6341423544840b5ec4b6b1aab
Rajarshi Karan <72148786+arkayyy@users.noreply.github.com>
2022-03-24 03:15:13 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (25.38%) - LeetHub

--- 1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one.cpp
+++ 1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one.cpp
@@ -7,6 +7,8 @@ public:
         //ALGO: First we move from the right until we encounter a 1, and at every move we add 1 to steps. 
         //Because for all trailing zeros, we right shift(divide by 2) until we have no trailing zeros and we encounter 1.
         //Next, for every 0 encountered, we add 2 steps, and for every 1 encountered, we add 1 step.
+        //Therefore for each subsequent '0', you need to add 2 to answer as it gets carried 1 and then 1 need to be added.
+        //If '1' then simply add 1 to answer as it becomes '0' from due to carried '1'. The final result is res + 1 as this will result in one additional bit to the string.
         
         int steps = 0;
         
@@ -28,6 +30,6 @@ public:
                 ++steps;
             i--;
         }
-        return steps+1; 
+        return steps+1;  
     }
 };

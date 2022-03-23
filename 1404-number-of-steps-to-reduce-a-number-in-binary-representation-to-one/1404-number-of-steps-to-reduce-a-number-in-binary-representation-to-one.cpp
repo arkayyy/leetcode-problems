f8f824c43c00ed5cd4b9f4ca70dd6d8d6ee243a1
Rajarshi Karan <72148786+arkayyy@users.noreply.github.com>
2022-03-24 03:12:37 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (60.91%) - LeetHub

--- 1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one.cpp
+++ 1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one.cpp
@@ -2,27 +2,32 @@ class Solution {
 public:
     int numSteps(string s) {
         //INTUITION: 
-        //(i)If last digit is 1, number is odd, so add 2 steps (add 1 and divide by 2)
-        //(ii) If last digit is 0, number is even, so add 1 step (divide by 2)
+        //Dividing by 2 means number>>1 (right shift by 1 bit) (i.e. 1001 >> 1 = 0100, 0100 >> 1 = 0010)
         
-        //Now, dividing by 2 means number>>1 (right shift by 1 bit) (i.e. 1001 >> 1 = 0100, 0100 >> 1 = 0010)
-        //This means we add one 0 to the left of the binary string. So nothing is needed to do anymore since only zeros will be adding from the left.
-        //So now if we traverse the whole string, for every encountered 1, we add two steps, and for every encountered 0, we add one step
+        //ALGO: First we move from the right until we encounter a 1, and at every move we add 1 to steps. 
+        //Because for all trailing zeros, we right shift(divide by 2) until we have no trailing zeros and we encounter 1.
+        //Next, for every 0 encountered, we add 2 steps, and for every 1 encountered, we add 1 step.
         
-        //Now, adding 1 to the number might produce a carry, so we handle that too.
+        int steps = 0;
         
-        int steps = 0, carry = 0;
-        for(int i = s.length()-1; i>0; i--) // we traverse from the end, since addition occurs from the right to left
+        int i = s.length()-1;
+        while(i>=0 && s[i]=='0')
         {
-            if((s[i]=='0' && carry==1) || (s[i]=='1' && carry == 0))  // or if sum(s[i]-'0'+carry) == 1 (i.e. encountered a 1)
+            i--, steps++;
+        }
+        if(i==0)
+            return steps;
+        
+        while(i>=0)
+        {
+            if(s[i]=='0')
             {
                 steps+=2;
-                carry = 1;
             }
-            else  //encountered 0
+            else
                 ++steps;
+            i--;
         }
-        
-        return steps+carry; //if carry is still present
+        return steps+1; 
     }
 };

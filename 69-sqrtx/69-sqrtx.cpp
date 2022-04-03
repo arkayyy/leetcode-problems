62341417fe747b4210f9e231ba22c90d5795a871
Rajarshi Karan <72148786+arkayyy@users.noreply.github.com>
2022-04-03 13:23:30 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (74.98%) - LeetHub

--- 69-sqrtx/69-sqrtx.cpp
+++ 69-sqrtx/69-sqrtx.cpp
@@ -1,10 +1,32 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        long long i =1;
-        for( i = 1; i*i<=(long long)x; i++)
-            continue;
+        //Normal method:-
+//         long long i =1;
+//         for( i = 1; i*i<=(long long)x; i++)
+//             continue;
         
-        return (int)i-1;
+//         return (int)i-1;
+        
+        //Newton's method:-
+        // long long r = x;
+        // while (r*r > x)
+        //     r = (r + x/r) / 2;
+        // return r;
+        
+        //Binary search:-
+        long long ans = 0;
+        long long lo = 1, hi = INT_MAX;
+        while(lo<=hi)
+        {
+            long long mid = lo+((hi-lo)>>1);
+            if(mid*mid==(long long)x) return mid;
+            if(mid*mid < x)
+            {ans = mid; lo = mid+1;}
+            else
+                hi = mid-1;
+        }
+        
+        return ans;
     }
 };

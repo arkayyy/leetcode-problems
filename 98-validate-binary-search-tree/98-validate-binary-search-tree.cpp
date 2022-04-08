177fe2592fd4518a6c9bc186a1d1aad4ad37630d
Rajarshi Karan <72148786+arkayyy@users.noreply.github.com>
2022-04-08 16:17:21 +0530

    Time: 22 ms (28.28%), Space: 21.7 MB (30.93%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.cpp
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.cpp
@@ -12,12 +12,16 @@
 #define ll long long
 class Solution {
 public:
+    //INTUITION: Every node in a binary search tree must lie within a range that has been fixed by its ancestor nodes.
+    //ALGO: So we take a left range and right range and keep updating it differently as we traverse to the left and right of a node.
     bool util(TreeNode* root, ll leftLim,ll rightLim)
     {
         if(!root) return true;
         
         if((ll)root->val<=leftLim || (ll)root->val>=rightLim) return false;
         
+        //for left child node, the value can't be greater or equal to current(ancestor)  node value, so updating only right(upper) limit
+        //for right child node, the value can't be less or equal to current(ancestor)  node value, so updating only left(lower) limit
         return util(root->left, leftLim, root->val) && util(root->right, root->val, rightLim);
     }
     bool isValidBST(TreeNode* root) {

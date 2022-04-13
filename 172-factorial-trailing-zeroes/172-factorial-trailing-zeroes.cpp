670cf3f290a01cf5133fa0ba45fdefd72dcdac63
Rajarshi Karan <72148786+arkayyy@users.noreply.github.com>
2022-04-13 16:21:05 +0530

    Time: 2 ms (50.27%), Space: 6 MB (31.03%) - LeetHub

--- 172-factorial-trailing-zeroes/172-factorial-trailing-zeroes.cpp
+++ 172-factorial-trailing-zeroes/172-factorial-trailing-zeroes.cpp
@@ -1,7 +1,13 @@
 class Solution {
 public:
+    //INTUITION: We have to count for all multiples of 5 and all multiples of the powers of 5 too. Because those will produce one zero each.
+    //e.g. 5's multiples will produce one zero each. 5*5 (25's) multiples will produce additional one zeros each because of the extra 5.
     int trailingZeroes(int n) {
-        if(n==0) return 0;
-        return n/5 + trailingZeroes(n/5);
+        // if(n==0) return 0;
+        // return n/5 + trailingZeroes(n/5);
+        int ans = 0;
+        for(int i = 5; (n/i)>0; i*=5)
+            ans += (n/i);
+        return ans;
     }
 };

a6e3517580454f82d7cb49919d6d7f891a966d84
Rajarshi Karan <72148786+arkayyy@users.noreply.github.com>
2022-04-18 11:46:01 +0530

    Time: 15 ms (92.75%), Space: 23.9 MB (98.53%) - LeetHub

--- 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.cpp
+++ 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.cpp
@@ -13,9 +13,10 @@ class Solution {
 public:
     int kthSmallest(TreeNode* root, int &k) {
         if(!root) return 0; //doesn't matter what we return here, writing just for handling termination of recursive fn
-        int x = kthSmallest(root->left, k);
+        int x = kthSmallest(root->left, k); //traverses and reaches the leftmost(smallest) node first
         //when k==0, we return the kth smallest node that we already have stored in x as our answer
         //when (--k) or k-1==0, it means there are k-1 nodes to the left of current node and this is the desired answer (kth smallest)
+        //otherwise, we traverse to the right to find greater value node until we reach kth smallest node
         return k==0?x:((--k)==0? root->val : kthSmallest(root->right, k));
     }
 };

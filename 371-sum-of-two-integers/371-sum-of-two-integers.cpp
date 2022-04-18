6c6df6fd6679fc597387e63f2bb9d9036ca5aee9
Rajarshi Karan <72148786+arkayyy@users.noreply.github.com>
2022-04-18 12:36:42 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (75.61%) - LeetHub

--- 371-sum-of-two-integers/371-sum-of-two-integers.cpp
+++ 371-sum-of-two-integers/371-sum-of-two-integers.cpp
@@ -3,9 +3,9 @@ public:
     //INTUITION: We make sure the first bit (sign bit) of carry (a & b) always be zero to avoid the left shift of negative value error.
     int getSum(int a, int b) {
         while(a){
-        int sum = a ^ b;
-        a = ((a & b) & 0x7fffffff) << 1;
-        b = sum;
+        int sum = a ^ b; //sum of a and b without the carry
+        a = ((a & b) & 0x7fffffff) << 1; //calculating the carry
+        b = sum; //adding sum without carry and the carry
         }
         return b;
     }

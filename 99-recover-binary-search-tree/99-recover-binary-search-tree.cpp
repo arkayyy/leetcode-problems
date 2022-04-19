d9500dfb6a2e6c4f66558f99e05afe9002d197ac
Rajarshi Karan <72148786+arkayyy@users.noreply.github.com>
2022-04-20 02:05:45 +0530

    Time: 27 ms (91.47%), Space: 57.8 MB (91.42%) - LeetHub

--- 99-recover-binary-search-tree/99-recover-binary-search-tree.cpp
+++ 99-recover-binary-search-tree/99-recover-binary-search-tree.cpp
@@ -11,23 +11,26 @@
  */
 class Solution {
 public:
-    void markTree(TreeNode* root, TreeNode*& prev, TreeNode*& first, TreeNode*& end) {
+    //INTUITION: We are doing inorder traversal, at any point if we find the current node value conflicts its ancestor node value as per rules of a BST, we store both nodes.
+    
+    //ALGO: For every node, we check violation caused by any of its child/grandchild/further lower level nodes in its right subtree(we can also check in left subtree)...
+    void check(TreeNode* root, TreeNode*& prev, TreeNode*& first, TreeNode*& end) {
         if (!root) return;
-        markTree(root->left, prev, first, end);
+        check(root->left, prev, first, end); //traversing to the leftmost node first, then we will backtrack and check in all traversed nodes' right subtrees
         if (prev) {
-            if (root->val < prev->val) {
-                if (!first) {
+            if (root->val < prev->val) { //conflict found (if any node in the right subtree has value less than its ancestor node)
+                if (!first) { //first if already there, means it is the most ancestor node already which needs to be swapped, so no need to change
                     first = prev;
                 }
-                end = root;
+                end = root;//we change end everytime the violation occurs because we need to find the lowermost violating node
             }
         }
-        prev = root;
-        markTree(root->right, prev, first, end);
+        prev = root; //setting current root as prev
+        check(root->right, prev, first, end);//and then checking for conflicts in all nodes of the right subtree
     }
     void recoverTree(TreeNode* root) {
-        TreeNode *prev = nullptr, *first = nullptr, *end = nullptr;
-        markTree(root, prev, first, end);
+        TreeNode *prev = NULL, *first = NULL, *end = NULL;
+        check(root, prev, first, end);
         swap(first->val, end->val);
         return;
     }

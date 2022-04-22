bd8f03cf98fe5214e7af15277f1431caebe30465
Rajarshi Karan <72148786+arkayyy@users.noreply.github.com>
2022-04-23 02:49:11 +0530

    Time: 111 ms (93.30%), Space: 52.1 MB (75.12%) - LeetHub

--- 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.cpp
+++ 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.cpp
@@ -11,20 +11,21 @@ public:
         // }
         // return j - i;
         
-        //-------------APPROACH 2: 2 Dequeues--------------------//
+        //-------------APPROACH 2: 2 Monotonic Dequeues--------------------//
         deque<int> maxd, mind;
         int i = 0, j;
         for (j = 0; j < nums.size(); ++j) {
-            while (!maxd.empty() && nums[j] > maxd.back()) maxd.pop_back();
+            while (!maxd.empty() && nums[j] > maxd.back()) maxd.pop_back(); 
             while (!mind.empty() && nums[j] < mind.back()) mind.pop_back();
-            maxd.push_back(nums[j]);
-            mind.push_back(nums[j]);
-            if (maxd.front() - mind.front() > limit) {
+            maxd.push_back(nums[j]);//maxd stores number in descending order from start to end
+            mind.push_back(nums[j]);//mind stores number in ascending order from start to end
+            if (maxd.front() - mind.front() > limit) { //maxd.front() - min. element till now
+                //mind.front() - max element till now
                 if (maxd.front() == nums[i]) maxd.pop_front();
                 if (mind.front() == nums[i]) mind.pop_front();
                 ++i;
             }
         }
-        return j - i;
+        return nums.size() - i;
     }
 };

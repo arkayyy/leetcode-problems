ba0f72ed8f4ddacb89012d7d1c6e271e553a56af
Rajarshi Karan <72148786+arkayyy@users.noreply.github.com>
2022-04-24 05:08:42 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (5.84%) - LeetHub

--- 535-encode-and-decode-tinyurl/535-encode-and-decode-tinyurl.cpp
+++ 535-encode-and-decode-tinyurl/535-encode-and-decode-tinyurl.cpp
@@ -1,14 +1,33 @@
 class Solution {
+    private:
+    //simple arithmetic hashing using a prime no.
+    uint64_t hash_function(const string& str) {
+        uint64_t hash = 0x811c9dc5;
+        uint64_t prime = 0x1000193;
+
+        for(int i = 0; i < str.size(); ++i) {
+            uint8_t value = str[i];
+            hash = hash ^ value;
+            hash *= prime;
+        }
+
+        return hash;
+    }
+    
+    unordered_map<string, string> long_to_short;
+    unordered_map<string, string> short_to_long;
 public:
 
     // Encodes a URL to a shortened URL.
     string encode(string longUrl) {
-        return longUrl;
+        long_to_short[longUrl] = "http://tinyurl.com/" + to_string(hash_function(longUrl));
+        short_to_long[long_to_short[longUrl]] = longUrl;
+        return long_to_short[longUrl];
     }
 
     // Decodes a shortened URL to its original URL.
     string decode(string shortUrl) {
-        return shortUrl;
+        return short_to_long[shortUrl];
     }
 };
 

24e47cfd0c73235b6b1017f2dea2706e760ca97b
Rajarshi Karan <72148786+arkayyy@users.noreply.github.com>
2022-04-24 10:52:31 +0530

    Time: 1974 ms (20.00%), Space: 96.1 MB (60.00%) - LeetHub

--- 2250-count-number-of-rectangles-containing-each-point/2250-count-number-of-rectangles-containing-each-point.cpp
+++ 2250-count-number-of-rectangles-containing-each-point/2250-count-number-of-rectangles-containing-each-point.cpp
@@ -1,5 +1,7 @@
 class Solution {
 public:
+    //INTUITION: Since the constraints for y coordinates is less (only upto 100), So for every y coordinate we store all associated x coordinates of a rectangle
+    //Later for every point, we check for every y coordinate between that point's y coordinate and 100, and their possible x coordinates in rectangle for containing the point
     vector<int> countRectangles(vector<vector<int>>& rectangles, vector<vector<int>>& points) {
         unordered_map<int,vector<int>> mp;
         for(auto rect: rectangles)
@@ -12,7 +14,9 @@ public:
             int x = p[0], y = p[1];
             int sum = 0;
             for(int i = y; i<=100; i++)
-                sum+= mp[i].end() - lower_bound(mp[i].begin(),mp[i].end(),x);
+                sum+= mp[i].end() - lower_bound(mp[i].begin(),mp[i].end(),x); //lower bound will give the position of the rectangle upper point's x coordinate that is just more than (or not less than) the given value of x (in point)
+            //This will ensure that all such rectangles are counted whose upper corner's x coordinate is more than this given point's x coordinate...
+            //...because only those rectangles can contain this point!
             ans.push_back(sum);
         }
         

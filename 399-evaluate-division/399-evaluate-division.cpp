10ab3a3a851200d03d67848fa6c2d91c4ef69bba
Rajarshi Karan <72148786+arkayyy@users.noreply.github.com>
2022-04-30 09:56:17 +0530

    Time: 4 ms (51.49%), Space: 8.3 MB (74.56%) - LeetHub

--- 399-evaluate-division/399-evaluate-division.cpp
+++ 399-evaluate-division/399-evaluate-division.cpp
@@ -1,5 +1,12 @@
 class Solution {
 public:
+    //INTUITION: a/b can also be expressed as => a/c * c/b. So it means we go through the c factor in middle.
+    //SO indirectly its like traversing from a vertex to b vertex in a graph through multiple vertices, since all ways will lead to same result. IT'S MATHS!
+    
+    //ALGO: Doing simple DFS+hashing. We store the provided equation results between variables in our hash table to use them during DFS.
+    //We use a visited array just to not take any middle factor/variable multiple times.
+    //e.g.if a/b = a/c*c/b. Taking c factor in middle once more will give a/c*c/c*c/b = a/b which was the original required expression.
+    
     double util(string s1, string s2, unordered_map<string, unordered_map<string,double>> &mp, unordered_set<string> &visited)
     {
         if(mp.find(s1)!=mp.end() && mp[s1].find(s2)!=mp[s1].end()) return mp[s1][s2];
@@ -10,7 +17,6 @@ public:
                 {
                 visited.insert(m.first);
                 double m2 = util(m.first,s2, mp, visited);
-                //mp[m.first][s2] = m2;
                 if(m2!=-1)  return m.second*m2;
                 }
         }

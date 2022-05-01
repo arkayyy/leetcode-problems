fb154c9966d0270abb0e360a74d43a385420f864
Rajarshi Karan <72148786+arkayyy@users.noreply.github.com>
2022-05-01 14:03:59 +0530

    Time: 40 ms (100.00%), Space: 10.5 MB (100.00%) - LeetHub

--- 2262-total-appeal-of-a-string/2262-total-appeal-of-a-string.cpp
+++ 2262-total-appeal-of-a-string/2262-total-appeal-of-a-string.cpp
@@ -1,13 +1,14 @@
 class Solution {
 public:
+    //INTUITION: If we store the previous occurence index of each character, we can ensure that the substring between those two indexes will contain all unique letters.
+    //ALGO: We keep on adding the length of these substrings
     long long appealSum(string s) {
-        vector<int> last(26);
-        long res = 0, n = s.size();
-        for (int i = 0; i < n; ++i) {
-            last[s[i] - 'a'] = i + 1;
-            for (int j: last)
-                res += j;
-        }
-        return res;
+    long long res = 0, cur = 0, prev[26] = {};
+    for (int i = 0; i < s.size(); ++i) {
+        cur += i  - prev[s[i] - 'a'] + 1; //length of string b/w ith index and the index of previous occurence of same character at ith index
+        prev[s[i] - 'a'] = i + 1; //marking the current occurence of this character as previous occurence index for the next occuring same character
+        res += cur;
+    }
+    return res;
     }
 };

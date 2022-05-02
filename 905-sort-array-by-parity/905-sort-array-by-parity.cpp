b97abbce85fab694a7b52814ea48aa2654a7bbbb
Rajarshi Karan <72148786+arkayyy@users.noreply.github.com>
2022-05-02 07:52:53 +0530

    Time: 18 ms (31.70%), Space: 16.1 MB (83.25%) - LeetHub

--- 905-sort-array-by-parity/905-sort-array-by-parity.cpp
+++ 905-sort-array-by-parity/905-sort-array-by-parity.cpp
@@ -4,9 +4,9 @@ public:
         int evenIdx = 0, oddIdx = nums.size()-1;
         while(evenIdx<oddIdx)
         {
-            if(!(nums[evenIdx]&1) && (nums[oddIdx]&1)) {++evenIdx,--oddIdx; continue;}
-            else if((nums[evenIdx]&1)&& !(nums[oddIdx]&1)) swap(nums[oddIdx],nums[evenIdx]);
-            else if((nums[evenIdx]&1)) --oddIdx;
+            if(!(nums[evenIdx]&1) && (nums[oddIdx]&1)) {++evenIdx,--oddIdx; continue;} //NO VIOLATION
+            else if((nums[evenIdx]&1)&& !(nums[oddIdx]&1)) swap(nums[oddIdx],nums[evenIdx]); //will swap only in case of violation( odd number at evenIdx position and even number at oddIdx position)
+            else if((nums[evenIdx]&1)) --oddIdx; //In case of only one violation, we shift only the other pointer which doesn't have violating element
             else if(!(nums[oddIdx]&1)) ++evenIdx;
         }
         return nums;

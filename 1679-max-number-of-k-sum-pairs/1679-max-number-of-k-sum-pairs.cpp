72edfa863ebd2a7cc566643864b797e8218f0e39
Rajarshi Karan <72148786+arkayyy@users.noreply.github.com>
2022-05-04 16:30:44 +0530

    Time: 190 ms (53.70%), Space: 58.2 MB (62.65%) - LeetHub

--- 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.cpp
+++ 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.cpp
@@ -1,20 +1,35 @@
 class Solution {
 public:
     int maxOperations(vector<int>& nums, int k) {
-        unordered_map<int,int> freq;
-        for(auto n: nums) freq[n]++;
+        //INTUITION: Similar to 2 sum problem...
+        
+        //==========================APPROACH 1: Frequency map Time O(2N) Space O(N) =======================================//
+        // unordered_map<int,int> freq;
+        // for(auto n: nums) freq[n]++;
+        // int ans = 0;
+        // for(auto& m: freq)
+        // {
+        //     if(m.first==(k-m.first)) ans+=(m.second/=2);
+        //     else{
+        //         auto found = freq.find(k-m.first);
+        //         if(found!=freq.end()) {
+        //             int deduct = min(found->second,m.second);
+        //             ans+=deduct;
+        //             found->second-=deduct; m.second-=deduct;
+        //         }
+        //     }
+        // }
+        // return ans;
+        
+        //======================APPROACH 2: Sorting + 2Pointer TC: O(NlogN) SC:O(1) =========================================//
+        sort(nums.begin(),nums.end());
         int ans = 0;
-        for(auto& m: freq)
+        int i = 0, j = nums.size()-1;
+        while(i<j)
         {
-            if(m.first==(k-m.first)) ans+=(m.second/=2);
-            else{
-                auto found = freq.find(k-m.first);
-                if(found!=freq.end()) {
-                    int deduct = min(found->second,m.second);
-                    ans+=deduct;
-                    found->second-=deduct; m.second-=deduct;
-                }
-            }
+            if(nums[i]+nums[j]>k) --j;
+            else if(nums[i]+nums[j]<k) ++i;
+            else ++i,--j,++ans;
         }
         return ans;
     }

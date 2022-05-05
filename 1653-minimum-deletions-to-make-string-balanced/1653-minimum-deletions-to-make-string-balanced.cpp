cd581555eb5acb516955714a637550c61863252b
Rajarshi Karan <72148786+arkayyy@users.noreply.github.com>
2022-05-05 21:38:57 +0530

    Time: 92 ms (85.86%), Space: 22.1 MB (59.21%) - LeetHub

--- 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.cpp
+++ 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.cpp
@@ -1,27 +1,7 @@
 class Solution {
 public:
-    int util(int i, bool aOccured, bool bOccured, string s)
-    {
-        if(i==s.length()) { if(!aOccured && !bOccured) return INT_MAX-1; else return 0;}
-        int res1,res2;
-        if(s[i]=='a')
-        {
-            if(bOccured) res1 = 1+util(i+1,aOccured,bOccured,s);
-            //(aOccured) res1 =  1+util(i+1,aOccured,bOccured,s);
-            else res1 = min(1+util(i+1,aOccured,bOccured,s), util(i+1,true,bOccured,s));
-        }
-        else
-        {
-            // if(!aOccured) res2 = 1+util(i+1, aOccured, bOccured,s);
-            // else if(bOccured) res2 =  1+util(i+1, aOccured, bOccured,s);
-            //else 
-            res2 = min(1+util(i+1,aOccured, bOccured, s), util(i+1, aOccured, true, s));
-        }
-        
-        return min(res1,res2);
-    }
+
     int minimumDeletions(string s) {
-        // return util(0,false,false,s);
         
         //============================Tabulation DP Approach O(N) Time O(N) Space==============================//
 //         vector<int> dp(s.size()+1);
@@ -42,6 +22,7 @@ public:
         
         
         //========================Space Optimized DP O(N) Time O(1) Space =================================//
+        //Since we are only using current and previous state of the dp array...!
         int prev = 0, curr = 0;
         int b = 0;
         for(int i = 1; i<=s.size(); i++)

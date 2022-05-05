cded9d56ea4a6974bc2aa0a7a9521b0b0c198e7c
Rajarshi Karan <72148786+arkayyy@users.noreply.github.com>
2022-05-05 17:52:01 +0530

    Time: 75 ms (67.49%), Space: 18.2 MB (38.84%) - LeetHub

--- 221-maximal-square/221-maximal-square.cpp
+++ 221-maximal-square/221-maximal-square.cpp
@@ -15,7 +15,24 @@ public:
         return allZeros?0:(ans*ans);
 
         //Space optimized DP:-
-
+//         vector<vector<int>> dp(2, vector<int>(matrix[0].size())); //taking two rows, 0th for the previous row and 1st for the current
+//         for(int i = 0; i<matrix.size(); i++) if(matrix[i][0]=='1') allZeros = false; //to handle the edge case of only one column in matrix
+        
+//         for(int j = 0; j<matrix[0].size(); j++) {if(matrix[0][j]=='1') {allZeros = false; dp[0][j] = 1;} else dp[0][j] = 0; } //initializing DP array
+        
+//         if(matrix.size()>1) dp[1][0] = matrix[1][0]=='1'?1:0;
+        
+//         int ans = 1; 
+//         for(int i = 1; i<matrix.size(); i++)
+//           {  for(int j = 1; j<matrix[0].size(); j++)
+//                { 
+//                 if(matrix[i][j]=='1')
+//                     { allZeros = false; ans = max(ans, dp[1][j] = 1+min({dp[0][j], dp[1][j-1], dp[0][j-1]}) );}
+               
+//             }
+//            dp[0] = dp[1];
+//         }
+//         return allZeros?0:(ans*ans);
         
     }
 };

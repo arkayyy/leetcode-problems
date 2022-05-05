c808b818484e609c38921364c4a42483ce82ef2a
Rajarshi Karan <72148786+arkayyy@users.noreply.github.com>
2022-05-05 21:37:48 +0530

    Time: 94 ms (83.22%), Space: 22 MB (79.50%) - LeetHub

--- 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.cpp
+++ 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.cpp
@@ -24,20 +24,39 @@ public:
         // return util(0,false,false,s);
         
         //============================Tabulation DP Approach O(N) Time O(N) Space==============================//
-        vector<int> dp(s.size()+1);
+//         vector<int> dp(s.size()+1);
+//         int b = 0;
+//         dp[0] = 0;
+//         for(int i = 1; i<=s.size(); i++)
+//         {
+//             if(s[i-1]=='a')
+//             {
+//                 dp[i] = min(1+dp[i-1],b); //if current character is 'a', we can either remove all b's that have appeared before it OR we can remove the current character 'a' itself
+//             }
+//             else
+//             {
+//                 dp[i] = dp[i-1], ++b; //we can consider 'b' in any case, doesn't matter if 'a' or 'b' have appeared before or not...we just count b at every occurence
+//             }
+//         }
+//         return dp[s.size()];
+        
+        
+        //========================Space Optimized DP O(N) Time O(1) Space =================================//
+        int prev = 0, curr = 0;
         int b = 0;
-        dp[0] = 0;
         for(int i = 1; i<=s.size(); i++)
         {
             if(s[i-1]=='a')
             {
-                dp[i] = min(1+dp[i-1],b); //
+                curr = min(1+prev,b); //if current character is 'a', we can either remove all b's that have appeared before it OR we can remove the current character 'a' itself
             }
             else
             {
-                dp[i] = dp[i-1], ++b;
+                curr = prev, ++b; //we can consider 'b' in any case, doesn't matter if 'a' or 'b' have appeared before or not...we just count b at every occurence
             }
+            prev = curr;
         }
-        return dp[s.size()];
+        
+        return curr;
     }
 };

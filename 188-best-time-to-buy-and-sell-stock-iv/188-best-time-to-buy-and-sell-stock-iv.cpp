43f53723b53974e268a322244014767a1e12e224
Rajarshi Karan <72148786+arkayyy@users.noreply.github.com>
2022-05-05 13:43:43 +0530

    Time: 16 ms (50.98%), Space: 10.9 MB (81.33%) - LeetHub

--- 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -38,33 +38,63 @@ public:
         
         
         //===================== APPROACH 2: Tabulation DP O(KN) Time O(KN) Space ========================================//
-       vector<vector<int>> dp(n+1,vector<int>(transactions+1,-1));
+//        vector<vector<int>> dp(n+1,vector<int>(transactions+1,-1));
         
-        //step 1: base case transfer as per the ones used in recursion+memoization
+//         //step 1: base case transfer as per the ones used in recursion+memoization
+//         for(int t = 0; t<=transactions; t++)
+//             dp[n][t] = 0;
+//         for(int d = 0; d<=n; d++)
+//             dp[d][0] = 0;
+//         //step 2: recurrence relations similar as per the ones used in recursion+memoization
+//         for(int days = n-1; days>=0; days--)
+//         {
+//             for(int t = 1; t<=transactions; t++)
+//             {
+//                 int dontBuy = dp[days+1][t];
+//                 //if we transact the current index share
+//                 int buy;
+//                 int shouldBuy = t%2==0; //since we can only one share at once, and buy another only after we sell that one.
+//                 //e.g. we have all 4 transactions left means, we can only buy share. 
+//                 //and when we have 3 left, means we have already bought one, so we can only sell before we buty another.
+//                 if(shouldBuy)
+//                     buy = -prices[days]+dp[days+1][t-1]; 
+//                 else
+//                     buy = prices[days]+dp[days+1][t-1];
+
+//                 dp[days][t] = max(dontBuy, buy);
+//             }
+//         }
+
+//         return dp[0][transactions];
+        
+        
+        //============================APPROACH 3: Space Optimised DP O(KN) Time O(1) Time==================================//
+        vector<vector<int>> dp(2,vector<int>(transactions+1)); 
+
         for(int t = 0; t<=transactions; t++)
-            dp[n][t] = 0;
-        for(int d = 0; d<=n; d++)
-            dp[d][0] = 0;
-        //step 2: recurrence relations similar as per the ones used in recursion+memoization
+            {dp[n%2][t] = 0;}
+        dp[0][0] = 0, dp[1][0] = 0;
+        
         for(int days = n-1; days>=0; days--)
         {
             for(int t = 1; t<=transactions; t++)
             {
-                int dontBuy = dp[days+1][t];
+                int dontBuy = dp[(days+1)%2][t]; 
                 //if we transact the current index share
                 int buy;
                 int shouldBuy = t%2==0; //since we can only one share at once, and buy another only after we sell that one.
                 //e.g. we have all 4 transactions left means, we can only buy share. 
                 //and when we have 3 left, means we have already bought one, so we can only sell before we buty another.
                 if(shouldBuy)
-                    buy = -prices[days]+dp[days+1][t-1]; //dp[(days+1)%2][t-1] denotes the max profit that has been earned before curr transaction (upto t-1 transactions)
+                    buy = -prices[days]+dp[(days+1)%2][t-1]; //dp[(days+1)%2][t-1] denotes the max profit that has been earned before curr transaction (upto t-1 transactions)
                 else
-                    buy = prices[days]+dp[days+1][t-1];
+                    buy = prices[days]+dp[(days+1)%2][t-1];
 
-                dp[days][t] = max(dontBuy, buy);
+                dp[days%2][t] = max(dontBuy, buy);
             }
         }
-
+        
         return dp[0][transactions];
+
     }
 };

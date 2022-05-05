0abe468ab9d36a370361b14782b3176ba1b05a24
Rajarshi Karan <72148786+arkayyy@users.noreply.github.com>
2022-05-05 13:08:25 +0530

    Time: 10 ms (70.73%), Space: 12.7 MB (33.70%) - LeetHub

--- 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,9 +1,44 @@
 class Solution {
 public:
+    //Recursion + Memoization fn. -> Going top-down and memoizing max profit related to indexes as (day, transactions left)
+    //Top down -> days from 0 to n, transactions from k to 0
+    //So bottom up -> days from n to 0, transactions from 0 to k
+    int util(int day, int transactions, vector<int>&prices, vector<vector<int>>& dp)
+    {
+        if(day==prices.size())
+            return 0;
+        if(transactions==0)
+            return 0;
+    
+        if(dp[day][transactions]!=-1) return dp[day][transactions];
+        
+        //if we don't transact with the current index share
+        int dontBuy = util(day+1, transactions, prices,dp);
+        
+        //if we transact the current index share
+        int buy;
+        int shouldBuy = transactions%2==0; //since we can only one share at once, and buy another only after we sell that one.
+        //e.g. we have all 4 transactions left means, we can only buy share. 
+        //and when we have 3 left, means we have already bought one, so we can only sell before we buty another.
+        if(shouldBuy)
+            buy = -prices[day]+util(day+1, transactions-1, prices,dp);
+        else
+            buy = prices[day]+util(day+1, transactions-1, prices,dp);
+        
+        return dp[day][transactions] = max(dontBuy, buy);
+    }
+    
     int maxProfit(int k, vector<int>& prices) {
         int n = prices.size();
-        int transactions = 2*k;  //taking both buy and sell transactions total
-        vector<vector<int>> dp(n+1,vector<int>(transactions+1,-1));
+         int transactions = 2*k; //selling + buying transactions (We buy when transactions value is even, and sell when it is odd)
+        
+       //===========================APPROACH 1: Recursion + Memoization O(KN) Time O(KN)+O(N) aux space================================//
+        // vector<vector<int>> dp(n,vector<int>(transactions+1,-1));
+        // return util(0,transactions,prices,dp);
+        
+        
+        //===================== APPROACH 2: Tabulation DP O(KN) Time O(KN) Space ========================================//
+       vector<vector<int>> dp(n+1,vector<int>(transactions+1,-1));
         
         //step 1: base case transfer as per the ones used in recursion+memoization
         for(int t = 0; t<=transactions; t++)

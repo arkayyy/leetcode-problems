8f445bd2638f15ca71e43c8327ddedd4ae1cfcfb
Rajarshi Karan <72148786+arkayyy@users.noreply.github.com>
2022-05-06 19:42:21 +0530

    Time: 3 ms (44.24%), Space: 6.1 MB (35.20%) - LeetHub

--- 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.cpp
+++ 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.cpp
@@ -1,20 +1,28 @@
 class Solution {
-public:
-    int util(int i, int n, vector<char> &vowels, int&ans, vector<vector<int>>& dp)
+public://Top down: i-> 0 to 5 AND n->n to 0
+    int util(int i, int n, vector<char> &vowels, vector<vector<int>>& dp)
     {
-        if(n==0) {++ans; return 1;}
+        if(n==0) {return 1;}
         
         int res = 0;
         for(int j = i; j<vowels.size(); j++)
-            res += util(j,n-1, vowels,ans,dp);
+            res += util(j,n-1, vowels,dp);
         
         return dp[n][i] = res;
     }
     int countVowelStrings(int n) {
+        
         vector<char> vowels = {'a','e','i','o','u'};
-        int ans = 0;
-        vector<vector<int>> dp(n+1, vector<int> (5));
-        return util(0,n,vowels,ans, dp);
-        //return ans;
+        
+        //====================APPROACH 1: Recursion+Memoization O(5*N) Time O(5*N)+O(N) aux space========================// 
+        // vector<vector<int>> dp(n+1, vector<int> (5));
+        // return util(0,n,vowels, dp);
+        
+        //====================APPROACH 2: Space Optimized Tabulation DP ==============================//
+        vector<int> dp(5,1);
+        for(int i = 0; i<n; i++)
+            for(int j = 1; j<5; j++)
+                dp[j]+=dp[j-1];
+        return dp[4];
     }
 };

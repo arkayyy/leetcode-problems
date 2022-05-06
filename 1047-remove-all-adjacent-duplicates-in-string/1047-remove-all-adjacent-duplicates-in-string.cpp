3977774bbcc59c6c619931a564fff2d104eb1d6d
Rajarshi Karan <72148786+arkayyy@users.noreply.github.com>
2022-05-06 09:17:17 +0530

    Time: 16 ms (90.93%), Space: 10.8 MB (77.35%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
@@ -1,16 +1,30 @@
 class Solution {
 public:
+    
     string removeDuplicates(string s) {
-        stack<char> st;
-        for(auto ch:s)
+        //==================================APPROACH 1: Stack Based O(2N) Time O(N) Space==============================// 
+        // stack<char> st;
+        // for(auto ch:s)
+        // {
+        //     if(st.empty()) st.push(ch);
+        //     else if(st.top()==ch) st.pop();
+        //     else st.push(ch);
+        // }
+        // string ans = "";
+        // while(!st.empty()) {ans+=st.top(); st.pop();}
+        // reverse(ans.begin(),ans.end());
+        // return ans;
+        
+        //================================APPROACH 2: Two Pointer O(N) Time O(1) Space=================================///
+        string t = s;
+        int i = 0, j = 0; //remember that i index will be at a position such that from 0 to i there will be no repitition of any letter
+        while(j<s.length())
         {
-            if(st.empty()) st.push(ch);
-            else if(st.top()==ch) st.pop();
-            else st.push(ch);
+            t[i] = t[j]; //we kind of eliminate all the repeating letters in between i and j index
+            if(i>0 && t[i-1]==t[i])  //checking for two adjacent characters
+                i-=2;
+            ++i,++j;
         }
-        string ans = "";
-        while(!st.empty()) {ans+=st.top(); st.pop();}
-        reverse(ans.begin(),ans.end());
-        return ans;
+        return t.substr(0,i); 
     }
 };

8beb88136c940c0e1d890a80975eb5abf276f793
Rajarshi Karan <72148786+arkayyy@users.noreply.github.com>
2022-05-07 16:23:59 +0530

    Time: 116 ms (25.10%), Space: 23.5 MB (90.75%) - LeetHub

--- 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.cpp
+++ 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.cpp
@@ -3,7 +3,7 @@ public:
     //INTUITION: Similar to LC221-Maximal Sqaure in a matrix
     //Everytime we have a chance to expand the square we do that. And we keep adding the total no. of sqaures made using each 1 in the grid.
     
-    //See notes for visual explanation...
+    //See notes for visual explanation: https://github.com/arkayyy/leetcode-problems/blob/main/1277-count-square-submatrices-with-all-ones/HW%20Notes.pdf
     int countSquares(vector<vector<int>>& matrix) {
         int squares = 0;
         for(int i = 0; i<matrix.size(); i++)
@@ -14,5 +14,25 @@ public:
                 else squares+=(matrix[i][j]=1+min({matrix[i-1][j], matrix[i][j-1], matrix[i-1][j-1]}));
             }
         return squares;
+        
+        
+        //Space optimised DP (ERROR):-
+//         vector<vector<int>> dp(2,vector<int>(matrix[0].size(), 0));
+//         for(int j = 0; j<matrix[0].size(); j++)
+//             if(matrix[0][j]==1) {dp[0][j] = 1; ++squares;}
+    
+//         for(int i = 1; i<matrix.size(); i++)
+//         {
+//             for(int j = 0; j<matrix[0].size(); j++)
+//             {
+//                 if(matrix[i][j]==0) continue;
+//                 if(j==0) {dp[1][j] = 1;  ++squares; }
+//                 else squares+=(dp[1][j] = 1 + min({dp[0][j], dp[1][j-1], dp[0][j-1]}));
+//             }
+//             dp[0] = dp[1];
+//         }
+
+//         return squares;
     }
+    
 };

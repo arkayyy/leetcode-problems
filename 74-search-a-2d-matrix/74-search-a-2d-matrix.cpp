e6180f1fd8b0081315b76f7dc5d805fd55a7b074
Rajarshi Karan <72148786+arkayyy@users.noreply.github.com>
2022-05-08 16:42:01 +0530

    Time: 15 ms (6.51%), Space: 9.6 MB (53.60%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
@@ -14,14 +14,45 @@ public:
     return false;
 }
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        for(auto row: matrix)
-    {
-        if(target>=row[0] && target<=row[matrix[0].size()-1])
+        
+        //======================APPROACH 1: Select row and binary search O(MlogN) Time O(1) space======================//
+//         for(auto row: matrix)
+//         {
+//             if(target>=row[0] && target<=row[matrix[0].size()-1])
+//             {
+//                 if(binSearch(row,target)) return true;
+//                 else return false;
+//             }
+//         }
+//         return false;
+        
+        
+        
+        //=======================APPROACH 2: select both row and col by binary search O(logM + log N) Time O(1) Space=======================//
+        int n = matrix.size(), m = matrix[0].size();
+        int lo = 0, hi = n-1;
+        while(n>1 && lo<=hi)
         {
-            if(binSearch(row,target)) return true;
-            else return false;
+            int mid= lo+((hi-lo)>>1);
+            if(matrix[mid][m-1]==target) return true;
+            if(matrix[mid][m-1] < target)
+                lo = mid+1;
+            else
+                hi = mid - 1;
         }
-    }
-    return false;
+        if(lo>=n) return false;
+        int row = lo;
+        lo = 0, hi = m-1; 
+        while(m>1 && lo<=hi)
+        {
+            int mid= lo+((hi-lo)>>1);
+            if(matrix[row][mid]==target) return true;
+            if(matrix[row][mid] < target)
+                lo = mid+1;
+            else
+                hi =mid - 1;
+        }
+        
+        return m==1?matrix[lo][0]==target:false;
     }
 };

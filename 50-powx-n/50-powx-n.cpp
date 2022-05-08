83a6ebfac6534c0c4c3f05a973dc88c686aac9ae
Rajarshi Karan <72148786+arkayyy@users.noreply.github.com>
2022-05-08 18:56:41 +0530

    Time: 4 ms (27.36%), Space: 6 MB (45.70%) - LeetHub

---
+++ 50-powx-n/50-powx-n.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        //Iterative:-
+        // double res = 1;
+        // while(n)
+        // {
+        //     if(n&1) res = n<0?(res*(1/x)):(res*x);
+        //     x = x*x;
+        //     n = (n>>1);
+        // }
+        // return res;
+        
+        //Recursive:-
+        if(n<0) return (1/x)*myPow(1/x,-(n+1)); //extra case for negative powers
+        if(n==0) return 1;
+        if(n&1) return x* myPow(x*x, (n>>1));
+        return myPow(x*x, (n>>1));
+    }
+};

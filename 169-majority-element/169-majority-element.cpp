6a2f312a28dadd2b358317ece00741427b8c1744
Rajarshi Karan <72148786+arkayyy@users.noreply.github.com>
2022-05-08 22:22:17 +0530

    Time: 30 ms (34.51%), Space: 19.5 MB (59.88%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -7,18 +7,12 @@ public:
         
         //AALGO: We start with the first element and count as 1, as soon as a dissimilar element is found we decrease the count...
         //...as our count reaches 0, we reset the count to 1 and set the new element as candidate
-        if(nums.size()==1) return nums[0];
-        int cand = nums[0], cnt = 1;
-        for(int i = 1; i<nums.size(); i++)
+        int major = -1, freq = 0;
+        for(auto n: nums)
         {
-            if(cnt)
-            {
-                if(cand==nums[i]) cnt++;
-                else cnt--;
-            }
-            else if(cnt==0)
-                {cand = nums[i]; cnt =1;}
+            if(!freq) major = n, freq = 1;
+            else freq += n==major?1:-1;
         }
-        return cand;
+        return major;
     }
 };

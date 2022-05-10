dc11b0b94741550c58f52eeeb9c68e5b5a33f43b
Rajarshi Karan <72148786+arkayyy@users.noreply.github.com>
2022-05-10 22:59:57 +0530

    Time: 92 ms (49.19%), Space: 66.3 MB (7.95%) - LeetHub

--- 334-increasing-triplet-subsequence/334-increasing-triplet-subsequence.cpp
+++ 334-increasing-triplet-subsequence/334-increasing-triplet-subsequence.cpp
@@ -1,9 +1,11 @@
 class Solution {
 public:
+    //INTUITION: We check if the min. element on the left of any index is less than the current element, and the max element on the right is more than current element
+    //only in that case, it will satisfy the condition for increasing triplet subsequence...!
     bool increasingTriplet(vector<int>& nums) {
         if(nums.size()<3)return false;
-        vector<int> maxRight(nums.size());
-        vector<int> minLeft(nums.size());
+        vector<int> maxRight(nums.size()); //to access maximum element on right of any index
+        vector<int> minLeft(nums.size()); //to access min element on left of any index
         minLeft[0] = nums[0]; maxRight[nums.size()-1] = nums.back();
         for(int i = 1; i<nums.size();i++) {minLeft[i] = min(nums[i], minLeft[i-1]); maxRight[nums.size()-1-i] = max(nums[nums.size()-i-1],maxRight[nums.size()-i]); }
         

8cdcd1b97d9bb655106ff8d48235d9fd151f6944
Rajarshi Karan <72148786+arkayyy@users.noreply.github.com>
2022-05-11 13:46:04 +0530

    Time: 0 ms (100.00%), Space: 6 MB (58.60%) - LeetHub

--- 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.cpp
+++ 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.cpp
@@ -2,7 +2,7 @@ class Solution {
 public://Top down: i-> 0 to 5 AND n->n to 0
     int util(int i, int n, vector<char> &vowels, vector<vector<int>>& dp)
     {
-        if(n==0) {return 1;}
+        if(n==0) {return 1;} //base case
         
         int res = 0;
         for(int j = i; j<vowels.size(); j++)
@@ -20,8 +20,9 @@ public://Top down: i-> 0 to 5 AND n->n to 0
         
         //====================APPROACH 2: Space Optimized Tabulation DP O(5*N) Time O(1) Space ==============================//
         vector<int> dp(5,1);
+        dp[0] = 1; // similar to base case in recursion
         for(int i = 0; i<n; i++)
-            for(int j = 1; j<5; j++)
+            for(int j = 1; j<vowels.size(); j++)
                 dp[j]+=dp[j-1];
         return dp[4];
     }

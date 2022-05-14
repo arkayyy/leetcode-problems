910ab48e9b88fe76df550e55269e8853d06fa195
Rajarshi Karan <72148786+arkayyy@users.noreply.github.com>
2022-05-15 02:39:27 +0530

    Time: 25 ms (60.94%), Space: 9.9 MB (91.85%) - LeetHub

--- 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.cpp
+++ 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int maxVowels(string s, int k) {
+        //===============================APPROACH 1: Prefix vowel count approach (2pass) O(N) Time O(N) Space===============================//
         // vector<int> pre(s.size(),0);
         // pre[0] =(s[0]=='a'||s[0]=='e'||s[0]=='i'||s[0]=='o'||s[0]=='u');
         // int ans = 0;
@@ -11,6 +12,8 @@ public:
         //     ans = max(ans, pre[i+k-1] - (i-1<0?0:pre[i-1]));}
         // return ans;
         
+        //==============================APPROACH: Sliding window (1 pass) O(N) Time O(1) Space=========================================//
+        //ALGO: First count the vowels in first k length substring, and keep subtracting or adding the count of vowel by excluding one character from left and including one character from the right of the k window at every iteration
         int cnt = 0;
         for(int i = 0; i<k; i++) cnt+=(s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u');
         int ans = cnt;

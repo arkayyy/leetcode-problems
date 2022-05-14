1845dcd2bbccabfa27c85bfc7658089e366d77ce
Rajarshi Karan <72148786+arkayyy@users.noreply.github.com>
2022-05-15 04:02:59 +0530

    Time: 454 ms (6.50%), Space: 41.2 MB (49.50%) - LeetHub

--- 743-network-delay-time/743-network-delay-time.cpp
+++ 743-network-delay-time/743-network-delay-time.cpp
@@ -1,9 +1,12 @@
 class Solution {
 public:
-    //======================APPROACH 1: Simple BFS based Djikstra to reach all nodes! TC: O(NlogN + E) SC:O(N+E)==========================//
+    
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
+        
+        //======================APPROACH 3: Simple BFS based Djikstra to reach all nodes! TC: O(NlogN + E) SC:O(N+E)==========================//
+        
         //unordered_map<int,vector<pair<int,int>>> adj;
-        vector<long long> dis(n+1,LLONG_MAX);
+        //vector<long long> dis(n+1,LLONG_MAX);
 //         vector<bool> visited(n+1,false);
         
 //         for(auto t: times)
@@ -30,14 +33,36 @@ public:
 //         return ans == LLONG_MAX ? -1 : ans;
         
         //======================APPROACH 2: Bellman Ford Algo TC: avg: O(NE) worst: O(N^3) SC:(N)==========================//
-        dis[k] = 0;
-        for(int i = 1; i<=n; i++)
-            for(auto t: times)
-               {if(dis[t[0]]==LLONG_MAX) continue;
-                dis[t[1]] = min(dis[t[1]], dis[t[0]]+t[2]);}
-        int ans = *max_element(dis.begin() + 1, dis.end());
-        return ans == LLONG_MAX ? -1 : ans;
+        // vector<long long> dis(n+1,LLONG_MAX);
+        // dis[k] = 0;
+        // for(int i = 1; i<=n; i++)
+        //     for(auto t: times)
+        //        {if(dis[t[0]]==LLONG_MAX) continue;
+        //         dis[t[1]] = min(dis[t[1]], dis[t[0]]+t[2]);}
+        // int ans = *max_element(dis.begin() + 1, dis.end());
+        // return ans == LLONG_MAX ? -1 : ans;
         
         
+        //=======================APPROACH 1: Floyd Warshall Algo TC:O(N^3) SC:O(N^2) ==================================//
+        vector<vector<long long>> dis(n+1, vector<long long>(n+1, LLONG_MAX));
+        for(auto t: times) dis[t[0]][t[1]] = t[2];
+        for(int a = 1; a<=n; a++) dis[a][a] = 0;
+        long long ans = 0;
+        
+        for(int c = 1; c<=n; c++)
+            for(int a = 1; a<=n; a++)
+                for(int b = 1; b<=n;b++)
+                {
+                    if(dis[a][c]==LLONG_MAX || dis[c][b]==LLONG_MAX) continue;
+                    if(dis[a][b] > dis[a][c] + dis[c][b]) //relaxation condition
+                        dis[a][b] = dis[a][c] + dis[c][b];
+                }
+
+        for(int a = 1; a<=n; a++)
+        {    
+            if(dis[k][a]==LLONG_MAX) return -1;
+            ans = max(ans, dis[k][a]);
+        }
+        return (int)ans;
     }
 };

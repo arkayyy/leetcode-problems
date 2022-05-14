b8db1aeccb7f65bc34d066088dd39b27d565d1de
Rajarshi Karan <72148786+arkayyy@users.noreply.github.com>
2022-05-15 04:03:57 +0530

    Time: 175 ms (44.71%), Space: 43.6 MB (27.73%) - LeetHub

--- 743-network-delay-time/743-network-delay-time.cpp
+++ 743-network-delay-time/743-network-delay-time.cpp
@@ -3,34 +3,28 @@ public:
     
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
         
-        //======================APPROACH 3: Simple BFS based Djikstra to reach all nodes! TC: O(NlogN + E) SC:O(N+E)==========================//
+        //=======================APPROACH 1: Floyd Warshall Algo TC:O(N^3) SC:O(N^2) ==================================//
+//         vector<vector<long long>> dis(n+1, vector<long long>(n+1, LLONG_MAX));
+//         for(auto t: times) dis[t[0]][t[1]] = t[2];
+//         for(int a = 1; a<=n; a++) dis[a][a] = 0;
+//         long long ans = 0;
         
-        //unordered_map<int,vector<pair<int,int>>> adj;
-        //vector<long long> dis(n+1,LLONG_MAX);
-//         vector<bool> visited(n+1,false);
-        
-//         for(auto t: times)
-//             adj[t[0]].push_back({t[1],t[2]});
-        
-//         dis[k] = 0;
-        
-//         priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>> > pq;
-//         pq.emplace(0, k);
-        
-//         while (!pq.empty()) {
-//             int u = pq.top().second; pq.pop();
-// 			if (visited[u]) continue;
-// 			visited[u] = true;
-//             for (auto& a : adj[u]) {
-//                 int v = a.first, w = a.second;
-//                 if (dis[v] > dis[u] + w) { //checking if it can be relaxed (relaxation condition)
-//                     dis[v] = dis[u] + w;
-//                     pq.emplace(dis[v], v);
+//         for(int c = 1; c<=n; c++)
+//             for(int a = 1; a<=n; a++)
+//                 for(int b = 1; b<=n;b++)
+//                 {
+//                     if(dis[a][c]==LLONG_MAX || dis[c][b]==LLONG_MAX) continue;
+//                     if(dis[a][b] > dis[a][c] + dis[c][b]) //relaxation condition
+//                         dis[a][b] = dis[a][c] + dis[c][b];
 //                 }
-//             }
+
+//         for(int a = 1; a<=n; a++)
+//         {    
+//             if(dis[k][a]==LLONG_MAX) return -1;
+//             ans = max(ans, dis[k][a]);
 //         }
-//         int ans = *max_element(dis.begin() + 1, dis.end());
-//         return ans == LLONG_MAX ? -1 : ans;
+//         return (int)ans;
+        
         
         //======================APPROACH 2: Bellman Ford Algo TC: avg: O(NE) worst: O(N^3) SC:(N)==========================//
         // vector<long long> dis(n+1,LLONG_MAX);
@@ -43,26 +37,34 @@ public:
         // return ans == LLONG_MAX ? -1 : ans;
         
         
-        //=======================APPROACH 1: Floyd Warshall Algo TC:O(N^3) SC:O(N^2) ==================================//
-        vector<vector<long long>> dis(n+1, vector<long long>(n+1, LLONG_MAX));
-        for(auto t: times) dis[t[0]][t[1]] = t[2];
-        for(int a = 1; a<=n; a++) dis[a][a] = 0;
-        long long ans = 0;
+        //======================APPROACH 3: Simple BFS based Djikstra to reach all nodes! TC: O(NlogN + E) SC:O(N+E)==========================//
         
-        for(int c = 1; c<=n; c++)
-            for(int a = 1; a<=n; a++)
-                for(int b = 1; b<=n;b++)
-                {
-                    if(dis[a][c]==LLONG_MAX || dis[c][b]==LLONG_MAX) continue;
-                    if(dis[a][b] > dis[a][c] + dis[c][b]) //relaxation condition
-                        dis[a][b] = dis[a][c] + dis[c][b];
+        unordered_map<int,vector<pair<int,int>>> adj;
+        vector<long long> dis(n+1,LLONG_MAX);
+        vector<bool> visited(n+1,false);
+        
+        for(auto t: times)
+            adj[t[0]].push_back({t[1],t[2]});
+        
+        dis[k] = 0;
+        
+        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>> > pq;
+        pq.emplace(0, k);
+        
+        while (!pq.empty()) {
+            int u = pq.top().second; pq.pop();
+			if (visited[u]) continue;
+			visited[u] = true;
+            for (auto& a : adj[u]) {
+                int v = a.first, w = a.second;
+                if (dis[v] > dis[u] + w) { //checking if it can be relaxed (relaxation condition)
+                    dis[v] = dis[u] + w;
+                    pq.emplace(dis[v], v);
                 }
-
-        for(int a = 1; a<=n; a++)
-        {    
-            if(dis[k][a]==LLONG_MAX) return -1;
-            ans = max(ans, dis[k][a]);
+            }
         }
-        return (int)ans;
+        int ans = *max_element(dis.begin() + 1, dis.end());
+        return ans == LLONG_MAX ? -1 : ans;
+    
     }
 };

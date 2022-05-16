063ddff8a861b2f91fd1283a9c242e22aca55e41
Rajarshi Karan <72148786+arkayyy@users.noreply.github.com>
2022-05-16 15:47:08 +0530

    Time: 156 ms (31.31%), Space: 30.2 MB (33.23%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    //INTUITION & ALGO: Simple BFS from (0,0) to (n-1,n-1) through a path of 0's only!
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
         if(grid[0][0]==1) return -1;
         int dx[8] = {0,0,1,1,1,-1,-1,-1};
@@ -19,9 +20,14 @@ public:
             q.pop();
             ++ans;
             for(int i = 0; i<8; ++i)
-                {
+            {
                 if(x+dx[i]>=0 && x+dx[i]<n && y+dy[i]>=0 && y+dy[i]<m && grid[x+dx[i]][y+dy[i]]==0 &&  !visited[x+dx[i]][y+dy[i]])
-                   { if(x+dx[i]==n-1 && y+dy[i]==m-1) return steps+1; visited[x+dx[i]][y+dy[i]] = true; q.push({x+dx[i], y+dy[i], steps+1});}}
+                {   
+                    if(x+dx[i]==n-1 && y+dy[i]==m-1) return steps+1; 
+                    visited[x+dx[i]][y+dy[i]] = true; 
+                    q.push({x+dx[i], y+dy[i], steps+1});
+                }
+            }
         }
         
         return -1;

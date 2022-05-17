4d79dfaf26a97806d5997cd5b2cfe354a241012a
Rajarshi Karan <72148786+arkayyy@users.noreply.github.com>
2022-05-17 15:44:01 +0530

    Time: 48 ms (18.97%), Space: 13.9 MB (42.81%) - LeetHub

---
+++ 547-number-of-provinces/547-number-of-provinces.cpp
@@ -0,0 +1,60 @@
+class Solution {
+public:
+    //1-indexed disjoint set code
+    class disjoint_set
+    {
+        private:
+        int n;
+        vector<int> parent, rank;
+        
+        public:
+        disjoint_set(int n)
+        {
+            this->n = n;
+            rank = vector<int>(n+1,0);
+            parent = vector<int> (n+1,0);
+            for(int i =1; i<=n; i++)
+                parent[i] = i;
+        }
+        
+        int findParent(int v)
+        {
+            if(v==parent[v]) return v;
+            return parent[v] = findParent(parent[v]);
+        }
+        
+        void unionBoth(int u, int v)
+        {
+            u = findParent(u);
+            v = findParent(v);
+            if(rank[u]<rank[v])
+                parent[u] = v;
+            else if(rank[v]<rank[u])
+                parent[v] = u;
+            else
+                parent[v] = u, ++rank[u];
+        }
+        
+        int countProvinces()
+        {
+            unordered_set<int> s(parent.begin(),parent.end());
+            return s.size();
+        }
+    };
+    
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int n = isConnected[0].size();
+        disjoint_set d(n); //will create nodes from 1 to n inclusive
+        for(auto i = 0; i<isConnected.size(); ++i)
+        {
+            for(auto j = 0; j<n; j++)
+                if(isConnected[i][j])
+                    d.unionBoth(i+1,j+1);
+        }
+        
+        for(int i = 1; i<=n; i++)
+            d.findParent(i);
+        
+        return d.countProvinces()-1;
+    }
+};

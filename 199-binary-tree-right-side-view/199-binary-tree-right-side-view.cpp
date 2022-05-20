74a6314b7fc6e463c5b9fe878ac8fb2bc1625ebe
Rajarshi Karan <72148786+arkayyy@users.noreply.github.com>
2022-05-21 02:37:41 +0530

    Time: 11 ms (10.14%), Space: 12 MB (68.88%) - LeetHub

--- 199-binary-tree-right-side-view/199-binary-tree-right-side-view.cpp
+++ 199-binary-tree-right-side-view/199-binary-tree-right-side-view.cpp
@@ -11,24 +11,42 @@
  */
 class Solution {
 public:
+    void dfs(TreeNode* root,int level, vector<int>& ans)
+    {
+        //IDEA for DFS: the right boundary would consist of the rightmost node and their right child of every level. Except when the right child is NULL, we go to left child, and again try to extract the rightmost nodes at every level below that.
+        if(!root) return;  
+        
+        if(ans.size()<=level) ans.emplace_back(root->val);
+        
+        //if(!root->left && !root->right) {ans.emplace_back(root->val); return;}
+        dfs(root->right, level+1, ans);
+        
+        dfs(root->left, level+1, ans);  
+    }
+    
     vector<int> rightSideView(TreeNode* root) {
         if(!root) return {};
         //BFS Level Order:-
+        // vector<int> ans;
+        // queue<TreeNode*> q;
+        // q.push(root);
+        // while(!q.empty())
+        // {
+        //     int len = q.size();
+        //     TreeNode* t;
+        //     for(int i = 0; i<len; i++)
+        //     {
+        //         t = q.front(); q.pop();
+        //         if(t->left) q.push(t->left);
+        //         if(t->right) q.push(t->right);
+        //     }
+        //     ans.emplace_back(t->val); //at every level, 't' will finally contain last node of that level
+        // }
+        // return ans;
+        
+        //Recursive DFS:-
         vector<int> ans;
-        queue<TreeNode*> q;
-        q.push(root);
-        while(!q.empty())
-        {
-            int len = q.size();
-            TreeNode* t;
-            for(int i = 0; i<len; i++)
-            {
-                t = q.front(); q.pop();
-                if(t->left) q.push(t->left);
-                if(t->right) q.push(t->right);
-            }
-            ans.emplace_back(t->val);
-        }
+        dfs(root,0,ans);
         return ans;
     }
 };

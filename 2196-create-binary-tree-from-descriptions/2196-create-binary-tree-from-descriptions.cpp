40dca019769fe9c67f27795971517612bcb19ecc
Rajarshi Karan <72148786+arkayyy@users.noreply.github.com>
2022-05-21 04:08:33 +0530

    Time: 1191 ms (73.15%), Space: 291 MB (44.79%) - LeetHub

---
+++ 2196-create-binary-tree-from-descriptions/2196-create-binary-tree-from-descriptions.cpp
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* createBinaryTree(vector<vector<int>>& descriptions) {
+        unordered_map<int,int> isRoot;//we will take convention: 2 for parent, 1 for child. Any child child can't be a root.
+        unordered_map<int,TreeNode*> mp;
+        for(auto d: descriptions)
+        {
+            if(mp.find(d[0])==mp.end()) {mp[d[0]] = new TreeNode(d[0]);}
+            
+            if(mp.find(d[1])==mp.end()) {mp[d[1]] = new TreeNode(d[1]);}
+            
+            if(d[2])
+                mp[d[0]]->left = mp[d[1]];
+            if(!d[2])
+                mp[d[0]]->right = mp[d[1]];
+               
+            if(isRoot.find(d[0])==isRoot.end()) isRoot[d[0]] = 2; //marked as parent only if previously never marked as child
+            isRoot[d[1]] = 1; //marked as child
+        }
+               
+        for(auto node: isRoot)
+            if(node.second==2) //found root node (only node which is not a child node)
+               return mp[node.first];
+               
+        return NULL;
+    }
+};

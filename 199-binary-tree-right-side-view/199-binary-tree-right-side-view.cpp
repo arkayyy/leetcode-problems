0f6db7f7e17f239e6c0e8973ddb7cfd1e6a93aee
Rajarshi Karan <72148786+arkayyy@users.noreply.github.com>
2022-05-21 02:41:19 +0530

    Time: 7 ms (38.01%), Space: 11.9 MB (68.88%) - LeetHub

--- 199-binary-tree-right-side-view/199-binary-tree-right-side-view.cpp
+++ 199-binary-tree-right-side-view/199-binary-tree-right-side-view.cpp
@@ -13,10 +13,12 @@ class Solution {
 public:
     void dfs(TreeNode* root,int level, vector<int>& ans)
     {
-        //IDEA for DFS: the right boundary would consist of the rightmost node and their right child of every level. Except when the right child is NULL, we go to left child, and again try to extract the rightmost nodes at every level below that.
+        //IDEA for DFS: the right boundary would consist of the rightmost node and their right child of every level. 
+        //Except when the right child is NULL, we go to left child, and again try to extract the rightmost nodes at every level below that.
         if(!root) return;  
         
-        if(ans.size()==level) ans.emplace_back(root->val); //IMP step: since level initially starts from 0 (0 indexed), so just when the level reaches the value ans.size(), it means current recursive call candidate holds the value for rightmost node of this new level
+        if(ans.size()==level) ans.emplace_back(root->val); //IMP step: since level initially starts from 0 (0 indexed), 
+        //so just when the level reaches the value ans.size(), it means current recursive call candidate holds the value for rightmost node of this new level
         
         //if(!root->left && !root->right) {ans.emplace_back(root->val); return;}
         dfs(root->right, level+1, ans);

3f54bd66d933869d4f1f426b62490b7cab22fa65
Rajarshi Karan <72148786+arkayyy@users.noreply.github.com>
2022-05-21 01:51:45 +0530

    Time: 59 ms (61.53%), Space: 42.1 MB (12.28%) - LeetHub

--- 1315-sum-of-nodes-with-even-valued-grandparent/1315-sum-of-nodes-with-even-valued-grandparent.cpp
+++ 1315-sum-of-nodes-with-even-valued-grandparent/1315-sum-of-nodes-with-even-valued-grandparent.cpp
@@ -11,6 +11,7 @@
  */
 class Solution {
 public:
+    //INTUITION: Simple BFS!
     int sumEvenGrandparent(TreeNode* root) {
         queue<TreeNode*> q;
         int ans = 0;
@@ -18,12 +19,14 @@ public:
         while(!q.empty())
         {
             TreeNode* t = q.front(); q.pop();
-            if(t->left){ 
+            if(t->left){
+                //Checking both grandchilds in left subtree:-
                 if(t->left->left && (t->val&1)==0) ans+=t->left->left->val; 
                 if(t->left->right && (t->val&1)==0) ans+=t->left->right->val; 
                 q.push(t->left); 
             }
             if(t->right) {
+                //Checking both grandchilds in right subtree:-
                 if(t->right->left && (t->val&1)==0) ans+=t->right->left->val; 
                 if(t->right->right && (t->val&1)==0) ans+=t->right->right->val; 
                 q.push(t->right); 

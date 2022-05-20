d6e3ee1dc83f5608ca96c920c1b6abd72b07adc1
Rajarshi Karan <72148786+arkayyy@users.noreply.github.com>
2022-05-21 03:34:41 +0530

    Time: 154 ms (70.88%), Space: 86.4 MB (11.26%) - LeetHub

--- 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.cpp
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.cpp
@@ -11,25 +11,35 @@
  */
 class Solution {
 public:
-    int goodNodes(TreeNode* root) {
-        int violations = 0, totalNodes = 0;
-        queue<pair<TreeNode*,int>> q;
-        q.push({root, root->val});
-        while(!q.empty())
-        {
-            TreeNode* t = q.front().first; int maxFromRoot = q.front().second; q.pop();
-            ++totalNodes;
-            if(t->left)
-            {
-                if(t->left->val<maxFromRoot) ++violations;
-                 q.push({t->left, max(t->left->val, maxFromRoot)}); 
-            }
-            if(t->right)
-            {
-                if(t->right->val<maxFromRoot) ++violations;
-                q.push({t->right, max(t->right->val, maxFromRoot)});
-            }
-        }
-        return totalNodes-violations;
+    //INTUITION: Let's count the violating nodes or 'not good nodes' using a reverse logic.
+    
+    //ALGO: The nodes that are not good, it means that in between root and that node, there's an element that is greater than that node's value.
+    //So we keep storing the maximum node value occuring between root and a specific node in the queue (extracted as maxFromRoot).
+    //If this maximum value > the current node value, then this is a violating node, i.e. NOT A GOOD NODE.
+    //We count total number of nodes and subtract number of violating nodes to get the no. of good nodes.
+    int goodNodes(TreeNode* root, int maxFromRoot = INT_MIN) {
+        // int violations = 0, totalNodes = 0;
+        // queue<pair<TreeNode*,int>> q;
+        // q.push({root, root->val});
+        // while(!q.empty())
+        // {
+        //     TreeNode* t = q.front().first; int maxFromRoot = q.front().second; q.pop();
+        //     ++totalNodes;
+        //     if(t->left)
+        //     {
+        //         if(t->left->val<maxFromRoot) ++violations; 
+        //          q.push({t->left, max(t->left->val, maxFromRoot)}); 
+        //     }
+        //     if(t->right)
+        //     {
+        //         if(t->right->val<maxFromRoot) ++violations;
+        //         q.push({t->right, max(t->right->val, maxFromRoot)});
+        //     }
+        // }
+        // return totalNodes-violations;
+        
+        
+        //=======================APPROACH 2: One Liner DFS===========================================//
+        return root?((root->left?goodNodes(root->left, max({root->left->val, maxFromRoot, root->val})):0) + (root->right?goodNodes(root->right, max({root->right->val, maxFromRoot, root->val})):0) + (root->val>=maxFromRoot) ):0;
     }
 };

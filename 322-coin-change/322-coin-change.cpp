89931beea8f681359fdb5d508f32a55f99bc2bf5
Rajarshi Karan <72148786+arkayyy@users.noreply.github.com>
2022-05-21 08:36:42 +0530

    Time: 307 ms (8.48%), Space: 22.7 MB (23.30%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -15,12 +15,14 @@ public:
     }
     int coinChange(vector<int>& coins, int amount) {
         sort(coins.begin(), coins.end()); //sorting for greedily choosing least no. of coins first
+        
+        //===================Approach 1: Recursion + Memoization TC: O(n*amount) SC:O(n*amount)==========================//
         //vector<vector<int>> dp(coins.size()+1,vector<int>(amount+1));
         // int ans = util(amount, coins.size()-1, coins, dp);
         // return ans==INT_MAX-1?-1:ans;
         
         
-        //APPROACH 2: DP Tabulation TC:O(n*amount) SC:O(n*amount) no aux space
+        //======================APPROACH 2: DP Tabulation TC:O(n*amount) SC:O(n*amount) no aux space=====================//
 //         for(int j = 0; j<=amount; j++) dp[0][j] = INT_MAX-1;
 //         for(int i = 0; i<=coins.size(); ++i) dp[i][0] = 0;
         
@@ -34,7 +36,7 @@ public:
 //         return dp[coins.size()][amount]==INT_MAX-1?-1:dp[coins.size()][amount];
         
         
-        //Space optimised DP Tabulation:-
+        //=====================Space optimised DP Tabulation (since we only need current and previous row) SC:O(2*amount)=======================//
         vector<vector<int>> dp(2, vector<int>(amount+1));
         for(int j = 0; j<=amount; ++j) dp[0][j] = INT_MAX-1;
         dp[0][0] = 0;

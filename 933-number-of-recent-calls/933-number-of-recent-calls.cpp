e74a3869f2369c7c3f337ea97d3297ad942e3426
Rajarshi Karan <72148786+arkayyy@users.noreply.github.com>
2022-05-22 05:37:27 +0530

    Time: 140 ms (92.16%), Space: 58.5 MB (6.39%) - LeetHub

--- 933-number-of-recent-calls/933-number-of-recent-calls.cpp
+++ 933-number-of-recent-calls/933-number-of-recent-calls.cpp
@@ -1,18 +1,26 @@
 class RecentCounter {
 public:
     vector<int> times;
+    queue<int> q;
     RecentCounter() {
         times = vector<int>();
         times.reserve(1e4);
     }
     
     int ping(int t) {
-        times.push_back(t);
-        int i = times.size()-1;
-        int res = 0;
-        while(i>=0 && (times[i]>=t-3000 && times[i]<=t))
-            ++res, --i;
-        return res;
+        //Vector based soln:-
+        // times.push_back(t);
+        // int i = times.size()-1;
+        // int res = 0;
+        // while(i>=0 && (times[i]>=t-3000 && times[i]<=t))
+        //     ++res, --i;
+        // return res;
+        
+        //Queue based soln.:-
+        q.push(t);
+        while(q.front() < t-3000) q.pop(); //we don't care about the previous values and we can discard them, 
+        //as the next value is always going to be higher than the highest time in the queue
+        return q.size();
     }
 };
 

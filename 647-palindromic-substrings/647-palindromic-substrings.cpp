a211462854072390eab5578210aec674f4a34f34
Rajarshi Karan <72148786+arkayyy@users.noreply.github.com>
2022-05-22 06:22:24 +0530

    Time: 36 ms (28.59%), Space: 20.2 MB (12.05%) - LeetHub

--- 647-palindromic-substrings/647-palindromic-substrings.cpp
+++ 647-palindromic-substrings/647-palindromic-substrings.cpp
@@ -12,7 +12,7 @@ public:
         return dp[i][j] = s[i]==s[j]?checkPalindrome(s,i+1,j-1, dp):0;
     }
     int countSubstrings(string s) {
-        //==============================APPROACH 1: Recursion + Memoization O(N^2) Time O(N^2) + recusrion aux Space============================//
+        //==========================APPROACH 1: Recursion + Memoization [O(N^2)] Time and [O(N^2) + recusrion aux] Space======================//
         // vector<vector<int>> dp(s.size(), vector<int>(s.size(), -1)); //dp[i][j] = 1 or 0, denotes whether the substring in between i and j indices is plaindromic or not
         // int ans = 0;
         // for(int i = 0; i<s.size(); ++i)

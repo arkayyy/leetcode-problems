83ce6014a567f128629c8c103c748383f18ed850
Rajarshi Karan <72148786+arkayyy@users.noreply.github.com>
2022-05-24 04:28:40 +0530

    Time: 12 ms (23.43%), Space: 8.4 MB (42.65%) - LeetHub

--- 206-reverse-linked-list/206-reverse-linked-list.cpp
+++ 206-reverse-linked-list/206-reverse-linked-list.cpp
@@ -14,10 +14,10 @@ public:
         ListNode* prev = NULL, *next = NULL, *curr = head;
         while(curr)
         {
-            next = curr->next;
-            curr->next = prev;
-            prev = curr;
-            curr = next;
+            next = curr->next; //storing which was already the next of current node
+            curr->next = prev; //switching the next node of current node to the previous node(which we keep updating at every step)
+            prev = curr; //obvio, current node is going to be next node's previous node
+            curr = next; //moving forward curr in the original given LL order
         }
         return prev;
     }

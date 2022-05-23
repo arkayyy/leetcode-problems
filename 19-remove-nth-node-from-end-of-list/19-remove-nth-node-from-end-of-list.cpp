c53c72585ffcf1a099a078f603e88028c2be32bc
Rajarshi Karan <72148786+arkayyy@users.noreply.github.com>
2022-05-24 05:19:34 +0530

    Time: 7 ms (50.48%), Space: 10.8 MB (34.18%) - LeetHub

--- 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.cpp
+++ 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.cpp
@@ -10,13 +10,18 @@
  */
 class Solution {
 public:
+    //INTUITION: We take two pointers, and move one forward for n times, and then move both pointers ahead at same speed.
+    //So when the ahead pointer reaches the end of LL, the slower one would be at the nth node from end of LL 
     ListNode* removeNthFromEnd(ListNode* head, int n) { if(!head->next) return NULL;
         ListNode *prev = NULL, *p1 = head, *p2 = head;
         for(int i = 1; i<=n; i++)
             p2 = p2->next;
         while(p2) {prev = p1; p1 = p1->next; p2 = p2->next;}
+        
+        //EDGE CASE ALERT: if we need to delete the starting node of the LL
         if(!prev) return head->next; //prev would still be NULL after initialising only in the case if the while loop above never runs, which denotes that p2 was already NULL after moving forward n nodes,
         //which means its the last node from the ending of the LL(i.e. the starting node of the LL) that we want to delete!
+        
         prev->next = prev->next->next; 
         return head;
     }

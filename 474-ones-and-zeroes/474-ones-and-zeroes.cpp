b49b3c360b3cdd421482b84f68f45fa85b250c0e
Rajarshi Karan <72148786+arkayyy@users.noreply.github.com>
2022-05-24 03:27:18 +0530

    Time: 558 ms (27.36%), Space: 35.7 MB (50.65%) - LeetHub

--- 474-ones-and-zeroes/474-ones-and-zeroes.cpp
+++ 474-ones-and-zeroes/474-ones-and-zeroes.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
     int dp[601][101][101]; //memoizing using arrays is faster since memory is allocated statically here
-    int util(int i, int m, int n, vector<int>&ones, vector<int>&zeros, int one, int zero)
+    int util(int i, int m, int n, vector<int>&ones, vector<int>&zeros)
     {
-        if(i<0 || zero>m || one>n) return 0;
-        if(dp[i][zero][one]!=-1) return dp[i][zero][one];
+        if(i<0 || m<0 || n<0) return 0;
+        if(dp[i][m][n]!=-1) return dp[i][m][n];
         
-        if(zero+zeros[i]<=m && one+ones[i]<=n)
-            return dp[i][zero][one] = max(1+util(i-1, m, n, ones, zeros, one+ones[i], zero+zeros[i]) ,  util(i-1,m,n,ones, zeros, one, zero));
+        if(m-zeros[i]>=0 && n-ones[i]>=0)
+            return dp[i][m][n] = max(1+util(i-1, m-zeros[i], n-ones[i], ones, zeros) ,  util(i-1,m,n,ones, zeros));
         
-        return dp[i][zero][one] = util(i-1,m,n,ones, zeros, one, zero);
+        return dp[i][m][n] = util(i-1,m,n,ones, zeros);
     }
     int findMaxForm(vector<string>& strs, int m, int n) {
-        //vector<vector<vector<int>>> dp(600, vector<vector<int>> (100, vector<int>(100,-1))); -> NOTE: using vector for memoizing gives TLE here.
+        
         memset(dp,-1,sizeof(dp));
         vector<int> ones(strs.size()), zeros(strs.size());
         for(int i = 0; i<strs.size(); ++i)
@@ -21,6 +21,8 @@ public:
             for(auto c: strs[i]) {if(c=='0') ++z; else ++o;}
             zeros[i] = z, ones[i] = o;
         }
-        return util(strs.size()-1, m, n, ones, zeros, 0, 0);
+        return util(strs.size()-1, m, n, ones, zeros);
+        
+        
     }
 };

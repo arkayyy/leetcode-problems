8f06e7bf96c4e4121331d14ad33ac2a6e27af436
Rajarshi Karan <72148786+arkayyy@users.noreply.github.com>
2022-05-24 04:19:21 +0530

    Time: 752 ms (14.81%), Space: 35.8 MB (43.10%) - LeetHub

--- 474-ones-and-zeroes/474-ones-and-zeroes.cpp
+++ 474-ones-and-zeroes/474-ones-and-zeroes.cpp
@@ -13,7 +13,7 @@ public:
     }
     int findMaxForm(vector<string>& strs, int m, int n) {
         
-        memset(dp,-1,sizeof(dp));
+        // memset(dp,-1,sizeof(dp));
         vector<int> ones(strs.size()), zeros(strs.size());
         for(int i = 0; i<strs.size(); ++i)
         {
@@ -21,8 +21,20 @@ public:
             for(auto c: strs[i]) {if(c=='0') ++z; else ++o;}
             zeros[i] = z, ones[i] = o;
         }
-        return util(strs.size()-1, m, n, ones, zeros);
+        // return util(strs.size()-1, m, n, ones, zeros);
         
         
+        //Tabulation DP:-
+        memset(dp,0,sizeof(dp)); //base case (just for visualising, actual variable names may be taken differently): 
+        //whenever i==0 || m==0 || n==0 then dp[i][m][n] = 0. So by default setting all as 0.
+        for(int i = 1; i<=strs.size(); ++i)
+            for(int j = 0; j<=m; ++j)
+                for(int k = 0; k<=n; ++k)
+                {
+                    dp[i][j][k] = dp[i-1][j][k];
+                    if(j-zeros[i-1]>=0 && k-ones[i-1]>=0)
+                        dp[i][j][k] = max(1+dp[i-1][j-zeros[i-1]][k-ones[i-1]], dp[i-1][j][k]);
+                }
+        return dp[strs.size()][m][n];
     }
 };

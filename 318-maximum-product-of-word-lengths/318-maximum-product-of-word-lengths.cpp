c0a4ccec9b308aeee8cd4fb15ca9880ff2cbdb28
Rajarshi Karan <72148786+arkayyy@users.noreply.github.com>
2022-05-29 09:40:45 +0530

    Time: 832 ms (10.03%), Space: 23.5 MB (16.58%) - LeetHub

---
+++ 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    unordered_map<int,vector<int>> mp;
+    bool commChar(vector<int>& freq1, vector<int>&freq2)
+    {
+        for(int i = 0; i<26; ++i)
+            if(freq1[i]>0 && freq2[i]>0) return true;
+        return false;
+    }
+    int maxProduct(vector<string>& words) {
+        for(int i = 0; i<words.size(); ++i)
+        {
+            mp[i] = vector<int>(26,0);
+            for(auto c: words[i]) ++mp[i][c-'a'];
+        }
+        
+        int ans = 0;
+        
+        for(int i = 0; i<words.size(); ++i)
+            for(int j = 0; j<words.size(); ++j)
+                if(i!=j && !commChar(mp[i],mp[j]))
+                    ans = max(ans, (int)words[i].size() * (int)words[j].size());
+        
+        return ans;
+    }
+};

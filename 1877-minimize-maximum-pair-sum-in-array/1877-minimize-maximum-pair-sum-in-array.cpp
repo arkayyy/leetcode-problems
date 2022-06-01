6d36073e4f90dad0a59936a596a6f50f6c8dfb77
Rajarshi Karan <72148786+arkayyy@users.noreply.github.com>
2022-06-01 23:14:10 +0530

    Time: 346 ms (34.31%), Space: 96.2 MB (64.17%) - LeetHub

--- 1877-minimize-maximum-pair-sum-in-array/1877-minimize-maximum-pair-sum-in-array.cpp
+++ 1877-minimize-maximum-pair-sum-in-array/1877-minimize-maximum-pair-sum-in-array.cpp
@@ -1,5 +1,9 @@
 class Solution {
 public:
+    //INTUITION: Sum of distant elements would always be less than sum of nearby elements. e.g. sum of 1 and 4 is lesser than sum of 3 and 4. 
+    //So our target will be to consider sums of as distant elements as possible.
+    
+    //ALGO: We sort the array in ascending order and keep taking one element from start and one from end, and do their sum, so that most distant elements sum are considered.
     int minPairSum(vector<int>& nums) {
         sort(nums.begin(),nums.end());
         int ans = 0;

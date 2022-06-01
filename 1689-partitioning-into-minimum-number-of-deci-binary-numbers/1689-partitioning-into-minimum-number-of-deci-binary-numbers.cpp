87d09190d236dbc86202e9f19690710e1371e6e8
Rajarshi Karan <72148786+arkayyy@users.noreply.github.com>
2022-06-01 23:24:18 +0530

    Time: 53 ms (22.81%), Space: 13.6 MB (68.14%) - LeetHub

--- 1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.cpp
+++ 1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    //INTUITION: The min. no of decibinary needed would obviously be denoted by the largest digit in the given number, becuase we'll need to add up that man 1's to get that digit
     int minPartitions(string n) {
         int ans = 0;
         for(auto c:n) ans = max(ans,c-'0');

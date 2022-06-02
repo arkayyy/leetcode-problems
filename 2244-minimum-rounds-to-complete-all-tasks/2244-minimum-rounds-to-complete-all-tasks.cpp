6534cec5d331c50064a958a4602066c8019ab8d4
Rajarshi Karan <72148786+arkayyy@users.noreply.github.com>
2022-06-02 15:13:42 +0530

    Time: 306 ms (23.32%), Space: 103.6 MB (86.34%) - LeetHub

--- 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.cpp
+++ 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.cpp
@@ -1,12 +1,20 @@
 class Solution {
 public:
+    //INTUITION: We can greedily want to minimize the no. of rounds by taking max no. of 3 task groups possible.
+    //Now whatever the number of tasks be, it can either be divisible by 3, or leave a remainder of 1 or 2 when divided by 3.
+    
+    //Say 8 tasks are there ########...we can group them like ###/###/##, so remainder of 2, and 2 tasks are grouped into an extra group
+    //Say 7 tasks are there #######.. we can divide like ###/###/#, that would leave 1 task ungrouped, so we would group it like ###/##/##
+    //So in both cases of remainder 1 or 2 when divided by 3, we need to create an extra group apart from the 3-task groups
+    
+    
     int minimumRounds(vector<int>& tasks) {
         unordered_map<int,int> mp;
         for(auto t:tasks) ++mp[t];
         int ans = 0;
         for(auto m:mp) {
             if(m.second==1) return -1;
-            ans+=m.second/3 + (m.second%3!=0 ? 1 : 0);
+            ans+=m.second/3 + (m.second%3!=0 ? 1 : 0); //adding 1 extra group only when dividing by 3 leaves a remainder(either 1 or 2)
         }
         return ans;
     }

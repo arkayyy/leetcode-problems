cddc28cf6d952c236151d8d992a561782e18ff70
Rajarshi Karan <72148786+arkayyy@users.noreply.github.com>
2022-06-02 10:02:02 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (8.21%) - LeetHub

--- 1328-break-a-palindrome/1328-break-a-palindrome.cpp
+++ 1328-break-a-palindrome/1328-break-a-palindrome.cpp
@@ -6,7 +6,6 @@ public:
         for(i = 0; i<palindrome.size()/2; ++i)
             if(palindrome[i]>'a')
             {palindrome[i] = 'a'; return palindrome;}
-        // if(palindrome.size()%2==1 && i==)
         palindrome[palindrome.size()-1] += 1;
         return palindrome;
     }

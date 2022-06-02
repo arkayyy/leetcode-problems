03001e8b886487ff88320f48c0b104260740282f
Rajarshi Karan <72148786+arkayyy@users.noreply.github.com>
2022-06-02 10:23:04 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (49.26%) - LeetHub

--- 1328-break-a-palindrome/1328-break-a-palindrome.cpp
+++ 1328-break-a-palindrome/1328-break-a-palindrome.cpp
@@ -1,12 +1,16 @@
 class Solution {
 public:
+    //INTUITION: To make the lexicographically smallest non-palindrome, we need to find the first character greater than 'a' from the left, and convert it to 'a'.
+    //So the palindrome is also broken and at the same time, the lexicographically smallest string is made, since the dependency/significance of the character on lexicographic order pf the string decreases from left to right
     string breakPalindrome(string palindrome) {
-        if(palindrome.size()==1) return "";
+    if(palindrome.size()==1) return "";
        int i;
         for(i = 0; i<palindrome.size()/2; ++i)
             if(palindrome[i]>'a')
             {palindrome[i] = 'a'; return palindrome;}
-        palindrome[palindrome.size()-1] += 1;
+        palindrome[palindrome.size()-1] += 1; //this is for the edge case when there are all 'a' in the string...
+        //...AND also for all 'a' except the middle element in case of odd length string. In that case we'll need to increase one character. so we do that at the rear character so that the lexicographic order is least increased.
+        
         return palindrome;
     }
 };

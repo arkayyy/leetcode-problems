8c30773f2b1121db822f2660649f14585a9a4ac2
Rajarshi Karan <72148786+arkayyy@users.noreply.github.com>
2022-06-03 10:06:25 +0530

    Time: 488 ms (72.90%), Space: 148 MB (48.25%) - LeetHub

--- 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.cpp
+++ 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.cpp
@@ -2,19 +2,22 @@ class NumMatrix {
 public:
     int n,m;
     vector<vector<int>> prefix;
+    //int prefix;
     NumMatrix(vector<vector<int>>& matrix) {
         n = matrix.size(), m = matrix[0].size();
-        prefix = vector<vector<int>>(n,vector<int>(m,0));
-        for(int i = 0; i<n;++i)
-            for(int j = 0; j<m; ++j)
-                prefix[i][j] = j==0?matrix[i][j]:(prefix[i][j-1]+matrix[i][j]);
+        prefix = vector<vector<int>>(n+1,vector<int>(m+1,0));
+        for(int i = 1; i<=n;++i)
+            for(int j = 1; j<=m; ++j)
+                prefix[i][j] = prefix[i][j-1]+prefix[i-1][j]-prefix[i-1][j-1] + matrix[i-1][j-1];
     }
     
     int sumRegion(int row1, int col1, int row2, int col2) {
-        int sum = 0;
-        for(int i = row1; i<=row2; ++i)
-            sum += prefix[i][col2] - (col1>=1?prefix[i][col1-1]:0);
-        return sum;
+        // int sum = 0;
+        // for(int i = row1; i<=row2; ++i)
+        //     sum += prefix[i][col2] - (col1>=1?prefix[i][col1-1]:0);
+        // return sum;
+        
+        return prefix[row2+1][col2+1] - prefix[row2+1][col1] - prefix[row1][col2+1] + prefix[row1][col1];
     }
 };
 

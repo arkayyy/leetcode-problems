d623f8671b48eadb674dc45808c994d00f670f2c
Rajarshi Karan <72148786+arkayyy@users.noreply.github.com>
2022-06-06 22:31:12 +0530

    Time: 9 ms (5.86%), Space: 7 MB (56.06%) - LeetHub

--- 232-implement-queue-using-stacks/232-implement-queue-using-stacks.cpp
+++ 232-implement-queue-using-stacks/232-implement-queue-using-stacks.cpp
@@ -1,14 +1,18 @@
 class MyQueue {
+    //INTUITION: We use stack 1 for pushing elements only. Now as we know in stack, the order is going to get reversed as we pop out the elements
+    //So to fix that we use another stack to cut out this reversing effect.
+    //in stack 2, the elements will be stored in such a manner that the element popped should be as it would have been popped by a queue(in FIFO).
+    //Because the reversing effect is cut out using stack 2.
 public:
-    stack<int> st1; //used mainly for push 
-    stack<int> st2; //used mainly for peeking or popping
+    stack<int> st1; //used for push 
+    stack<int> st2; //used for peeking or popping
     
     MyQueue() {
         
     }
     
     void push(int x) {
-        st1.push(x);
+        st1.push(x); 
     }
     
     int pop() {
@@ -18,7 +22,7 @@ public:
         return popped;
     }
     
-    int peek() //shifts all elements from st1 to st2
+    int peek() //shifts all elements from st1 to st2, if st2 is empty
     {
         if(st2.empty())
             while(!st1.empty())

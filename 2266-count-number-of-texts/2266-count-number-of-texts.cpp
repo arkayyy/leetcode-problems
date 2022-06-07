f808c398cc6cd847858fa0bdbded7745bc9ec87c
Rajarshi Karan <72148786+arkayyy@users.noreply.github.com>
2022-06-07 12:39:14 +0530

    Time: 85 ms (43.27%), Space: 15 MB (78.90%) - LeetHub

---
+++ 2266-count-number-of-texts/2266-count-number-of-texts.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int MOD = 1e9 + 7;
+    int util(int i, string s, int n, vector<int>&dp)
+    {
+        if(i==n) return 1;
+        if(dp[i]!=-1) return dp[i]%MOD;
+        int cnt = 0;
+        if(i+3<n && (s[i]=='7'||s[i]=='9') && s[i]==s[i+1]&&s[i+1]==s[i+2] && s[i+2]==s[i+3]) cnt=(cnt%MOD+util(i+4,s,n,dp)%MOD)%MOD;
+        if(i+2<n && s[i]==s[i+1] && s[i+1]==s[i+2]) cnt=(cnt%MOD+ util(i+3,s,n,dp)%MOD)%MOD;
+        if(i+1<n && s[i]==s[i+1]) cnt= (cnt%MOD + util(i+2,s,n,dp)%MOD)%MOD;
+        cnt= (cnt%MOD + util(i+1,s,n,dp)%MOD)%MOD;
+        
+        return dp[i]=cnt%MOD;
+    }
+    int countTexts(string s) {
+        //=================APPROACH 1: Memoization O(N) Time & O(N)+Recursion Aux Space (Gives memory limit exceeded error)====================================//
+        //vector<int> dp(s.size(),-1);
+        //return util(0,s,s.size(),dp)%MOD;
+        
+        
+        //==============APPROACH 2: Tabulation DP O(N) Time O(N) Space ==================================//
+        vector<int> dp(s.size()+1,0);
+        dp[0] = 1;
+        for(int i = 1; i<=s.size(); ++i)
+        {
+            dp[i] =( dp[i]+dp[i-1])%MOD;
+            if(i-2>=0 && s[i-1]==s[i-2]) {
+                dp[i] = (dp[i]+dp[i-2])%MOD;
+                if(i-3>=0 && s[i-1]==s[i-3])
+                {
+                    dp[i] = (dp[i] + dp[i-3])%MOD;
+                    if(i-4>=0 && (s[i-1]=='7'||s[i-1]=='9') && s[i-1]==s[i-4])
+                        dp[i] = (dp[i]+dp[i-4])%MOD;
+                }
+            }
+        }
+        return dp.back();
+    }
+};

b0f310d26f59097f34d28b49cbbd30047e534a42
Rajarshi Karan <72148786+arkayyy@users.noreply.github.com>
2022-06-08 17:06:15 +0530

    Time: 89 ms (20.11%), Space: 7.6 MB (89.81%) - LeetHub

--- 812-largest-triangle-area/812-largest-triangle-area.cpp
+++ 812-largest-triangle-area/812-largest-triangle-area.cpp
@@ -8,6 +8,7 @@ public:
                     for(int k = 0; k<points.size(); ++k)
                         if(k!=i and k!=j)
                         {
+                            //For triangle formed by 3 points a,b and c: the area will be = 1/2 * [(xa*yb + xb*yc + xc*ya) - (ya*xb + yb*xc + yc*xa)]
                             ans = max(ans, 0.50000 * (points[i][0]*points[j][1] + points[j][0]*points[k][1] + points[k][0]*points[i][1] - points[i][1]*points[j][0] - points[j][1]*points[k][0] - points[k][1]*points[i][0]));
                         }
         return ans;

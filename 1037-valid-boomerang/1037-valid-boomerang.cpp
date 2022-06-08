b60b2481d00a697ffbd532bf0a8490882fb6a615
Rajarshi Karan <72148786+arkayyy@users.noreply.github.com>
2022-06-08 17:44:34 +0530

    Time: 4 ms (52.99%), Space: 10.4 MB (18.41%) - LeetHub

--- 1037-valid-boomerang/1037-valid-boomerang.cpp
+++ 1037-valid-boomerang/1037-valid-boomerang.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    //slope12 != slope23
+    //slope b/w 1st and 2nd pt != slope b/w 2nd and 3rd pt
     
     bool isBoomerang(vector<vector<int>>& points) {
         return (points[1][1]-points[0][1])*(points[2][0]-points[1][0]) != (points[2][1]-points[1][1])*(points[1][0]-points[0][0]);

d1650ebf23f5c14c5e3993f198aab1c2161479e0
Rajarshi Karan <72148786+arkayyy@users.noreply.github.com>
2022-06-09 01:16:46 +0530

    Time: 373 ms (77.09%), Space: 116.9 MB (98.53%) - LeetHub

--- 295-find-median-from-data-stream/295-find-median-from-data-stream.cpp
+++ 295-find-median-from-data-stream/295-find-median-from-data-stream.cpp
@@ -1,5 +1,17 @@
 class MedianFinder {
 public:
+    //INTUITION: We take two opposite fashion priority queues, one in ascending and one in descending. And we will push elements in queue based on their range of elements inside. 
+    //Our aim is to divide all the elements into two adjacent ranges.
+    //So this way at last, the top of the priority queues would always contain the central element(s).
+    
+    
+    //ALGO:
+    //So the whole sequence of number given is divided into two separate ranges, one stored in max heap and the other stored in min heap.
+    // e.g.[1,2,3,4,5,6] is divided into:
+    //top->[3,2,1] for pq1 (maxheap)     AND        top->[4,5,6] for pq2(minheap). So the top of both pq's give us the central elements.
+
+    //e.g. [1,2,3,4,5] is divided into:
+    //top->[3,2,1] for pq1 (maxheap)     AND     top->[4,5] for pq2(minheap)
     priority_queue<int, vector<int>, greater<int>> pq2;
     priority_queue<int> pq1;
     MedianFinder() {
@@ -7,10 +19,14 @@ public:
     }
     
     void addNum(int num) {
-        if(pq1.empty() || pq1.top()>num) pq1.push(num);
-        else pq2.push(num);
+        if(pq1.empty() || pq1.top()>num) pq1.push(num); //initially inserting in pq1, or if the element falls in range of the elements already present in pq1
+        else pq2.push(num); //otherwise inserting in pq2
         
-        if(pq1.size()> pq2.size()+1)
+        //the rule is that any priority queue having more than 1 elements more than the other priority queue,
+        //we will transfer the extra element to the other priority queue 
+        //as we will only transfer the topmost element of each pq, so for max heap it'll be the max element shifted, and for min head min. element shifted
+
+        if(pq1.size()> pq2.size()+1) 
         {pq2.push(pq1.top()); pq1.pop();}
         else if(pq2.size()>pq1.size()+1)
         {pq1.push(pq2.top()); pq2.pop();}

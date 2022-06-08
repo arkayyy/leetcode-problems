b328a54a5630a5c75798e56ccb067d1643f1f804
Rajarshi Karan <72148786+arkayyy@users.noreply.github.com>
2022-06-08 14:38:19 +0530

    Time: 11 ms (70.26%), Space: 9.5 MB (15.61%) - LeetHub

--- 892-surface-area-of-3d-shapes/892-surface-area-of-3d-shapes.cpp
+++ 892-surface-area-of-3d-shapes/892-surface-area-of-3d-shapes.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    //INTUITION: Let us consider that every height contributes to the surface area according to its difference with the smaller heights adjacent to it.
     int surfaceArea(vector<vector<int>>& grid) {
         int sarea = 0;
         int n = grid.size(), m = grid[0].size();
@@ -7,15 +8,15 @@ public:
         for(int i = 0; i<n;++i)
             for(int j = 0; j<m; ++j)
             {
-                if((i==0 && j==0) || (i==n-1 && j==m-1) || (i==0&&j==m-1) || (i==n-1 && j==0)) sarea+=2*grid[i][j];
-                else if(i==0 || j==0 || i==n-1 || j==m-1) sarea+=grid[i][j];
+                if((i==0 && j==0) || (i==n-1 && j==m-1) || (i==0&&j==m-1) || (i==n-1 && j==0)) sarea+=2*grid[i][j]; //for corner heights 2 extra sides
+                else if(i==0 || j==0 || i==n-1 || j==m-1) sarea+=grid[i][j]; //for other cells at boundaries 1 extra side
                 
                 sarea += max(0, i+1<n?grid[i][j]-grid[i+1][j]:0);
                 sarea += max(0, i-1>=0?grid[i][j]-grid[i-1][j]:0);
                 sarea += max(0, j+1<m?grid[i][j]-grid[i][j+1]:0);
                 sarea += max(0, j-1>=0?grid[i][j]-grid[i][j-1]:0);
                 
-                sarea+=grid[i][j]?2:0;
+                sarea+=grid[i][j]?2:0; //for top and bottom
             }
         return sarea;
     }

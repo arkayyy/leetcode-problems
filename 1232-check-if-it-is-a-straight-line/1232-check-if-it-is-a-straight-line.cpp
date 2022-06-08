9deac129ec8c5dc6c6dbb48101ab79f133f2149b
Rajarshi Karan <72148786+arkayyy@users.noreply.github.com>
2022-06-08 17:38:00 +0530

    Time: 26 ms (5.28%), Space: 10.1 MB (44.59%) - LeetHub

--- 1232-check-if-it-is-a-straight-line/1232-check-if-it-is-a-straight-line.cpp
+++ 1232-check-if-it-is-a-straight-line/1232-check-if-it-is-a-straight-line.cpp
@@ -1,5 +1,11 @@
 class Solution {
 public:
+    //INTUITION: The idea is to first calculate the slope b/w first 2 points. 
+    //In order for all pts to be on a straight line, all lines should also have this exact same slope with the point at 0th index.
+    
+    //let slope b/w first 2 points = dy/dx
+    //and slope b/w any  other pt and 0th index point = dy1/dx1
+    //for them to lie on straight line, dy1*dx1 = dy/dx ...cross multiplying we get the condition as: dy1*dx = dx1*dy
     bool checkStraightLine(vector<vector<int>>& coords) {
         int dy = coords[1][1]-coords[0][1];
         int dx = coords[1][0]-coords[0][0];

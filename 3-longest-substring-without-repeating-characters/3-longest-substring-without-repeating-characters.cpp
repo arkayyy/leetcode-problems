b68ca518685788dea08fcf5e116d75adf29c31f8
Rajarshi Karan <72148786+arkayyy@users.noreply.github.com>
2022-06-10 19:42:09 +0530

    Time: 9 ms (85.33%), Space: 8.4 MB (59.36%) - LeetHub

---
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    //INTUITION & ALGO: Sliding window + Frequency map for substrings
+    int lengthOfLongestSubstring(string s) {
+        
+        if(s.length()==0) return 0;
+        
+        vector<int> freq(256,0);
+        
+        int l = 0, r = 0;
+        int maxLen = INT_MIN;
+        
+        while(r<s.length())
+        {
+            //increasing frequency of right pointer char
+            freq[s[r]]++;
+            
+             
+            while(freq[s[r]]>1) //only when right pointer char is rep
+            {
+                freq[s[l]]--; //left pointer sliding one by one and simultaneously decreasing freq
+                l++;
+            }
+            
+            maxLen = max(maxLen, r-l+1);
+            r++;
+        }
+        
+        return maxLen;
+    }
+};

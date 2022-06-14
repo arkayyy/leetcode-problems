b2e5a6fdadc1398631590f00d3e143ea4264e998
Rajarshi Karan <72148786+arkayyy@users.noreply.github.com>
2022-06-14 07:35:06 +0530

    Time: 9 ms (24.29%), Space: 8.8 MB (50.23%) - LeetHub

--- 162-find-peak-element/162-find-peak-element.cpp
+++ 162-find-peak-element/162-find-peak-element.cpp
@@ -5,7 +5,13 @@ public:
         while(lo<=hi)
         {
             int mid = lo+((hi-lo)>>1);
-            if((mid-1<0 || nums[mid]>nums[mid-1]) && (mid+1>nums.size()-1 || nums[mid]>nums[mid+1])) return mid;
+        
+            if((mid-1<0 || nums[mid]>nums[mid-1]) && (mid+1>nums.size()-1 || nums[mid]>nums[mid+1])) return mid;//peak element found
+            
+            //otherwise going in the direction of the rising slope(by choosing the element which is greater than current)
+            //there are 2 possibilities:-
+            //a) rising slope has to keep rising till end of the array 
+            //b) rising slope will encounter a lesser element and go down 
             if((mid-1<0 || nums[mid]>nums[mid-1]))
                 lo = mid+1;
             else

df3a7b6948299742677fd3d292ccac50acb8ea1c
Rajarshi Karan <72148786+arkayyy@users.noreply.github.com>
2022-06-14 07:34:30 +0530

    Time: 238 ms (41.76%), Space: 46.4 MB (13.33%) - LeetHub

--- 1901-find-a-peak-element-ii/1901-find-a-peak-element-ii.cpp
+++ 1901-find-a-peak-element-ii/1901-find-a-peak-element-ii.cpp
@@ -7,7 +7,6 @@ public:
         //Now we check if this max element also is bigger than its left(previous column same row) and right(next column same row) element.
         //If yes, then peak element is FOUND!
         
-        
         int lo = 0, hi = mat[0].size()-1;
         while(lo<=hi)
         {
@@ -17,7 +16,12 @@ public:
 
             if((mid-1<0 || mat[maxRowIdx][mid]>=mat[maxRowIdx][mid-1]) && (mid+1>mat[0].size()-1 || mat[maxRowIdx][mid]>mat[maxRowIdx][mid+1])) return {maxRowIdx, mid};
             
-            if((mid-1<0 || mat[maxRowIdx][mid]>=mat[maxRowIdx][mid-1])) //only greater than left, not right
+            //going in the direction of the rising slope(by choosing the element which is greater than current)
+            //there are 2 possibilities:-
+            //a) rising slope has to keep rising till end of the array 
+            //b) rising slope will encounter a lesser element and go down 
+            
+            if((mid-1<0 || mat[maxRowIdx][mid]>=mat[maxRowIdx][mid-1])) //only greater than left, not right. so rising slope towards right
                 lo=mid+1;
             else //only greater than right,not left
                 hi=mid-1;

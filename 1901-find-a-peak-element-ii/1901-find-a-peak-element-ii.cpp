780137f6a27a7a4048b23480fa1b3104d0b537a5
Rajarshi Karan <72148786+arkayyy@users.noreply.github.com>
2022-06-14 07:24:34 +0530

    Time: 375 ms (5.10%), Space: 46.2 MB (76.08%) - LeetHub

--- 1901-find-a-peak-element-ii/1901-find-a-peak-element-ii.cpp
+++ 1901-find-a-peak-element-ii/1901-find-a-peak-element-ii.cpp
@@ -14,10 +14,10 @@ public:
             int mid = lo+((hi-lo)>>1);
             int maxRowIdx = 0;
             for(int i = 0; i<mat.size(); ++i) if(mat[i][mid]>mat[maxRowIdx][mid]) maxRowIdx = i; //finding max element row in the whole column
-            cout<<maxRowIdx<<endl;
-            if((mid-1<lo || mat[maxRowIdx][mid]>=mat[maxRowIdx][mid-1]) && (mid+1>hi || mat[maxRowIdx][mid]>mat[maxRowIdx][mid+1])) return {maxRowIdx, mid};
+
+            if((mid-1<0 || mat[maxRowIdx][mid]>=mat[maxRowIdx][mid-1]) && (mid+1>mat[0].size()-1 || mat[maxRowIdx][mid]>mat[maxRowIdx][mid+1])) return {maxRowIdx, mid};
             
-            if((mid-1<lo || mat[maxRowIdx][mid]>=mat[maxRowIdx][mid-1])) //only greater than left, not right
+            if((mid-1<0 || mat[maxRowIdx][mid]>=mat[maxRowIdx][mid-1])) //only greater than left, not right
                 lo=mid+1;
             else //only greater than right,not left
                 hi=mid-1;

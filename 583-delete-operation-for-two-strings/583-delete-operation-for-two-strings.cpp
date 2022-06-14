be229de2cf698c0b46eee1d3fb4613762bf78fe0
Rajarshi Karan <72148786+arkayyy@users.noreply.github.com>
2022-06-14 14:38:06 +0530

    Time: 14 ms (84.46%), Space: 7.5 MB (71.41%) - LeetHub

--- 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.cpp
+++ 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.cpp
@@ -4,14 +4,8 @@ public:
         int m=text1.length(); int n = text2.length();
         
         int dp[m+1][n+1];
-        for(int i=0;i<=m;i++)
-        {
-          for(int j=0;j<=n;j++)
-              dp[i][j]=0;
-            
-        }
-        
-        
+        memset(dp,0,sizeof(dp));
+         
         for(int i = 1;i<=m;i++)
         {
             for(int j=1;j<=n;j++)
@@ -21,8 +15,6 @@ public:
             else
                 dp[i][j]= max(dp[i][j],max(dp[i-1][j],dp[i][j-1]));
             }
-            
-            
         }
         return dp[m][n];
     }

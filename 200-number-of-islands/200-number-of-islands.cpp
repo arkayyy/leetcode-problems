88f444915f86acd6ff9583f6bcfc03c1ce239498
Rajarshi Karan <72148786+arkayyy@users.noreply.github.com>
2022-06-16 15:02:01 +0530

    Time: 57 ms (36.85%), Space: 12.4 MB (58.82%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -1,21 +1,89 @@
 class Solution {
 public:
-   
-    void dfs(int i,  int j, vector<vector<char>>&grid, vector<vector<int>>& visited)
+    
+    void dfs(int r, int c, vector<vector<char>>& grid)
     {
-        visited[i][j] = 1;
-        if(i+1<grid.size() && grid[i+1][j]=='1' && !visited[(i+1)][j]) dfs(i+1,j,grid,visited);
-        if(i-1>=0 && grid[i-1][j]=='1' && !visited[(i-1)][j]) dfs(i-1,j,grid,visited);
-        if(j+1<grid[0].size() && grid[i][j+1]=='1' && !visited[(i)][j+1]) dfs(i,j+1,grid,visited);
-        if(j-1>=0 && grid[i][j-1]=='1' && !visited[(i)][j-1]) dfs(i,j-1,grid,visited);
+        if(r>=grid.size() || c>=grid[0].size() || r<0 || c<0 || grid[r][c]=='0')
+            return;
+        
+        grid[r][c] = '0';
+        
+        dfs(r+1,c,grid);
+        dfs(r-1,c,grid);
+        dfs(r,c+1,grid);
+        dfs(r,c-1,grid);
+       
     }
     int numIslands(vector<vector<char>>& grid) {
-        vector<vector<int>> visited(grid.size(),vector<int>(grid[0].size(),0));
+        
+        //USING DFS:-
         int cnt = 0;
-        for(int i = 0; i<grid.size();++i)
-            for(int j=0;j<grid[0].size();++j)
-                if(grid[i][j]=='1' && !visited[i][j])
-                    ++cnt,dfs(i,j,grid,visited);
+        
+        for(int i=0;i<grid.size();i++)
+        {
+            for(int j=0;j<grid[0].size();j++)
+            {
+                if(grid[i][j]=='1')
+                {
+                    cnt++;
+                    dfs(i,j,grid);
+                }
+            }
+        }
         return cnt;
+        
+        
+        
+        
+        
+        //USING BFS:-
+        
+//         queue<pair<int,int>> q;
+        
+        
+//         int m = grid.size(), n=grid[0].size();
+        
+//         int cnt = 0;
+        
+//         for(int i=0;i<m; i++)
+//         {
+//             for(int j=0;j<n;j++)
+//             {
+//                 if(grid[i][j]=='1')
+//                 {
+//                     q.push({i,j});
+//                     cnt++;
+                    
+                    
+//                     while(!q.empty())
+//                     {
+//                         int len = q.size();
+//                         for(int k=0; k<len;k++)
+//                         {
+                            
+//                             pair<int,int> coords = q.front(); q.pop();
+                            
+//                             int p = coords.first, r = coords.second;
+                           
+                            
+//                             if(p+1<m && grid[p+1][r]=='1')
+//                                 { grid[p+1][r]=0; q.push({p+1,r});}
+//                             if(p-1>=0 && grid[p-1][r]=='1')
+//                                {grid[p-1][r]=0; q.push({p-1,r});}
+//                             if(r+1<n && grid[p][r+1]=='1')
+//                                {grid[p][r+1]=0; q.push({p,r+1});}
+//                             if(r-1>=0 && grid[p][r-1]=='1')
+//                                 {grid[p][r-1] = 0; q.push({p,r-1});}
+                            
+                            
+                            
+//                         }
+                            
+//                     }
+//                 }
+//             }
+//         }
+        
+//         return cnt;
     }
 };

6a699cbfb36f94837626c88c3aa11e7c1a538429
Rajarshi Karan <72148786+arkayyy@users.noreply.github.com>
2022-06-16 15:47:28 +0530

    Time: 37 ms (38.69%), Space: 13.6 MB (38.06%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.cpp
+++ 785-is-graph-bipartite/785-is-graph-bipartite.cpp
@@ -1,44 +1,11 @@
 class Solution {
 public:
     bool isBipartite(vector<vector<int>>& graph) {
-//         queue<int> bfs;
-//         vector<int> coloring(graph.size(),-1);
-        
-       
-//         int currcolor;
-//         for(int k=0;k<graph.size();k++)
-//         {  if(coloring[k]==-1) 
-//             {
-//                 bfs.push(k);
-//                 coloring[k]=currcolor+2;
-//                 while(!bfs.empty())
-//                 {
-//                     int t = bfs.front();
-//                     bfs.pop();
-//                     currcolor = coloring[t];
-//                     for(auto i:graph[t])
-//                     {
-//                         if(coloring[i]!=-1 && coloring[i]==coloring[t])
-//                             return false;
 
-//                         if(coloring[i]==-1)
-//                         {
-//                             coloring[i]=coloring[t]+2;
-
-//                             bfs.push(i);
-//                         }
-
-//                     }
-//                 }
-//             }
-                
-        
-//         }
-//         return true;
         
         vector<int> color(graph.size(),0);
         queue<int> q;
-        for(int i = 0; i<graph.size(); ++i) 
+        for(int i = 0; i<graph.size(); ++i) //running loop to ensure all disconnected components are also checked
         {   if(color[i]!=0)continue;
             q.push(i);
             color[0] = 1;

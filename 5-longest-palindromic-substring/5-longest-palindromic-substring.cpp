67b3324ce22258111495a94041d1adfa3937b7cd
Rajarshi Karan <72148786+arkayyy@users.noreply.github.com>
2022-06-16 14:50:35 +0530

    Time: 677 ms (17.92%), Space: 385.5 MB (10.29%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -1,6 +1,23 @@
 class Solution {
 public:
+      //also try by expanding from middle method
+    int expandFromMiddle(int l,int r, string s)
+    {
+        
+        if(l>r || s=="")
+            return 0;
+        
+        while(l>=0 && r<s.length() && s[l]==s[r])
+        {
+            l--;
+            r++;
+        }
+        
+        return r-l-1;
+    }
+    
     string longestPalindrome(string s) {
+        //DP Approach O(N^2) Time O(N^2) Space
         vector<vector<int>> dp(s.size(),vector<int>(s.size(),0));
         for(int i = 0; i<s.size(); ++i) dp[i][i] = 1;
         int maxLen = 1; string ans = s.substr(0,1);
@@ -15,5 +32,27 @@ public:
                     }
                 }
         return ans;
+        
+         //Expand from middle approach O(N^2) Time O(1) Space:-
+        
+//         if(s=="" || s.length()<1)
+//             return "";
+        
+        
+//         int end = 0, start = 0;
+        
+//         for(int i=0;i<s.length();i++)
+//         {
+//             int len1 = expandFromMiddle(i,i,s);
+//             int len2 = expandFromMiddle(i,i+1,s);
+//             int len = max(len1,len2);
+//             if(len>end-start)
+//             {
+//                 start = i-((len-1)/2);
+//                 end = i+(len/2);
+//             }
+//         }
+        
+//         return s.substr(start, end-start+1);
     }
 };

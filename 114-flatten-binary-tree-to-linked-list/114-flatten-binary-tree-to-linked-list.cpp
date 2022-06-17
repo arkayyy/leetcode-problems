3c760dd609fe5dc80712af552f9fbfacb467854e
Rajarshi Karan <72148786+arkayyy@users.noreply.github.com>
2022-06-17 17:34:25 +0530

    Time: 14 ms (24.65%), Space: 12.6 MB (79.21%) - LeetHub

--- 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.cpp
+++ 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.cpp
@@ -11,21 +11,38 @@
  */
 class Solution {
 public:
+    //Recursive approach (same logic as iterative):-
+    TreeNode* prev = NULL;
+    void util(TreeNode* root)
+    {
+        if(!root) return;
+        util(root->right);
+        util(root->left);
+        root->right = prev;
+        root->left = NULL;
+        prev = root;
+    }
+    
     void flatten(TreeNode* root) {
-        while(root)
-        {
-            if(root->left && root->right)
-            {
-                //flatenning the left subtree
-                TreeNode* t = root->left;
-                while(t->right)
-                    t = t->right;
-                t->right = root->right; //connecting the flatenned left subtree to the right subtree's first node
-            }
-            if(root->left) //if left subtree exists and has been flattened
-                root->right = root->left; //making it the right subtree
-            root->left = NULL;//and eliminating the left subtree
-            root = root->right;
-        }
+        
+        util(root);
+        
+        //Iterative approach O(N) Time O(1) Space:-
+        
+        // while(root)
+        // {
+        //     if(root->left && root->right)
+        //     {
+        //         //flatenning the left subtree
+        //         TreeNode* t = root->left;
+        //         while(t->right)
+        //             t = t->right;
+        //         t->right = root->right; //connecting the flatenned left subtree to the right subtree's first node
+        //     }
+        //     if(root->left) //if left subtree exists and has been flattened
+        //         root->right = root->left; //making it the right subtree
+        //     root->left = NULL;//and eliminating the left subtree
+        //     root = root->right;
+        // }
     }
 };

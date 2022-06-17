7c69015a0a408d2b696b3f34039734431a4e65f7
Rajarshi Karan <72148786+arkayyy@users.noreply.github.com>
2022-06-18 00:30:55 +0530

    Time: 0 ms (100.00%), Space: 8.1 MB (97.85%) - LeetHub

--- 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cpp
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cpp
@@ -9,24 +9,48 @@ public:
     }
     vector<int> inorderTraversal(TreeNode* root) {
         vector<int> ans;
-        //recursive call:-
-        // inorderUtil(root, ans);
+//         //recursive call:-
+//         // inorderUtil(root, ans);
         
-        stack<TreeNode*> s;
+//         stack<TreeNode*> s;
+//         TreeNode* curr = root;
+//         //iterative approach:-
+//         while(!s.empty() || curr)
+//         {
+//             //going tp the left most node and storing all nodes on the way
+//             while(curr)
+//             {
+//                 s.push(curr);
+//                 curr = curr->left;
+//             }
+//             curr = s.top(); //one by one popping the nodes that have come on the way on the left traversal
+//             s.pop();
+//             ans.emplace_back(curr->val); 
+//             curr = curr->right; //if any node has a right subtree, also shifting curr there in order to carry out inorder traversal
+//         }
+//         return ans;
+        
+        
+        //MORRIS Traversal O(N) Time O(1) Space:-
         TreeNode* curr = root;
-        //iterative approach:-
-        while(!s.empty() || curr)
+        while(curr)
         {
-            //going tp the left most node and storing all nodes on the way
-            while(curr)
+            if(!curr->left)//if no left subtree exists, we have to push the root, and move to right node
             {
-                s.push(curr);
-                curr = curr->left;
+                ans.emplace_back(curr->val);
+                curr = curr->right;
             }
-            curr = s.top(); //one by one popping the nodes that have come on the way on the left traversal
-            s.pop();
-            ans.emplace_back(curr->val); 
-            curr = curr->right; //if any node has a right subtree, also shifting curr there in order to carry out inorder traversal
+            else
+            {
+                //if left subtree exists, our aim will be to create a thread between the last rightmost node of this left subtree with the root, so that we can come back to the root later on
+                TreeNode* temp = curr->left;
+                while(temp->right && temp->right!=curr) //will stop when there's no right node anymore, or when a thread is found
+                    temp = temp->right;
+                
+                if(!temp->right) {temp->right = curr; curr = curr->left;} //if no thread found we build the thread
+                else {temp->right = NULL; ans.emplace_back(curr->val); curr = curr->right; } //if already thread found to root node(i.e.curr), that means we are visiting this root node 2nd time, after handling its left subtree, 
+                //so now will add this root node to inorder sequence, and proceed to the right subtree of the root node
+            }
         }
         return ans;
     }

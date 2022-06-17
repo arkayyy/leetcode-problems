3c99f98c091acd6d2b63879f17746d384f3b2f39
Rajarshi Karan <72148786+arkayyy@users.noreply.github.com>
2022-06-17 23:32:17 +0530

    Time: 6 ms (18.96%), Space: 8.4 MB (41.15%) - LeetHub

--- 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cpp
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cpp
@@ -1,37 +1,32 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    void inorder(vector<int>& ans, TreeNode* root)
+    void inorderUtil(TreeNode* root, vector<int> & ans)
     {
         if(!root) return;
-        inorder(ans,root->left);
-        ans.push_back(root->val);
-        inorder(ans,root->right);
+        inorderUtil(root->left,ans);
+        ans.emplace_back(root->val);
+        inorderUtil(root->right,ans);
     }
     vector<int> inorderTraversal(TreeNode* root) {
         vector<int> ans;
-        // inorder(ans,root);
-        // return ans;
+        //recursive call:-
+        // inorderUtil(root, ans);
         
-        stack<TreeNode*> st;
+        stack<TreeNode*> s;
         TreeNode* curr = root;
-        while(!st.empty() || curr)
+        //iterative approach:-
+        while(!s.empty() || curr)
         {
+            //going tp the left most node and storing all nodes on the way
             while(curr)
-                {st.push(curr); curr = curr->left;}
-            curr = st.top(); st.pop();
-            ans.emplace_back(curr->val);
-            curr = curr->right;
+            {
+                s.push(curr);
+                curr = curr->left;
+            }
+            curr = s.top(); //one by one popping the nodes that have come on the way on the left traversal
+            s.pop();
+            ans.emplace_back(curr->val); 
+            curr = curr->right; //if any node has a right subtree, also shifting curr there in order to carry out inorder traversal
         }
         return ans;
     }

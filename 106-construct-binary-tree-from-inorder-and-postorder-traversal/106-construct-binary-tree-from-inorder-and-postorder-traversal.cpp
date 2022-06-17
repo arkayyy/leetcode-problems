837a7f72c898978ebf8a8b23350ed8e7e700f39c
Rajarshi Karan <72148786+arkayyy@users.noreply.github.com>
2022-06-17 15:26:52 +0530

    Time: 66 ms (15.86%), Space: 26 MB (74.21%) - LeetHub

--- 106-construct-binary-tree-from-inorder-and-postorder-traversal/106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
+++ 106-construct-binary-tree-from-inorder-and-postorder-traversal/106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
@@ -16,12 +16,16 @@ public:
         if( lo>hi) return NULL;
         
         int j = 0;
-        while(inorder[j]!=postorder[idx]) ++j;
+        while(inorder[j]!=postorder[idx]) ++j;//finding the current root node position in inorder array
+        //left part of the root node in inorder array will be the elements of the left subtree, and on the right will be the elements of the right subtree
         
-        --idx;
+        --idx; //for moving backwards in the postorder array
+        
+         //since in postorder, left..right..root sequence is followed, so in backwards, root..right..left sequence must be followed in calling functions
         TreeNode* node = new TreeNode(inorder[j]);
-        node->right=util(j+1,hi,idx,postorder,inorder);
-        node->left = util(lo,j-1,idx,postorder,inorder);
+       
+        node->right=util(j+1,hi,idx,postorder,inorder);//lo and hi range are passed for right subtree elements in inorder array
+        node->left = util(lo,j-1,idx,postorder,inorder);//the lo and hi range are passed for left subtree elements as in inorder array
         return node;
     }
     TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {

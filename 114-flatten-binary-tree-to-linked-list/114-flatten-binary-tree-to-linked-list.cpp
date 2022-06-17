7059d678e62a8b0d50b9016001bab8ed116d8e1c
Rajarshi Karan <72148786+arkayyy@users.noreply.github.com>
2022-06-17 16:43:09 +0530

    Time: 10 ms (50.83%), Space: 12.7 MB (79.21%) - LeetHub

--- 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.cpp
+++ 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.cpp
@@ -16,14 +16,15 @@ public:
         {
             if(root->left && root->right)
             {
+                //flatenning the left subtree
                 TreeNode* t = root->left;
                 while(t->right)
                     t = t->right;
-                t->right = root->right;
+                t->right = root->right; //connecting the flatenned left subtree to the right subtree's first node
             }
-            if(root->left)
-                root->right = root->left;
-            root->left = NULL;
+            if(root->left) //if left subtree exists and has been flattened
+                root->right = root->left; //making it the right subtree
+            root->left = NULL;//and eliminating the left subtree
             root = root->right;
         }
     }

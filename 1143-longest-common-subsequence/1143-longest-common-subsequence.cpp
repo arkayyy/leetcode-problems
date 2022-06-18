e685e0dfda45c551049ed2004a2c0fb25617e18f
Rajarshi Karan <72148786+arkayyy@users.noreply.github.com>
2022-06-18 14:46:30 +0530

    Time: 30 ms (58.42%), Space: 10.6 MB (87.76%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -6,8 +6,8 @@ public:
         memset(dp,0,sizeof(dp));
         for(int i = 1; i<=n; ++i)
             for(int j = 1; j<=m; ++j)
-               if(text1[i-1]==text2[j-1]) dp[i][j] = dp[i-1][j-1]+1;
-                else dp[i][j] = max({dp[i-1][j-1], dp[i-1][j], dp[i][j-1]});
+               if(text1[i-1]==text2[j-1]) dp[i][j] = dp[i-1][j-1]+1; //current character included in LCS
+                else dp[i][j] = max({dp[i-1][j-1], dp[i-1][j], dp[i][j-1]}); //current char not included
         return dp[n][m];
     }
 };

0bfb9d8519493b4a2c97a8bfac45d2a2906767ad
Rajarshi Karan <72148786+arkayyy@users.noreply.github.com>
2022-06-18 14:03:02 +0530

    Time: 1628 ms (5.00%), Space: 292.2 MB (5.16%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -12,7 +12,21 @@ public:
         return dp[i][prevIdx+1]=res;
     }
     int lengthOfLIS(vector<int>& nums) {
-        vector<vector<int>> dp(nums.size(), vector<int>(nums.size()+1,-1));
-        return util(0,-1,nums,dp);
+        //vector<vector<int>> dp(nums.size(), vector<int>(nums.size()+1,-1));
+        // return util(0,-1,nums,dp);
+        
+        vector<vector<int>> dp(nums.size()+1, vector<int>(nums.size()+1));
+        for(int prevIdx = 0; prevIdx<=nums.size();++prevIdx) dp[nums.size()][prevIdx] = 0;  //equivalent to base case of recursion
+       
+        for(int i = nums.size()-1; i>=0; --i)
+            for(int prevIdx = nums.size()-1; prevIdx>=-1; --prevIdx)
+            {
+                int res = dp[i+1][prevIdx+1]; //not take
+                if(prevIdx==-1 || nums[i]>nums[prevIdx])
+                    res = max(res, 1+dp[i+1][i+1]);
+                dp[i][prevIdx+1] = res;
+            }
+        return dp[0][0];
+        
     }
 };

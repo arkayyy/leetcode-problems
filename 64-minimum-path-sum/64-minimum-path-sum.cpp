39f703f46366ce0d77ce0ee5cc1589349caee39a
Rajarshi Karan <72148786+arkayyy@users.noreply.github.com>
2022-06-19 00:58:28 +0530

    Time: 19 ms (24.92%), Space: 10.1 MB (28.01%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.cpp
+++ 64-minimum-path-sum/64-minimum-path-sum.cpp
@@ -1,38 +1,27 @@
 class Solution {
 public:
-    //INTUITION: Go through every route and calc the min. sum. We can store the min. sum attained starting from a specific index by memoizing.
-    int dfs(int i, int j,vector<vector<int>>& dp, vector<vector<int>>& grid)
+    int util(int i, int j, vector<vector<int>>& grid, vector<vector<int>>& dp)
     {
         if(i==grid.size()-1 && j==grid[0].size()-1) return grid[i][j];
+        if(i>=grid.size() || j>=grid[0].size()) return INT_MAX;
         
-        if(dp[i][j]!=-1) return dp[i][j]; //if value is already memoized, it means this path's min value has already been computed starting from (i,j)
+        if(dp[i][j]!=-1) return dp[i][j];
         
-        int mini = INT_MAX;
-        
-        if(i+1<grid.size())
-            mini = min(mini, grid[i][j]+dfs(i+1,j,dp,grid)); //moving down
-        if(j+1<grid[0].size())
-            mini = min(mini, grid[i][j]+dfs(i,j+1,dp,grid)); //moving right
-        
-        return dp[i][j] = mini;
+        return dp[i][j] = grid[i][j]+min(util(i+1,j,grid,dp),util(i,j+1,grid,dp));
     }
     int minPathSum(vector<vector<int>>& grid) {
-        int minSum = INT_MAX;
-        // vector<vector<int>> dp(grid.size(),vector<int>(grid[0].size(),-1));
-        // return dfs(0,0,dp,grid);
+        // vector<vector<int>> dp(grid.size(), vector<int>(grid[0].size(),-1));
+        // return util(0,0,grid,dp);
         
-        //TABULATION DP:-
-        vector<vector<int>> dp(grid.begin(),grid.end());
-        int r = grid.size(), c = grid[0].size();
-        for(int i = 1; i<r; i++)
-            dp[i][0] += dp[i-1][0]; //in 0th column, the path can come only from above, so adding the current value with the previous row value
-        for(int j = 1; j<c; j++)
-            dp[0][j] += dp[0][j-1]; //in 0th row, the path can come only from left, so adding the current value with the previous column value
+        vector<vector<int>> dp(grid.size()+1, vector<int>(grid[0].size()+1, INT_MAX));
         
-        for(int i= 1; i<r; i++)
-            for(int j = 1; j<c;j++)
-                dp[i][j] += min(dp[i-1][j], dp[i][j-1]); //addding the minimum of path from above and left to the current val
-        
-        return dp[r-1][c-1];
+        for(int i = grid.size()-1; i>=0; --i)
+            for(int j = grid[0].size()-1; j>=0; --j)
+            {
+                if(i==grid.size()-1 && j==grid[0].size()-1) dp[i][j] = grid[i][j];
+                else dp[i][j] = grid[i][j]+min(dp[i+1][j],dp[i][j+1]);
+            }
+                
+        return dp[0][0];
     }
 };

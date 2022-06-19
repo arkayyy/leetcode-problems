e7898012ff0b4c1d24b602057158f0e26bedcb12
Rajarshi Karan <72148786+arkayyy@users.noreply.github.com>
2022-06-19 12:04:08 +0530

    Added solution - LeetHub

---
+++ Palindromic Partitioning - GFG/palindromic-partitioning.cpp
@@ -0,0 +1,55 @@
+// { Driver Code Starts
+// Initial Template for c++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+public:
+vector<vector<int>> dp;
+    bool isPalin(int i,int j, string&s)
+    {
+        while(i<j)
+            if(s[i++]!=s[j--]) return false;
+        return true;
+    }
+    
+    int util(int i,int j, string& s)
+    {
+        if(i>j) return 0;
+        
+        if(isPalin(i,j,s)) return 0;
+        
+        if(dp[i][j]!=-1) return dp[i][j];
+        
+        int res = INT_MAX;
+        for(int t = i; t<j;++t)
+            if(isPalin(i,t,s))
+                res = min(res, 1+util(t+1,j,s));
+                
+        return dp[i][j] = (res==INT_MAX?0:res);
+    }
+    int palindromicPartition(string str)
+    {
+        dp = vector<vector<int>> (str.size(),vector<int>(str.size(),-1));
+        return util(0,str.size()-1,str);
+    }
+};
+
+// { Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        string str;
+        cin>>str;
+        
+        Solution ob;
+        cout<<ob.palindromicPartition(str)<<"\n";
+    }
+    return 0;
+}  // } Driver Code Ends

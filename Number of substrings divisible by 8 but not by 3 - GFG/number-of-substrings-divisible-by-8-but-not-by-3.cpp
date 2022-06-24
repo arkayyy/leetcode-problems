f71eb8d5b7c7112c818d9f875f0475df3531011c
Rajarshi Karan <72148786+arkayyy@users.noreply.github.com>
2022-06-25 01:32:43 +0530

    Added solution - LeetHub

---
+++ Number of substrings divisible by 8 but not by 3 - GFG/number-of-substrings-divisible-by-8-but-not-by-3.cpp
@@ -0,0 +1,97 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+class Solution{
+
+	public:
+	long long countDivisibleSubstring(string s)
+	{
+	   long long ans = 0, n = s.size();
+	    //  dp[i][x] stores Number of Substrings ends at i that have digit Sum Mod 3 = x
+		vector<vector<long long>> dp(s.size(), vector<long long>(3));
+		dp[0][0] = (s[0]%3 == 0);
+    	dp[0][1] = (s[0]%3 == 1);
+    	dp[0][2] = (s[0]%3 == 2);
+        
+        for(int i = 1 ; i < n ; i++)
+        {
+            int m = s[i]%3;
+            
+            dp[i][m] = dp[i-1][0];
+    		dp[i][((m+1)%3)] = dp[i-1][1];
+    		dp[i][((m+2)%3)] = dp[i-1][2];
+            
+            dp[i][m]++;
+        }
+        
+        
+        for(int i = 0 ; i < n ; i++)
+    	{
+    	    if(i >= 2)
+    	    {
+    	        
+    	        if( ((s[i]-48) + (s[i-1]-48)*10 + (s[i-2]-48)*100) % 8 == 0) // if Number, formed by Last 3 digits ,
+    			{                                                           //is divisible by 8 the whole Number is Divisible by 8
+    	            int m = ((s[i]-48) + (s[i-1]-48) + (s[i-2]-48))%3;
+    	            
+    	            if(i > 2)
+    	            {
+    	                if(m==0)
+    	                ans += dp[i-3][1] + dp[i-3][2];
+    	                
+    	                else if(m==1)
+    	                ans += dp[i-3][0] + dp[i-3][1];
+    	                
+    	                else
+    	                ans += dp[i-3][0] + dp[i-3][2];
+    	            }
+    	        }
+    	        int x= (s[i]-48) ; int y = (s[i-1]-48) * 10 + x ; int z = (s[i-2]-48) * 100 + y;
+                
+                 ans += (x%8 == 0 && x%3 != 0)+(y%8 == 0 && y%3 != 0)+(z%8 == 0 && z%3 != 0);
+    	    }
+    	    else
+    	    {
+    	        if(i == 0)
+    	        ans += (s[0]%8 == 0 && s[i]%3 !=0);
+    	        
+    	        if(i == 1)
+    	        {
+    	            int x = (s[0]-48) ; int y = (s[1]-48) ; int z = (x*10 + y);
+                
+                    ans += (y%8 == 0 && y%3 != 0)+(z%8 == 0 && z%3 != 0);
+    	        }
+    	    }
+    	}
+    	
+    	return ans;
+	}
+  
+
+};
+	 
+
+// { Driver Code Starts.
+
+int main() 
+{
+   	
+   
+   	int t;
+    cin >> t;
+    while (t--)
+    {
+       	string str;
+       	cin >> str;
+
+       
+
+        Solution ob;
+        cout << ob.countDivisibleSubstring(str);
+	    cout << "\n";
+	     
+    }
+    return 0;
+}  // } Driver Code Ends

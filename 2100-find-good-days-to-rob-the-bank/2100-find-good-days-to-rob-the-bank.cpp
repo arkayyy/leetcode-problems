b4ee3633c114124fa5252c989fe7e26b4bc9fd1c
Rajarshi Karan <72148786+arkayyy@users.noreply.github.com>
2022-06-26 06:26:46 +0530

    Time: 214 ms (44.40%), Space: 90 MB (5.74%) - LeetHub

--- 2100-find-good-days-to-rob-the-bank/2100-find-good-days-to-rob-the-bank.cpp
+++ 2100-find-good-days-to-rob-the-bank/2100-find-good-days-to-rob-the-bank.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
     vector<int> goodDaysToRobBank(vector<int>& security, int time) {
-        vector<int> ans;
-        vector<int> leftDecreasing(security.size(),0), rightIncreasing(security.size(),0);
-        int decr = 1;
-        leftDecreasing[0] = 1;
-        for(int i = 1; i<security.size(); ++i)
+        vector<int> pre(security.size()), suf(security.size());
+        
+        int decr = 0;
+        for(int i = 0;i<security.size(); ++i)
         {
-            if( security[i]<=security[i-1]) ++decr;
+            if(i-1>=0 && security[i-1]>=security[i]) ++decr;
             else decr = 1;
-            leftDecreasing[i] = decr;
+            pre[i] = decr;
         }
-        int incr = 1;
-        rightIncreasing[security.size()-1] = 1;
-        for(int i = security.size()-2; i>=0; --i)
+        
+        int incr = 0;
+        for(int i = security.size()-1; i>=0; --i)
         {
-            if(security[i]<=security[i+1]) ++incr;
+            if(i+1<security.size() && security[i]<=security[i+1]) ++incr;
             else incr = 1;
-            rightIncreasing[i] = incr;
+            suf[i] = incr;
         }
+        vector<int> ans;
         for(int i = 0; i<security.size(); ++i)
-            if(leftDecreasing[i]-1>=time && rightIncreasing[i]-1>=time) 
+            if(pre[i]-1>=time && suf[i]-1>=time)
                 ans.emplace_back(i);
         
         return ans;

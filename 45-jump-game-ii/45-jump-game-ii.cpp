bc20fa45874f947e2d6995d029b9247055a8e8f8
Rajarshi Karan <72148786+arkayyy@users.noreply.github.com>
2022-07-05 07:10:22 +0530

    Time: 17 ms (86.46%), Space: 16.6 MB (52.37%) - LeetHub

---
+++ 45-jump-game-ii/45-jump-game-ii.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int start = 0, end = 0, maxend = 0;
+        int step = 0;
+        while(end<nums.size()-1)
+        {
+            maxend = end+1;
+            ++step;
+            for(int i = start; i<=end; ++i)
+            {
+                if(i+nums[i]>=nums.size()) return step;
+                maxend = max(maxend,i+nums[i]);
+            }
+            start = end+1;
+            end = maxend;
+        }
+        return step;
+    }
+};

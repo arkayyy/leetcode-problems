9af6411a52482d31d17f8243b84c2bab9cb42f67
Rajarshi Karan <72148786+arkayyy@users.noreply.github.com>
2022-07-05 07:19:52 +0530

    Time: 12 ms (96.68%), Space: 16.5 MB (81.05%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.cpp
+++ 45-jump-game-ii/45-jump-game-ii.cpp
@@ -1,11 +1,16 @@
 class Solution {
 public:
     int jump(vector<int>& nums) {
+        //Most optimal Greedy/BFS Solution:- O(N) Time O(1) Space
+        //Since here if we at an index i, we can move any no. of steps from 0 to nums[i],
+        //therefore we can do it using greedy basis to reach the max possible index from that index
+        
+        //[start,end] is the range of the elements that we could jump to from previous index
         int start = 0, end = 0, maxend = 0;
         int step = 0;
         while(end<nums.size()-1)
         {
-            maxend = end+1;
+           // maxend = end+1; //
             ++step;
             for(int i = start; i<=end; ++i)
             {

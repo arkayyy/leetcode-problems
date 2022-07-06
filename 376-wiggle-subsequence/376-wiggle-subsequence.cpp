db376dc9b67b6f7395da080e1d23e939383cc6af
Rajarshi Karan <72148786+arkayyy@users.noreply.github.com>
2022-07-06 11:19:13 +0530

    Time: 3 ms (63.15%), Space: 7 MB (69.36%) - LeetHub

--- 376-wiggle-subsequence/376-wiggle-subsequence.cpp
+++ 376-wiggle-subsequence/376-wiggle-subsequence.cpp
@@ -1,10 +1,14 @@
 class Solution {
 public:
     int wiggleMaxLength(vector<int>& nums) {
-        int firstElementPeak = 1, firstElementValley = 1;
+    //purpose of total1 and total2 both are to store the total number of peaks and valleys
+        int total1 = 1, total2 = 1; //total1->updated only when a new peak is found; total2-> updated whenever a new valley found
         for(int i = 1; i<nums.size(); ++i)
-            if(nums[i]>nums[i-1]) firstElementPeak =  firstElementValley+1;
-            else if(nums[i]<nums[i-1]) firstElementValley = firstElementPeak +1;
-        return max(firstElementPeak ,firstElementValley);
+        {
+            if(nums[i]>nums[i-1]) total1 = total2+1;
+            else if(nums[i]<nums[i-1]) total2 = total1+1;
+        }
+        //total1 and total2(or vice versa) will always have a difference of 1
+        return max(total1,total2);
     }
 };

f570822aa2405b902cbdaf5e1863656b8d8e6cb3
Rajarshi Karan <72148786+arkayyy@users.noreply.github.com>
2022-11-16 21:41:03 +0530

    Time: 6 ms (19.12%), Space: 6.2 MB (90.85%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -10,7 +10,10 @@ public:
                 while(i<haystack.size() && j<needle.size() && needle[j]==haystack[i])                           {if(j!=0 && last ==-1 && haystack[i]==needle[0]) last= i; 
                    ++i,++j;}
                 if(j==needle.size()) return i-needle.size();
-                else {j=0; if(last!=-1)i = last;}
+                else {j=0; 
+                if(last!=-1)i = last; //setting value of i to the firsmost index of 0th character of needle that was encountered during current matching. 
+                      //This step is done for cases like: inp = "mississippi" and needle="issi"
+                     }
             }
             else ++i;
         }

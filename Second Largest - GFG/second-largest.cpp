a60a96ac90835df1b607acc2c4d5bb457215a65b
Rajarshi Karan <72148786+arkayyy@users.noreply.github.com>
2022-11-18 01:42:09 +0530

    Added solution - LeetHub

--- Second Largest - GFG/second-largest.cpp
+++ Second Largest - GFG/second-largest.cpp
@@ -12,7 +12,7 @@ public:
 	int print2largest(int arr[], int n) {
         int maxi1 = INT_MIN, maxi2 = INT_MIN;
         for(auto i = 0; i<n; ++i) {
-            if(arr[i]>maxi1) {maxi2 = max(maxi1,maxi2); maxi1 = arr[i]; }
+            if(arr[i]>maxi1) {maxi2 = maxi1; maxi1 = arr[i]; }
             else if(arr[i]>maxi2 && arr[i]!=maxi1) {maxi2 = arr[i];}
         }
         return maxi2==INT_MIN?-1:maxi2;

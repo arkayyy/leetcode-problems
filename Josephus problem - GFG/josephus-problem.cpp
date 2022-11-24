345c31a292a7c46c975bc6031ff0e4fa89d0ced7
Rajarshi Karan <72148786+arkayyy@users.noreply.github.com>
2022-11-25 02:34:43 +0530

    Added solution - LeetHub

--- Josephus problem - GFG/josephus-problem.cpp
+++ Josephus problem - GFG/josephus-problem.cpp
@@ -12,8 +12,13 @@ class Solution
     public:
     int josephus(int n, int k)
     {
-       if(n==1) return 1;
-       return (josephus(n-1,k) + k - 1)%n + 1;
+      if(n==1) return 1;
+      return (josephus(n-1,k) + k - 1)%n + 1;//first doing -1 to convert to 0-based index, process it and then 
+       //doing +1 to return answer in 1 based indexing
+       
+       //O(1) approach (Power of K approach):-
+       
+    //   return 2*(n-pow(K,floor(power of K nearest to N)))+1;
     }
 };
 
